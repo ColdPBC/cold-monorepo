d9a59ba229c1b662376d676104d1f24e8eddd495
Troy Morvant <troy.morvant@gmail.com>
2023-09-06 18:54:52 -0500

    fix: storybook issues

---
+++ apps/cold-ui/.storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/addons";
+import coldTheme from './coldTheme';
+
+addons.setConfig({
+  theme: coldTheme,
+});
---
+++ apps/cold-ui/.storybook/preview.js
@@ -0,0 +1,101 @@
+import documentationTemplate from './documentationTemplate.mdx';
+import "../src/styles.css";
+import 'flowbite';
+
+
+import React from 'react';
+import {cookieDecorator} from 'storybook-addon-cookie';
+import {auth0UserMock} from "@coldpbc/mocks";
+
+// Storybook executes this module in both bootstap phase (Node)
+// and a story's runtime (browser). However, we cannot call `setupWorker`
+// in Node environment, so need to check if we're in a browser.
+if (typeof global.process === 'undefined') {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const {worker} = require('@coldpbc/mocks');
+    // Start the mocking when each story is loaded.
+    // Repetitive calls to the `.start()` method do not register a new worker,
+    // but check whether there's an existing once, reusing it, if so.
+    worker.start({
+        onUnhandledRequest(req) {
+            if(!req.url.href.includes('http://localhost:6006')){
+                console.warn(
+                    'Found an unhandled %s request to %s',
+                    req.method,
+                    req.url.href,
+                )
+            } else {
+                console.warn(
+                    'Found an unhandled %s request to %s',
+                    req.method,
+                    req.url.href,
+                )
+            }
+
+        },
+    }).then((r) => console.log(r));
+}
+
+export default {
+    parameters: {
+        actions: {argTypesRegex: "^on[A-Z].*"},
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/,
+            },
+        },
+        previewTabs: {
+            "storybook/docs/panel": {index: -1},
+        },
+        docs: {
+            page: documentationTemplate,
+        },
+        backgrounds: {
+            default: 'cold',
+            values: [
+                {
+                    name: 'cold',
+                    value: '#080912',
+                },
+                {
+                    name: 'white',
+                    value: '#FFFFFF'
+                },
+            ],
+        },
+        cookie: {
+            "coldpbc": {
+                accessToken: "accessToken",
+                user: auth0UserMock,
+            }
+        },
+        auth0AddOn: {
+            isLoading: false,
+            isAuthenticated: true,
+            getAccessTokenSilently: () => {
+                return "accessToken"
+            },
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            getAccessTokenWithPopup: ()  => {},
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            getIdTokenClaims: ()  => {},
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            loginWithRedirect: ()  => {},
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            loginWithPopup: ()  => {},
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            logout: ()  => {},
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            handleRedirectCallback: ()  => {},
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            buildAuthorizeUrl: ()  => {},
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            buildLogoutUrl: ()  => {},
+            user: auth0UserMock,
+        }
+    },
+    decorators: [
+    ]
+};
