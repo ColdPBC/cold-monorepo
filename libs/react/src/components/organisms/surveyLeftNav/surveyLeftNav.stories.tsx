b19a35ae96b3161e4f2bb74323c779cd6d7d60c7
Qaalib Farah <qaalib1598@gmail.com>
2023-09-08 10:11:01 -0500

    feat: surveyleftnav component

---
+++ libs/react/src/components/organisms/surveyLeftNav/surveyLeftNav.stories.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { withKnobs } from "@storybook/addon-knobs";
+import { Meta, StoryObj } from "@storybook/react";
+import {SurveyLeftNav, SurveyLeftNavProps} from "./surveyLeftNav";
+import {getTestingSurveyFormDefinition} from "@coldpbc/mocks";
+
+const meta = {
+  title: "Organisms/SurveyLeftNav",
+  component: SurveyLeftNav,
+  tags: ["autodocs"],
+  decorators: [withKnobs],
+  argTypes: {
+
+  },
+} satisfies Meta<typeof SurveyLeftNav>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const SurveyLeftNavStory = (args: SurveyLeftNavProps) => {
+
+  const [activeKey, setActiveKey] = React.useState(args.activeKey);
+
+  return <SurveyLeftNav
+    {...args}
+    activeKey={activeKey}
+    setActiveKey={setActiveKey}
+  />
+}
+
+export const Intro: Story = {
+  render: (args) => <SurveyLeftNavStory {...args} />,
+  args: {
+    activeKey: "",
+    surveyFormDefinition: getTestingSurveyFormDefinition().definition,
+  },
+};
+
+export const InSurvey: Story = {
+  render: (args) => <SurveyLeftNavStory {...args} />,
+  args: {
+    activeKey: "product",
+    surveyFormDefinition: getTestingSurveyFormDefinition().definition,
+  },
+};
