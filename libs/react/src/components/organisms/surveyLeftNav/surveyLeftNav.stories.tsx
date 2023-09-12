9c4273bfaad61e448fffaad5547a5e766e357422
Qaalib Farah <qaalib1598@gmail.com>
2023-09-12 12:03:04 -0500

    feat: updated SurveyLeftNav

--- libs/react/src/components/organisms/surveyLeftNav/surveyLeftNav.stories.tsx
+++ libs/react/src/components/organisms/surveyLeftNav/surveyLeftNav.stories.tsx
@@ -1,45 +1,50 @@
-import React from "react";
-import { withKnobs } from "@storybook/addon-knobs";
-import { Meta, StoryObj } from "@storybook/react";
-import {SurveyLeftNav, SurveyLeftNavProps} from "./surveyLeftNav";
-import {getTestingSurveyData} from "@coldpbc/mocks";
+import React from 'react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { Meta, StoryObj } from '@storybook/react';
+import { SurveyLeftNav, SurveyLeftNavProps } from './surveyLeftNav';
+import { getTestingSurveyData } from '@coldpbc/mocks';
 
 const meta = {
-  title: "Organisms/SurveyLeftNav",
+  title: 'Organisms/SurveyLeftNav',
   component: SurveyLeftNav,
-  tags: ["autodocs"],
+  tags: ['autodocs'],
   decorators: [withKnobs],
-  argTypes: {
-
-  },
+  argTypes: {},
 } satisfies Meta<typeof SurveyLeftNav>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 const SurveyLeftNavStory = (args: SurveyLeftNavProps) => {
-
   const [activeKey, setActiveKey] = React.useState(args.activeKey);
 
-  return <SurveyLeftNav
-    {...args}
-    activeKey={activeKey}
-    setActiveKey={setActiveKey}
-  />
-}
+  return (
+    <SurveyLeftNav
+      {...args}
+      activeKey={activeKey}
+      setActiveKey={setActiveKey}
+    />
+  );
+};
 
 export const Intro: Story = {
   render: (args) => <SurveyLeftNavStory {...args} />,
   args: {
-    activeKey: "",
-    surveyFormDefinition: getTestingSurveyData().definition,
+    activeKey: {
+      value: '',
+      isFollowUp: false,
+    },
+    surveyData: getTestingSurveyData(),
   },
 };
 
 export const InSurvey: Story = {
   render: (args) => <SurveyLeftNavStory {...args} />,
   args: {
-    activeKey: "product",
-    surveyFormDefinition: getTestingSurveyData().definition,
+    activeKey: {
+      value: 'product',
+      isFollowUp: false,
+    },
+    surveyData: getTestingSurveyData(),
   },
 };
