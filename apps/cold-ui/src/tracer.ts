11df8d884a2c2e24140da976abfac96ba84b47b9
Troy Morvant <troy.morvant@gmail.com>
2023-09-15 14:03:22 -0500

    chore: update rum file

--- apps/cold-ui/src/tracer.ts
+++ apps/cold-ui/src/tracer.ts
@@ -1,4 +1,17 @@
-// tracer.ts
-import tracer from 'dd-trace';
-tracer.init(); // initialized in a different file to avoid hoisting.
-export default tracer;
+import { datadogRum } from '@datadog/browser-rum';
+import { get } from 'lodash';
+datadogRum.init({
+  applicationId: `${get(import.meta, 'env.VITE_DD_RUM_APPLICATION_ID', '')}`,
+  clientToken: `${get(import.meta, 'env.VITE_DD_RUM_CLIENT_TOKEN', '')}`,
+  site: 'us5.datadoghq.com',
+  service: 'ui',
+  env: `${get(import.meta, 'env.DD_ENV', '')}`,
+  // Specify a version number to identify the deployed version of your application in Datadog
+  // version: '1.0.0',
+  sessionSampleRate: 100,
+  premiumSampleRate: 100,
+  trackUserInteractions: true,
+  defaultPrivacyLevel: 'mask-user-input',
+});
+
+datadogRum.startSessionReplayRecording();
