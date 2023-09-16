3277c0d4510590ee8cbd88a9c48cf4ef0491c414
Qaalib Farah <qaalib1598@gmail.com>
2023-09-15 23:04:24 -0500

    feat: created signup form component

---
+++ libs/react/src/__mocks__/signupHandlers.ts
@@ -0,0 +1,22 @@
+import { rest } from 'msw';
+import { getFootprintDataMock } from './categoriesMock';
+
+export const getSignUpHandler = {
+  DEFAULT: [
+    rest.patch(`*/users/:emailOrId`, (req, res, ctx) => {
+      return res(ctx.json({}));
+    }),
+    rest.post(`*/organizations`, (req, res, ctx) => {
+      const body = req.body as {
+        name: string;
+      };
+      return res(ctx.json({}));
+    }),
+    rest.post(`*/policies/:id/signed`, (req, res, ctx) => {
+      const body = req.body as {
+        name: string;
+      };
+      return res(ctx.json({}));
+    }),
+  ],
+};
