f4934b9de27f7df3af24a8d481a8a50001b62edd
Dan Lindquist <dan.lindquist@coldclimate.com>
2023-09-20 18:44:29 -0500

    Fix: update dismissableInfoCard story to have dismissKey prop

--- libs/react/src/components/molecules/dismissableInfoCard/dismissableInfoCard.stories.tsx
+++ libs/react/src/components/molecules/dismissableInfoCard/dismissableInfoCard.stories.tsx
@@ -17,6 +17,7 @@ export const Default: Story = {
       <DismissableInfoCard
         onDismiss={() => {}}
         text='Your footprint is a snapshot of the greenhouse gases your company emitted over a specific timeframe. It is measured in tons of carbon dioxide equivalent, expressed as tCO2e.'
+        dismissKey="story_key"
       />
   )
 };
