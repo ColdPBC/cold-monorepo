fc4a20b8a88914052bef824b22bdb52d6ac0fa79
Brec Hanson <brechanson@gmail.com>
2023-09-25 19:37:15 -0400

    Add minWidth to force centerColumnContent to right width

--- libs/react/src/components/organisms/centerColumnContent/centerColumnContent.tsx
+++ libs/react/src/components/organisms/centerColumnContent/centerColumnContent.tsx
@@ -8,7 +8,7 @@ export function CenterColumnContent(
   props: PropsWithChildren<CenterColumnContentProps>,
 ) {
   return (
-    <div className="w-[668px] flex flex-col items-center gap-6">
+    <div className="min-w-[668px] max-w-[668px] flex flex-col items-center gap-6">
       {
         // show title if we have one
         props.title && <div className="text-h1 self-stretch text-tc-primary">{props.title}</div>
