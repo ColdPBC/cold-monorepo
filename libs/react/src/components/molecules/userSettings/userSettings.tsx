895c111d1274650c7c3582b5d849ab65f2eac64a
Brec Hanson <brechanson@gmail.com>
2023-09-27 01:05:26 -0400

    Attempt

--- libs/react/src/components/molecules/userSettings/userSettings.tsx
+++ libs/react/src/components/molecules/userSettings/userSettings.tsx
@@ -2,7 +2,6 @@ import { User } from "@auth0/auth0-react";
 import { ButtonTypes } from "@coldpbc/enums";
 import { axiosFetcher } from "@coldpbc/fetchers";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router"
 import { BaseButton, Input } from "../../atoms";
 import { Card } from "../card"
 import { Modal } from "../modal";
@@ -21,7 +20,6 @@ export const UserSettings = ({
         email
     } = user;
 
-    const navigate = useNavigate();
     const [showFirstNameModal, setShowFirstNameModal] = useState(false);
     const [showLastNameModal, setShowLastNameModal] = useState(false);
     const [firstName, setFirstName] = useState<string | undefined>(
@@ -61,7 +59,7 @@ export const UserSettings = ({
             ctas={[
                 {
                     text: 'Log Out',
-                    action: () => navigate('/logout')
+                    action: () => console.log('Steve Austin')
                 }
             ]}
         >
