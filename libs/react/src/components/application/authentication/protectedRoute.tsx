c75687d6df1b76f923d1c5270bbf423b05d54d36
Qaalib Farah <qaalib1598@gmail.com>
2023-10-11 11:23:54 -0500

    fix: remove old cookie handling

--- libs/react/src/components/application/authentication/protectedRoute.tsx
+++ libs/react/src/components/application/authentication/protectedRoute.tsx
@@ -108,13 +108,7 @@ export const ProtectedRoute = () => {
               }
               const accessToken = await getAccessTokenSilently(options);
 
-              setCookie(
-                'coldpbc',
-                { user, accessToken },
-                {
-                  maxAge: 60 * 60 * 24,
-                },
-              );
+              setCookie('coldpbc', { user, accessToken });
 
               if (ldClient && user.coldclimate_claims.org_id) {
                 await ldClient.identify({
