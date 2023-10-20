50173ebb6ba5cc8d78552fffd84f81e38c461755
Qaalib Farah <qaalib1598@gmail.com>
2023-10-20 14:34:36 -0500

    feat: Update mock to dynamic date and not static

--- libs/react/src/__mocks__/newsMock.ts
+++ libs/react/src/__mocks__/newsMock.ts
@@ -5,7 +5,9 @@ export function getNewsDefault() {
       url: 'https://www.reuters.com/sustainability/carbon-credit-market-confidence-ebbs-big-names-retreat-2023-09-01/',
       image_url:
         'https://www.reuters.com/resizer/19ZnOlXJL0lqbq588RsYF5Z-cAM=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/6ELBCZRUIRK7FNNQCF66P47QIA.jpg',
-      published_at: '2023-09-27',
+      published_at: new Date(
+        new Date().setDate(new Date().getDate() - 1),
+      ).toISOString(),
       source_name: 'Reuters',
     },
     {
@@ -13,7 +15,9 @@ export function getNewsDefault() {
       url: 'https://www.nytimes.com/2023/07/21/climate/diet-vegan-meat-emissions.html',
       image_url:
         'https://static01.nyt.com/images/2023/07/21/science/21CLI-VEGAN/21CLI-VEGAN-facebookJumbo.jpg',
-      published_at: '2023-07-21',
+      published_at: new Date(
+        new Date().setMonth(new Date().getMonth() - 1),
+      ).toISOString(),
       source_name: 'The New York Times',
     },
   ];
@@ -25,14 +29,18 @@ export function getNewsAllMissingProperties() {
       title: 'Carbon credit market confidence ebbs as big names retreat',
       image_url:
         'https://www.reuters.com/resizer/19ZnOlXJL0lqbq588RsYF5Z-cAM=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/6ELBCZRUIRK7FNNQCF66P47QIA.jpg',
-      published_at: '2023-09-01',
+      published_at: new Date(
+        new Date().setDate(new Date().getDate() - 1),
+      ).toISOString(),
       source_name: 'Reuters',
     },
     {
       url: 'https://www.nytimes.com/2023/07/21/climate/diet-vegan-meat-emissions.html',
       image_url:
         'https://static01.nyt.com/images/2023/07/21/science/21CLI-VEGAN/21CLI-VEGAN-facebookJumbo.jpg',
-      published_at: '2023-07-21',
+      published_at: new Date(
+        new Date().setMonth(new Date().getMonth() - 1),
+      ).toISOString(),
       source_name: 'The New York Times',
     },
   ];
@@ -44,7 +52,9 @@ export function getNewsSomeMissingProperties() {
       url: 'https://www.reuters.com/sustainability/carbon-credit-market-confidence-ebbs-big-names-retreat-2023-09-01/',
       image_url:
         'https://www.reuters.com/resizer/19ZnOlXJL0lqbq588RsYF5Z-cAM=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/6ELBCZRUIRK7FNNQCF66P47QIA.jpg',
-      published_at: '2023-09-01',
+      published_at: new Date(
+        new Date().setDate(new Date().getDate() - 1),
+      ).toISOString(),
       source_name: 'Reuters',
     },
     {
@@ -52,7 +62,9 @@ export function getNewsSomeMissingProperties() {
       url: 'https://www.nytimes.com/2023/07/21/climate/diet-vegan-meat-emissions.html',
       image_url:
         'https://static01.nyt.com/images/2023/07/21/science/21CLI-VEGAN/21CLI-VEGAN-facebookJumbo.jpg',
-      published_at: '2023-07-21',
+      published_at: new Date(
+        new Date().setMonth(new Date().getMonth() - 1),
+      ).toISOString(),
       source_name: 'The New York Times',
     },
   ];
