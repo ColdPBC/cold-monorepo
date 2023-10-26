3f00bf597911a17a5ac5a182e4a2b61aac53dce3
Qaalib Farah <qaalib1598@gmail.com>
2023-10-26 09:38:08 -0500

    fix: remove extra period

--- libs/react/src/components/pages/actionDetail/completedBanner.tsx
+++ libs/react/src/components/pages/actionDetail/completedBanner.tsx
@@ -5,7 +5,7 @@ export const CompletedBanner = () => {
       <div className="z-20">
         <div className="font-bold text-xl leading-normal mb-2">Keep it up!</div>
         <div className="font-medium leading-normal text-sm">
-          You've done everything you can here for now..
+          You've done everything you can here for now.
         </div>
       </div>
       <svg
