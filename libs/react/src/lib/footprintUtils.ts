6597f1b99d9508b25efa956e687a8f9e82e44cc0
Qaalib Farah <qaalib1598@gmail.com>
2023-11-01 18:19:09 -0500

    feat: add 2nd decimal number if needed

--- libs/react/src/lib/footprintUtils.ts
+++ libs/react/src/lib/footprintUtils.ts
@@ -2,5 +2,5 @@ import { NumericFormat } from 'react-number-format';
 import numeral from 'numeral';
 
 export const formatTonnes = (value: any) => {
-  return numeral(value).format('0,0[.]0');
+  return numeral(value).format('0,0[.]00');
 };
