a85afe08f6390a2728b4c15a7a551666573176c0
Troy Morvant <troy.morvant@gmail.com>
2023-11-28 17:38:40 -0600

    build: improve build

--- updateVersion.mjs
+++ updateVersion.mjs
@@ -2,22 +2,35 @@ import lodash from 'lodash';
 const {set} = lodash;
 import * as fs from 'fs';
 
-const updateVersion = (version) => {
+const updateVersion = (version, branch) => {
+    console.log('Updating version to', version, 'for branch', branch);
     fs.readFile('flightcontrol.json', 'utf8', (err, data) => {
         if (err) {
             return console.log(err);
         }
         const fcData = JSON.parse(data);
 
-        if (process.env.NODE_ENV === 'production') {
+        if (branch === 'production') {
             set(fcData, 'environments[0].services[0].envVariables.VITE_APP_VERSION', version);
+            console.log(`Updated 'environments[${branch}].services[UI].envVariables.VITE_APP_VERSION' to ${version}`);
+
             set(fcData, 'environments[0].services[1].dockerLabels["com.datadoghq.tags.version"]', version);
-        } else {
+            console.log(`Updated 'environments[${branch}].services[API].dockerLabels["com.datadoghq.tags.version"]' to ${version}`);
+
+        } else if(branch === 'staging') {
             set(fcData, 'environments[1].services[0].envVariables.VITE_APP_VERSION', version);
+            console.log(`Updated 'environments[${branch}].services[UI].envVariables.VITE_APP_VERSION' to ${version}`);
+
             set(fcData, 'environments[1].services[1].dockerLabels["com.datadoghq.tags.version"]', version);
+            console.log(`Updated 'environments[${branch}].services[API].dockerLabels["com.datadoghq.tags.version"]' to ${version}`);
+        } else {
             set(fcData, 'environments[2].services[0].envVariables.VITE_APP_VERSION', version);
+            console.log(`Updated 'environments[${branch}].services[UI].envVariables.VITE_APP_VERSION' to ${version}`);
+
             set(fcData, 'environments[2].services[1].dockerLabels["com.datadoghq.tags.version"]', version);
+            console.log(`Updated 'environments[${branch}].services[API].dockerLabels["com.datadoghq.tags.version"]' to ${version}`);
         }
+
         fs.writeFile('flightcontrol.json', JSON.stringify(fcData, null, 2), 'utf8', (err) => {
             if (err) {
                 return console.log(err);
@@ -26,5 +39,5 @@ const updateVersion = (version) => {
     });
 };
 
-updateVersion(process.argv[2]);
+updateVersion(process.argv[2], process.argv[3]);
 
