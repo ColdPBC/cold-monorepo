a9920cfb02312b7353351d75e9f3c02b0b122c90
Troy Morvant <troy.morvant@gmail.com>
2023-11-28 15:44:02 -0600

    build: improve build

--- updateVersion.mjs
+++ updateVersion.mjs
@@ -1,41 +1,30 @@
-import lod from 'lodash';
-const {get, set} = lod;
+import lodash from 'lodash';
+const {set} = lodash;
 import * as fs from 'fs';
-import path from "path";
-const getJson = (name) => {
-    const firstPath = path.resolve('./', name);
-    if (fs.existsSync(firstPath)) {
-        return fs.readFileSync(firstPath).toString();
-    } else {
-        throw new Error(`${name} is not found in '${firstPath}'`);
-    }
-}
-const updateVersion = () => {
+
+const updateVersion = (version) => {
     fs.readFile('flightcontrol.json', 'utf8', (err, data) => {
         if (err) {
             return console.log(err);
         }
         const fcData = JSON.parse(data);
-        const raw = getJson('package.json');
-        if(raw && typeof raw === 'string'){
-            const pkg = JSON.parse(raw);
-            const version = get(pkg, 'version', '0.0.0');
-            if(process.env.NODE_ENV === 'production'){
-                set(fcData, 'environments[0].services[0].envVariables.VITE_APP_VERSION', version);
-                set(fcData, 'environments[0].services[1].dockerLabels["com.datadoghq.tags.version"]', version);
-            } else {
-                set(fcData, 'environments[1].services[0].envVariables.VITE_APP_VERSION"]', version);
-                set(fcData, 'environments[1].services[1].dockerLabels["com.datadoghq.tags.version"]', version);
-                set(fcData, 'environments[2].services[0].envVariables.VITE_APP_VERSION"]', version);
-                set(fcData, 'environments[2].services[1].dockerLabels["com.datadoghq.tags.version"]', version);
-            }
-            fs.writeFile('flightcontrol.json', JSON.stringify(fcData, null, 2), 'utf8', (err) => {
-                if (err) {
-                    return console.log(err);
-                }
-            });
+
+        if (process.env.NODE_ENV === 'production') {
+            set(fcData, 'environments[0].services[0].envVariables.VITE_APP_VERSION', version);
+            set(fcData, 'environments[0].services[1].dockerLabels["com.datadoghq.tags.version"]', version);
+        } else {
+            set(fcData, 'environments[1].services[0].envVariables.VITE_APP_VERSION', version);
+            set(fcData, 'environments[1].services[1].dockerLabels["com.datadoghq.tags.version"]', version);
+            set(fcData, 'environments[2].services[0].envVariables.VITE_APP_VERSION', version);
+            set(fcData, 'environments[2].services[1].dockerLabels["com.datadoghq.tags.version"]', version);
         }
+        fs.writeFile('flightcontrol.json', JSON.stringify(fcData, null, 2), 'utf8', (err) => {
+            if (err) {
+                return console.log(err);
+            }
+        });
     });
 };
 
-updateVersion();
+updateVersion(process.argv[2]);
+
