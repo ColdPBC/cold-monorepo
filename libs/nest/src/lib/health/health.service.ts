fc6299d9e07e099997cf0fbdfc3a2c92407ae22b
Troy Morvant <troy.morvant@gmail.com>
2023-11-29 18:19:41 -0600

    fix: health check logs

--- libs/nest/src/lib/health/health.service.ts
+++ libs/nest/src/lib/health/health.service.ts
@@ -15,7 +15,7 @@ export class HealthService extends BaseWorker {
    */
   async health(req: { body: any; headers: any; query: any }): Promise<any> {
     if (get(process.env, 'ENABLE_HEALTH_LOGS', false)) {
-      this.logger.log('Health check request:', { req });
+      this.logger.log('Health check request:', { body: req.body, headers: req.headers, query: req.query });
     }
     return 'ok';
   }
