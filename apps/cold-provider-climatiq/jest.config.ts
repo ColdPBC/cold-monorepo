c333d3e66beb3ac971c8eeb25577aac5eb91ed6b
Troy Morvant <troy.morvant@gmail.com>
2023-12-12 13:59:10 -0600

    test: Update displayName and coverageDirectory in jest.config
    The jest.config files have been updated to change the displayName and coverageDirectory from 'cold-provider-provider' to 'cold-integrations-integrations'. This is to ensure consistency and accurate identification of the application across different testing and coverage environments.

--- apps/cold-provider-climatiq/jest.config.ts
+++ apps/cold-provider-climatiq/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'cold-provider-provider',
+  displayName: 'cold-integrations-integrations',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/cold-provider-provider',
+  coverageDirectory: '../../coverage/apps/cold-integrations-integrations',
 };
