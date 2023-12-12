dfc4853a2ace3d09eb9a1076ea38f81e3b832cb2
Troy Morvant <troy.morvant@gmail.com>
2023-12-12 13:59:47 -0600

    fix: error handling in organization.service
    Error handling in organization.service has been updated to use a switch statement for conditional exception throwing. This improves error specificity and logging, making it easier to track and solve errors. The change also includes an update to the metrics increment function by removing the recording of invitation failures.

--- apps/cold-api/src/platform/modules/resources/organizations/organization.service.ts
+++ apps/cold-api/src/platform/modules/resources/organizations/organization.service.ts
@@ -102,15 +102,20 @@ export class OrganizationService extends BaseWorker {
 
       return invitation.data;
     } catch (e) {
-      if (e.response.status !== 404) {
-        this.logger.error(e.response?.data ? e.response.data : e.response, data);
+      switch (e.response.status) {
+        case 404:
+          this.logger.error(e.response?.data ? e.response.data : e.response, data);
+          throw new NotFoundException(e);
+        case 400:
+          this.logger.error(e.response?.data ? e.response.data : e.response, data);
+          throw new UnprocessableEntityException(e);
+        case 409:
+          this.logger.error(e.response?.data ? e.response.data : e.response, data);
+          throw new ConflictException(e);
+        default:
+          this.logger.error(e.response?.data ? e.response.data : e.response, data);
+          throw new HttpException(e.response?.data ? e.response.data : e.response, e.response.status);
       }
-
-      // increment failed invitation metric
-      tags.status = 'failed';
-      if (!find(this.test_orgs, { id: orgId })) this.metrics.increment('cold.api.invitations.sent', tags);
-
-      return e.response?.data ? e.response.data : e.response;
     }
   }
 
