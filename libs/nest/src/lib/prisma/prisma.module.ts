b4df26200de4cff77e278dffbd475c39ef9f75ba
Troy Morvant <troy.morvant@gmail.com>
2023-12-20 23:44:53 -0600

    fix: make prisma module global

--- libs/nest/src/lib/prisma/prisma.module.ts
+++ libs/nest/src/lib/prisma/prisma.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { Global } from '@nestjs/common/decorators/modules/global.decorator';
 
+@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
