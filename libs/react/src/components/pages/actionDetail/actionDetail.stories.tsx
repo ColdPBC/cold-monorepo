c4661f6e24f7b161b4376ff79a3987e30e73d242
Qaalib Farah <qaalib1598@gmail.com>
2023-12-26 12:33:27 -0600

    feat: update to action detail

--- libs/react/src/components/pages/actionDetail/actionDetail.stories.tsx
+++ libs/react/src/components/pages/actionDetail/actionDetail.stories.tsx
@@ -2,8 +2,9 @@ import { StoryMockProvider, getCategoriesHandler, getActionHandler } from '@cold
 import { withKnobs } from '@storybook/addon-knobs';
 import { Meta, StoryObj } from '@storybook/react';
 import { ActionDetail } from './actionDetail';
-import { fireEvent, waitFor, within } from '@storybook/testing-library';
+import { userEvent, waitFor, within } from "@storybook/testing-library";
 import { expect } from '@storybook/jest';
+import { addYears, format } from "date-fns";
 
 const meta: Meta<typeof ActionDetail> = {
   title: 'Pages/ActionDetail',
@@ -28,12 +29,12 @@ export const Default: Story = {
       const addStewardButton = (await canvas.findByText('Add Steward')).closest('button');
       // click Add Steward button
       if (addStewardButton) {
-        fireEvent.click(addStewardButton);
+        await userEvent.click(addStewardButton);
       }
       await waitFor(async () => {
         const stewardDropdown = await canvas.findByTestId('flowbite-dropdown');
         const option = await within(stewardDropdown).findByText('Qaalib Farah');
-        fireEvent.click(option);
+        await userEvent.click(option);
       });
       await waitFor(async () => {
         await canvas.findByText('Qaalib Farah');
@@ -43,18 +44,33 @@ export const Default: Story = {
     await step('Assign Target Date', async () => {
       const targetDateText = await canvas.findByText('Target Date: December 10, 2023');
       const targetDateButton = await canvas.findByRole('button', { name: 'Edit Target Date' });
-      fireEvent.click(targetDateButton);
+      await userEvent.click(targetDateButton);
+      // click button that has text of current month and year. example: December 2023
+      const currentMonthYearButton = await canvas.findByRole('button', { name: format(new Date(), 'MMMM yyyy') });
+      await userEvent.click(currentMonthYearButton);
+      //click button with year text
+      const currentYearButton = await canvas.findByRole('button', { name: format(new Date(), 'yyyy') });
+      await userEvent.click(currentYearButton);
+      // find button with text of next year
+      const nextYearButton = await canvas.findByRole('button', { name: format(addYears(new Date(), 1), 'yyyy') });
+      await userEvent.click(nextYearButton);
+      // choose the Jan month button
+      const janButton = await canvas.findByRole('button', { name: 'Jan' });
+      await userEvent.click(janButton);
+      // click button with text of 15
+      const dayButton = await canvas.findByRole('button', { name: '15' });
+      await userEvent.click(dayButton);
       await waitFor(async () => {
-        // find button with 10 text
-        const button20 = await canvas.findByRole('button', { name: '20' });
-        fireEvent.click(button20);
+        // expect the target date text to be updated to be January 15 of next year
+        const nextYear = format(addYears(new Date(), 1), 'yyyy');
+        await expect(canvas.findByText(`Target Date: January 15, ${nextYear}`)).toBeTruthy();
       });
+      // click outside of the calendar to close it
+      await userEvent.click(targetDateText);
+      // verify its closed
       await waitFor(async () => {
-        const currentMonth = new Date().toLocaleString('default', { month: 'long' });
-        const currentYear = new Date().getFullYear();
-        await canvas.findByText(`Target Date: ${currentMonth} 20, ${currentYear}`);
+        await expect(canvas.queryByRole('button', { name: '15' })).toBeNull();
       });
-      fireEvent.click(targetDateButton);
     });
   },
 };
@@ -75,11 +91,11 @@ export const NoResources: Story = {
     await step('Set all steps to complete', async () => {
       await expect(canvas.queryByText('Keep it up!')).toBeNull();
       const emptyCheckboxes = await canvas.findAllByTestId('step-detail-empty-checkbox');
-      fireEvent.click(emptyCheckboxes[0]);
+      await userEvent.click(emptyCheckboxes[0]);
       await waitFor(async () => {
         // expect the empty checkboxes to be less than the original number
         await expect(await canvas.findAllByTestId('step-detail-empty-checkbox')).toHaveLength(emptyCheckboxes.length - 1);
-        fireEvent.click(emptyCheckboxes[1]);
+        await userEvent.click(emptyCheckboxes[1]);
       });
 
       await waitFor(async () => {
