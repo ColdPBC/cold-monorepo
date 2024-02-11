ccd14700de13a3660aefbe401a0dfac1182e35ce
Troy Morvant <troy.morvant@gmail.com>
2024-02-10 18:01:25 -0600

    fix: add survey id key

--- libs/nest/src/lib/aws/secrets/secrets.service.ts
+++ libs/nest/src/lib/aws/secrets/secrets.service.ts
@@ -1,31 +1,59 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { map } from 'lodash';
+import { get, map } from 'lodash';
 import { SecretsManager } from 'aws-sdk';
 import { GetSecretValueResponse } from 'aws-sdk/clients/secretsmanager';
 import { BaseWorker } from '../../worker';
-import { ConfigService } from '@nestjs/config';
 import { ConfigurationModule } from '../../configuration';
 
 @Injectable()
 export class SecretsService extends BaseWorker implements OnModuleInit {
   client: SecretsManager;
-  config: ConfigService;
 
   constructor() {
     super(SecretsService.name);
-    this.config = new ConfigService();
+
     this.client = new SecretsManager({
-      region: this.config.get('AWS_REGION', 'us-east-1'),
+      region: get('AWS_REGION', 'us-east-1'),
     });
   }
 
   override async onModuleInit() {
-    this.logger.info('SecretsService initialized');
     this.client = new SecretsManager(await ConfigurationModule.getAWSCredentials());
+    this.logger.info('SecretsService initialized');
+  }
+
+  async getServiceSecrets(serviceName: string): Promise<any> {
+    try {
+      const parts = serviceName.split('-');
+      const type = parts.length > 2 ? parts[1] : 'core';
+      const project = parts.length > 2 ? parts[2] : parts[1];
+
+      const secrets = await this.getSecrets(`${type}/${project}`);
+
+      return secrets;
+    } catch (e: any) {
+      this.logger.error(e.message, e);
+      return {};
+    }
+  }
+
+  async getRootSecrets(serviceName: string): Promise<any> {
+    try {
+      const parts = serviceName.split('-');
+      const type = parts.length > 2 ? parts[1] : 'core';
+
+      const secrets = await this.getSecrets(type);
+
+      return secrets;
+    } catch (e: any) {
+      this.logger.error(e.message, e);
+
+      throw e;
+    }
   }
 
   async getSecrets(name: string): Promise<any> {
-    const secretName = `${this.config.getOrThrow('NODE_ENV')}/${name}`;
+    const secretName = `${get(process.env, 'NODE_ENV', 'development')}/${name}`;
     try {
       const result: GetSecretValueResponse = await this.client.getSecretValue({ SecretId: secretName }).promise();
       let secret: any = {};
@@ -42,7 +70,7 @@ export class SecretsService extends BaseWorker implements OnModuleInit {
       return secret;
     } catch (err: any) {
       this.logger.error(`${err.message}: ${secretName}`, err);
-      //throw err;
+      throw err;
     }
   }
 }
