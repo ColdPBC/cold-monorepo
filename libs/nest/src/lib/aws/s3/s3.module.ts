2e0fa3ae7e4c42c59c04cb6814a7fe131040e94a
Troy Morvant <troy.morvant@gmail.com>
2024-02-19 13:39:10 -0600

    chore: update AWS to V3

--- libs/nest/src/lib/aws/s3/s3.module.ts
+++ libs/nest/src/lib/aws/s3/s3.module.ts
@@ -1,49 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { IAuthenticatedUser } from '@coldpbc/nest';
-import { MulterModule } from '@nestjs/platform-express';
-import multerS3 from 'multer-s3';
+import { ConfigurationModule } from '@coldpbc/nest';
 import { S3Service } from './s3.service';
-import * as crypto from 'crypto';
 
 @Module({})
 export class S3Module {
-  static async forRootAsync(bucket: string) {
+  static async forRootAsync(bucket: string, secrets: any) {
     const config = new ConfigService();
     return {
       module: S3Module,
-      imports: [
-        MulterModule.registerAsync({
-          useFactory: () => ({
-            storage: multerS3({
-              s3: S3Service.getS3Client(),
-              bucket: bucket,
-              contentType: multerS3.AUTO_CONTENT_TYPE,
-              key: function (req, file, cb) {
-                const user = req['user'] as IAuthenticatedUser;
-                const hash = crypto.createHash('sha1');
-                hash.setEncoding('hex');
-
-                file.stream.on('end', function () {
-                  hash.end();
-                  console.log(hash.read()); // the desired sha1sum
-                });
-
-                // read all file and pipe it (write it) to the hash object
-                file.stream.pipe(hash);
-                console.log(hash);
-
-                const orgId = req['orgId'];
-
-                // Adjust this based on your actual user object structure
-                cb(null, `${config.getOrThrow('NODE_ENV')}/${orgId}/${user.coldclimate_claims.email}/${file.originalname}`);
-              },
-            }),
-          }),
-        }),
-      ],
-      providers: [S3Service],
-      exports: [S3Service],
+      imports: [await ConfigurationModule.forRootAsync(config)],
+      providers: [S3Service, { provide: 'SECRETS', useValue: secrets }],
+      exports: [S3Service, 'SECRETS'],
     };
   }
 }
--- libs/nest/src/lib/aws/s3/s3.service.ts
+++ libs/nest/src/lib/aws/s3/s3.service.ts
@@ -1,50 +1,24 @@
 // s3.service.ts
 import { Injectable } from '@nestjs/common';
-import * as AWS from 'aws-sdk';
 import { ConfigService } from '@nestjs/config';
-import { S3Client } from '@aws-sdk/client-s3';
-import multerS3 from 'multer-s3';
 import { BaseWorker } from '../../worker';
 import { IAuthenticatedUser } from '../../primitives';
 import crypto from 'crypto';
 import stream from 'stream';
-import { ConfigurationModule } from '../../configuration';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { S3ConfigurationService } from '../../configuration';
 
 @Injectable()
 export class S3Service extends BaseWorker {
-  client: AWS.S3;
+  client: S3Client;
 
-  constructor(private readonly config: ConfigService) {
+  constructor(private readonly config: ConfigService, private readonly s3Config: S3ConfigurationService) {
     super(S3Service.name);
-    this.client = new AWS.S3({
-      credentials: {
-        accessKeyId: config.getOrThrow('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: config.getOrThrow('AWS_SECRET_ACCESS_KEY'),
-      },
-      region: config.getOrThrow('AWS_REGION', 'us-east-1'),
-    });
+    this.client = new S3Client(s3Config.getAWSCredentials());
   }
 
-  static async getS3Config(): Promise<{
-    credentials: { accessKeyId: string; secretAccessKey: string };
-    region: string;
-  }> {
-    //const config = new ConfigService();
-    return await ConfigurationModule.getAWSCredentials();
-    /*return {
-      credentials: {
-        accessKeyId: config.getOrThrow('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: config.getOrThrow('AWS_SECRET_ACCESS_KEY'),
-      },
-      region: config.get('AWS_REGION', 'us-east-1'),
-    };*/
-  }
-
-  static getS3Client() {
-    return new S3Client(S3Service.getS3Config());
-  }
-
-  static async calculateChecksum(file: Express.Multer.File): Promise<string> {
+  static async calculateChecksum(file): Promise<string> {
     const hash = crypto.createHash('md5');
     // Create a readable stream from the buffer
     const readStream = new stream.Readable();
@@ -65,46 +39,34 @@ export class S3Service extends BaseWorker {
     return hash.digest('hex');
   }
 
-  static getMulterS3Config() {
-    const config = new ConfigService();
-    return {
-      s3: S3Service.getS3Client(),
-      bucket: 'cold-api-uploaded-files',
-      contentType: multerS3.AUTO_CONTENT_TYPE,
-      metadata: async (req, file, cb) => {
-        const hash = await S3Service.calculateChecksum(file);
-        console.log(`${file.filename} Checksum: ${hash}`);
-        // Add custom metadata, such as the calculated hash
-        cb(null, { md5Hash: hash });
-      },
-      key: function (req, file, cb) {
-        const user = req['user'] as IAuthenticatedUser;
-        const orgId = req['orgId'];
-
-        // Adjust this based on your actual user object structure
-        cb(null, `${config.getOrThrow('NODE_ENV')}/${orgId}/${user.coldclimate_claims.email}/${file.originalname}`);
-      },
-    };
+  async getMD5Hash(file): Promise<string> {
+    return S3Service.calculateChecksum(file);
   }
 
-  async uploadStreamToS3(user: IAuthenticatedUser, org_id: string, file: Express.MulterS3.File): Promise<AWS.S3.ManagedUpload.SendData> {
+  async uploadStreamToS3(user: IAuthenticatedUser, org_id: string, file) {
     const key = `${this.details.env}/${org_id}/${file.originalname}`;
-
-    const params: AWS.S3.Types.PutObjectRequest = {
-      Bucket: `${this.details.service}`,
+    const bucket = 'cold-api-uploaded-files';
+    const putCommand: PutObjectCommand = new PutObjectCommand({
+      Bucket: bucket,
       Key: key,
-      Body: this.fs.createReadStream(`./uploads/${file.filename}`),
-    };
+      Body: file.buffer,
+    });
 
-    const uploaded = await this.client.upload(params).promise();
-    this.logger.info(`Uploaded ${uploaded.Key} to S3`, uploaded);
+    const uploaded = await this.client.send(putCommand);
 
-    return uploaded;
+    this.logger.info(`Uploaded ${key} to S3`, {
+      user: user.coldclimate_claims,
+      key,
+      bucket,
+      version: uploaded.VersionId,
+    });
+
+    return { file, key, uploaded, bucket };
   }
 
   async getObject(user: IAuthenticatedUser, bucket: string, key: string) {
     try {
-      const s3 = new AWS.S3({
+      const s3 = new S3Client({
         credentials: {
           accessKeyId: this.config.getOrThrow('AWS_ACCESS_KEY_ID'),
           secretAccessKey: this.config.getOrThrow('AWS_SECRET_ACCESS_KEY'),
@@ -112,12 +74,12 @@ export class S3Service extends BaseWorker {
         region: this.config.get('AWS_REGION', 'us-east-1'),
       });
 
-      const params: AWS.S3.Types.GetObjectRequest = {
+      const params = new GetObjectCommand({
         Bucket: bucket,
         Key: key,
-      };
+      });
 
-      const response = await s3.getObject(params).promise();
+      const response = await s3.send(params);
 
       console.log(response.ChecksumSHA256);
 
@@ -128,9 +90,10 @@ export class S3Service extends BaseWorker {
     }
   }
 
-  getSignedURL(user: IAuthenticatedUser, bucket: string, key: string): string {
+  async getSignedURL(user: IAuthenticatedUser, bucket: string, key: string): Promise<string> {
     this.logger.info(`Generating signed URL for ${key} in bucket ${bucket}`);
-    const url = this.client.getSignedUrl('getObject', { Bucket: bucket, Key: key });
+    const command = new PutObjectCommand({ Bucket: bucket, Key: key });
+    const url = await getSignedUrl(this.client, command, { expiresIn: 3600 });
 
     this.logger.info(`${user.coldclimate_claims.email} generated a signed URL for file: ${key}`, { url, user });
     return url;
