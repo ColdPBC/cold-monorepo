18b074ca2345bfec2d85adfbd2415cfe5c7a60db
Troy Morvant <troy.morvant@gmail.com>
2024-03-01 13:49:28 -0600

    fix: survey dependency bug

--- apps/cold-api/src/platform/modules/resources/surveys/surveys.module.ts
+++ apps/cold-api/src/platform/modules/resources/surveys/surveys.module.ts
@@ -5,10 +5,11 @@ import { SurveysController } from './surveys.controller';
 import { SurveysService } from './surveys.service';
 import { SurveysRabbitService } from './surveys.rabbit';
 import { ScoringModule } from './scoring/scoring.module';
+import { SurveyFilterService } from './filter/survey.filter.service';
 
 @Module({
-  controllers: [SurveysController],
-  providers: [SurveysService, JwtService, JwtStrategy, SurveysRabbitService],
+  controllers: [SurveysController, ScoringModule],
+  providers: [SurveysService, JwtService, JwtStrategy, SurveysRabbitService, SurveyFilterService],
   exports: [SurveysService, JwtService, JwtStrategy, SurveysRabbitService],
   imports: [ScoringModule],
 })
