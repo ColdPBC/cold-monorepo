4b04ad40759803f21554b4e531804a43429161f6
Troy Morvant <troy.morvant@gmail.com>
2024-03-05 20:32:21 -0600

    fix: broken QuestionAnswered count

--- apps/cold-api/src/platform/modules/resources/surveys/filter/survey.filter.service.ts
+++ apps/cold-api/src/platform/modules/resources/surveys/filter/survey.filter.service.ts
@@ -122,9 +122,7 @@ export class SurveyFilterService extends BaseWorker {
         '        }));\n' +
         '                \n' +
         '        $totalQuestionCount := function(){$count($keys(definition.sections.*.follow_up))};\n' +
-        '        $totalQuestionAnswered := $totalQuestionAnswered := function() {$sum($map(definition.sections.*.follow_up.*, function($v, $k, $o) { $exists($v.value) ? 1 : 0 })) };\n' +
-        '        }\n' +
-        '        ));\n' +
+        '        $totalQuestionAnswered := function() { $sum($map(definition.sections.*.follow_up.*, function($v, $k, $o) { $exists($v.value) ? 1 : 0 })) };\n' +
         '\n' +
         '        $totalMaxScore := function(){$sum(definition.sections.*.follow_up.*.max_score)}; \n' +
         '        \n' +
