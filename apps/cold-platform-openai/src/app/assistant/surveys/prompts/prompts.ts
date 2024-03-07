b5768d061b0ce08de623c73e0f6469280928d4ce
Troy Morvant <troy.morvant@gmail.com>
2024-03-07 10:32:10 -0600

    fix: tweak prompts

--- apps/cold-platform-openai/src/app/assistant/surveys/prompts/prompts.ts
+++ apps/cold-platform-openai/src/app/assistant/surveys/prompts/prompts.ts
@@ -20,12 +20,12 @@ export class Prompts extends BaseWorker {
         additional_context = `IF you are able to answer the question, Please provide your answer in a paragraph form limited to 100 words or less.  If you do not have enough information to answer, do not include an answer. `;
         break;
       }
-      case 'select': {
-        additional_context = `Please respond to the question in the 'prompt' property, and since the 'component' is 'select', you are to limit your response to one topic that you judge to be the most relevant to the question it MUST match the selected values exactly. If you are able to provide a precise answer, then format it as a JSON string array. If you do not have enough information to answer, do not include an answer.`;
+      case 'multi_text': {
+        additional_context = `If you are able to provide a precise answer, then format it as a JSON string array.  If you do not have enough information to answer, do not include an answer. `;
         break;
       }
-      case 'table': {
-        additional_context = `Please respond to the question in the 'prompt' property. Your answer in a paragraph form limited to 100 words or less.  If you do not have enough information to answer, do not include an answer. `;
+      case 'select': {
+        additional_context = `Please respond to the question in the 'prompt' property, and since the 'component' is 'select', you are to limit your response to one topic that you judge to be the most relevant to the question it MUST match the selected values exactly. If you are able to provide a precise answer, then format it as a JSON string array. If you do not have enough information to answer, do not include an answer.`;
         break;
       }
       default: {
@@ -38,27 +38,11 @@ export class Prompts extends BaseWorker {
   }
 
   getBasePrompt(customer: string) {
-    /*const base: string =
-      'The user will provide you with a JSON object which contains the following information: \n' +
-      '- idx: The index of the question \n' +
-      '- prompt: The question to be answered \n' +
-      '- component: used to determine how to structure your answer \n' +
-      '- options: a list of options to be used to answer the question \n' +
-      '- tooltip: additional instructions for answering the question \n' +
-      '- basedOn: a key indicating which other question to which this one is related \n' +
-      '- additional_context: an object that may also contain a prompt and tooltip that should answered in addition to the main prompt \n' +
-      'Please use the following to understand how to interpret the component field: \n' +
-      '1. textarea: unless the prompt says otherwise, provide your answer in a paragraph form limited to 100 words or less \n' +
-      '2. yes_no: only answer with yes or no\n' +
-      '3. multi_select: select all the applicable answers provided in the options property \n' +
-      '4. single_select: select only one answer from the options property \n';*/
     const base =
       `You are an AI sustainability expert. You help ${customer} to understand ` +
-      ' their impact on the environment and what tasks must be done to meet a given set of compliance requirements. \n' +
-      ' If you have a completed B Corp Impact Assessment for the company, It has a set of questions, eligible \n' +
-      ' answers for those questions, and the answers that the company gave. You are tasked \n' +
+      ' their impact on the environment and what tasks must be done to meet a given set of compliance requirements. You are tasked \n' +
       ' with helping this company understand if they can answer other sustainability-related questions based on \n' +
-      ' their existing answers, otherwise use whatever data you have to attempt to answer the questions. \n' +
+      ' data found in the files provided, otherwise use whatever public data you have to attempt to answer the questions. \n' +
       ' The user will provide a JSON formatted "question" object that can include the following properties: \n' +
       '  - "prompt": The question to be answered \n' +
       '  - "component": used to determine how to structure your answer \n' +
