7e1d97863241a442c6616aba6e3d90ee27321625
Troy Morvant <troy.morvant@gmail.com>
2024-03-12 10:44:25 -0500

    feat: make component prompts configurable via darkly

--- apps/cold-platform-openai/src/app/assistant/surveys/prompts/prompts.service.ts
+++ apps/cold-platform-openai/src/app/assistant/surveys/prompts/prompts.service.ts
@@ -5,27 +5,47 @@ export class Prompts extends BaseWorker {
     super(Prompts.name);
   }
 
-  getComponentPrompt(item: any) {
-    let additional_context = '';
-    switch (item) {
+  async getComponentPrompt(key: string, item: any) {
+    let component_prompt = '';
+    switch (item.component) {
       case 'yes_no': {
-        additional_context = `If you are able to answer the question, Please answer with a boolean value true or false.  If you do not have enough information to answer, do not include an answer. `;
+        component_prompt = await this.darkly.getJSONFlag('dynamic-yes-no-component-prompt', {
+          kind: 'compliance-survey-prompt',
+          key: item.component,
+          name: key,
+        });
         break;
       }
       case 'multi_select': {
-        additional_context = `Please provide an answer to the question contained in the 'prompt' property which must conform to the following rules: limit your answer to only the values provided in the 'options' property in the provided JSON.  You may select as many values from the "options" property as are applicable however they MUST match the selected values exactly. If you are able to provide a precise answer, then format it as a JSON string array. Assume these 'options' cover all possible options and format your selections as a JSON string array. If you do not have enough information to answer, do not include an answer. `;
+        component_prompt = await this.darkly.getJSONFlag('dynamic-multi-select-component-prompt', {
+          kind: 'compliance-survey-prompt',
+          key: item.component,
+          name: key,
+        });
         break;
       }
       case 'textarea': {
-        additional_context = `IF you are able to answer the question, Please provide your answer in a paragraph form limited to 100 words or less.  If you do not have enough information to answer, do not include an answer. `;
+        component_prompt = component_prompt = await this.darkly.getJSONFlag('dynamic-text-area-component-prompt', {
+          kind: 'compliance-survey-prompt',
+          key: item.component,
+          name: key,
+        });
         break;
       }
       case 'multi_text': {
-        additional_context = `If you are able to provide a precise answer, then format it as a JSON string array.  If you do not have enough information to answer, do not include an answer. `;
+        component_prompt = component_prompt = await this.darkly.getJSONFlag('dynamic-multi-text-component-prompt', {
+          kind: 'compliance-survey-prompt',
+          key: item.component,
+          name: key,
+        });
         break;
       }
       case 'select': {
-        additional_context = `Please respond to the question in the 'prompt' property, and since the 'component' is 'select', you are to limit your response to one topic that you judge to be the most relevant to the question it MUST match the selected values exactly. If you are able to provide a precise answer, then format it as a JSON string array. If you do not have enough information to answer, do not include an answer.`;
+        component_prompt = component_prompt = await this.darkly.getJSONFlag('dynamic-select-component-prompt', {
+          kind: 'compliance-survey-prompt',
+          key: item.component,
+          name: key,
+        });
         break;
       }
       default: {
@@ -34,7 +54,7 @@ export class Prompts extends BaseWorker {
       }
     }
 
-    return additional_context;
+    return component_prompt;
   }
 
   getBasePrompt(customer: string) {
