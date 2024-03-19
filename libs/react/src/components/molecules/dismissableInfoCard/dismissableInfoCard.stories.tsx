a71be341acc7a3ff9937f2b6dfe22a74fc60ed90
Qaalib Farah <qaalib1598@gmail.com>
2024-03-19 10:57:51 -0500

    feat: update dismissible to use org partitioned local storage

--- libs/react/src/components/molecules/dismissableInfoCard/dismissableInfoCard.stories.tsx
+++ libs/react/src/components/molecules/dismissableInfoCard/dismissableInfoCard.stories.tsx
@@ -1,6 +1,7 @@
 import { withKnobs } from '@storybook/addon-knobs';
 import { Meta, StoryObj } from '@storybook/react';
 import { DismissableInfoCard } from './dismissableInfoCard';
+import { StoryMockProvider } from '@coldpbc/mocks';
 
 const meta: Meta<typeof DismissableInfoCard> = {
   title: 'Molecules/DismissableInfoCard',
@@ -14,12 +15,12 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   render: () => (
+    <StoryMockProvider>
       <DismissableInfoCard
         onDismiss={() => {}}
-        text='Your footprint is a snapshot of the greenhouse gases your company emitted over a specific timeframe. It is measured in tons of carbon dioxide equivalent, expressed as tCO2e.'
+        text="Your footprint is a snapshot of the greenhouse gases your company emitted over a specific timeframe. It is measured in tons of carbon dioxide equivalent, expressed as tCO2e."
         dismissKey="story_key"
       />
-  )
+    </StoryMockProvider>
+  ),
 };
-
-
