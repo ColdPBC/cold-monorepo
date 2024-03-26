59715435a44d9dae33627f11d080dfb843b8a82b
Troy Morvant <troy.morvant@gmail.com>
2024-03-25 20:42:21 -0500

    test: tools.spec.ts

--- apps/cold-platform-openai/src/assistant/tools/tools.spec.ts
+++ apps/cold-platform-openai/src/assistant/tools/tools.spec.ts
@@ -1,7 +1,8 @@
 import { Tools } from './tools';
+import { DarklyService } from '@coldpbc/nest';
 
 describe('Tools', () => {
   it('should be defined', () => {
-    expect(new Tools()).toBeDefined();
+    expect(new Tools({} as DarklyService)).toBeDefined();
   });
 });
