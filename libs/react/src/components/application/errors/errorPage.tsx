72df5e101fdca3edf5e00531aeee0db8670b2605
Qaalib Farah <qaalib1598@gmail.com>
2024-03-26 11:59:50 -0500

    feat: update error page to have better message and re login button

--- libs/react/src/components/application/errors/errorPage.tsx
+++ libs/react/src/components/application/errors/errorPage.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
+import { useAuth0Wrapper } from '@coldpbc/hooks';
+import { ButtonTypes } from '@coldpbc/enums';
+import { BaseButton } from '@coldpbc/components';
 
 export type ErrorPageProps = {
   error?: string;
 };
 
 export const ErrorPage = ({ error }: ErrorPageProps) => {
+  const { logout } = useAuth0Wrapper();
+
+  const handleLogout = async () => {
+    await logout();
+  };
+
   return (
-    <div
-      className={
-        'fixed inset-0 h-screen w-screen bg-bgc-main flex items-center justify-center'
-      }
-    >
-      <div className={'text-tc-primary text-h2 w-1/2'}>
-        {error ? (
-          error
-        ) : (
-          <>
-            An error occurred accessing your account. Please contact us at{' '}
-            <a href="mailto: support@coldclimate.com">
-              support@coldclimate.com
-            </a>
-            .
-          </>
-        )}
+    <div className={'fixed inset-0 h-screen w-screen bg-bgc-main flex flex-col items-center justify-center'}>
+      <div className={'text-tc-primary text-h2 w-1/2 flex flex-col items-center justify-center space-y-10'}>
+        <div>{error}</div>
+        <div>
+          An error occurred accessing your account. Please contact us at <a href="mailto: support@coldclimate.com">support@coldclimate.com</a>.
+        </div>
+        <BaseButton onClick={handleLogout} label={'Re Login'} variant={ButtonTypes.secondary} className={'w-auto'} />
       </div>
     </div>
   );
