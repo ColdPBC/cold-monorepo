2aef043b8143b6f10665344b803f3fec467b0e53
Troy Morvant <troy.morvant@gmail.com>
2024-04-29 13:14:46 -0500

    fix: some minor compliance model changes

--- apps/cold-api/src/platform/modules/resources/compliance_definitions/compliance_definition_schema.ts
+++ apps/cold-api/src/platform/modules/resources/compliance_definitions/compliance_definition_schema.ts
@@ -4,6 +4,11 @@ export const ComplianceDefinitionSchema = z.object({
   id: z.string(),
   name: z.string(),
   title: z.string(),
+  image_url: z.string().url().nullable(),
+  order: z.number().nullable(),
+  metadata: z.any().nullable(),
+  visible: z.boolean(),
+  version: z.number().nullable(),
   logo_url: z.string().url(),
   surveys: z.array(z.string()),
 });
