c7966dc73b7592487fd77dc60e0eedc34e8c39c5
Troy Morvant <troy.morvant@gmail.com>
2024-04-30 15:18:08 -0500

    included in cold-770

--- libs/nest/prisma/migrations/20240429232448_add_corresponding_question_to_compliance_questions/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `coresponding_question` to the `compliance_questions` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "compliance_questions" ADD COLUMN     "coresponding_question" TEXT NOT NULL;
