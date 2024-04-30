6adea2f6e0499af5a2de0804f91e9f29a706eb72
Troy Morvant <troy.morvant@gmail.com>
2024-04-30 15:19:07 -0500

    included in cold-770

--- libs/nest/prisma/migrations/20240429235207_set_optional_question_properties/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "compliance_questions" ALTER COLUMN "tooltip" DROP NOT NULL,
-ALTER COLUMN "placeholder" DROP NOT NULL,
-ALTER COLUMN "options" DROP NOT NULL,
-ALTER COLUMN "question_summary" DROP NOT NULL,
-ALTER COLUMN "additional_context" DROP NOT NULL,
-ALTER COLUMN "coresponding_question" DROP NOT NULL;
