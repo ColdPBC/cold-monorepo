680875c04a6d727074c6e868a5586ae3fd62c0ad
Troy Morvant <troy.morvant@gmail.com>
2024-04-30 15:18:58 -0500

    included in cold-770

--- libs/nest/prisma/migrations/20240429235029_make_dependency_expression_optional/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "compliance_questions" ALTER COLUMN "dependency_expression" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "compliance_sections" ALTER COLUMN "dependency_expression" DROP NOT NULL;
