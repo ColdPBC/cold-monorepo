e553f0f74eced6c4ba7e19f7b80e3c36287d7c9e
Troy Morvant <troy.morvant@gmail.com>
2024-04-30 15:17:46 -0500

    included in cold-770

--- libs/nest/prisma/migrations/20240429230133_add_comp_sec_key_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[compliance_section_id,key]` on the table `compliance_questions` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "compliance_questions_compliance_section_id_key_key" ON "compliance_questions"("compliance_section_id", "key");
