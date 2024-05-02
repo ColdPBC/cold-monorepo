27fc40a2b93f8654739687ecc6e6206bd3417fb9
Troy Morvant <troy.morvant@gmail.com>
2024-05-02 18:01:58 -0500

    fix: add ComplianceDefinition entity in compliance_definitions module

---
+++ apps/cold-api/src/platform/modules/resources/compliance_definitions/entities/compliance_definition.entity.ts
@@ -0,0 +1,39 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { ComplianceSectionGroup } from '../../compliance_section_groups/entities/compliance_section_group.entity';
+import { OrganizationCompliance } from '../../organizations/organization_compliance/entities/organization_compliance.entity';
+
+@Entity('compliance_definitions')
+export class ComplianceDefinition {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column('int', { nullable: true })
+  order: number;
+
+  @Column()
+  title: string;
+
+  @Column({ nullable: true })
+  image_url: string;
+
+  @Column({ nullable: true })
+  logo_url: string;
+
+  @Column('json')
+  surveys: object;
+
+  @Column('json', { nullable: true })
+  metadata: object;
+
+  @Column('boolean', { default: false })
+  visible: boolean;
+
+  @OneToMany(() => ComplianceSectionGroup, complianceSectionGroups => complianceSectionGroups.compliance_definition)
+  compliance_section_groups: ComplianceSectionGroup[];
+
+  @OneToMany(() => OrganizationCompliance, organizationCompliances => organizationCompliances.compliance_definition)
+  organization_compliances: OrganizationCompliance[];
+}
