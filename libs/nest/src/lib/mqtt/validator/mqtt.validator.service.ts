b5270e24d19cfd7bf23cae41d2f04e87aabd34fd
Troy Morvant <troy.morvant@gmail.com>
2024-05-08 13:54:18 -0500

    feat: Extend MQTT validator schemas and create new compliance schemas for various payload types
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- libs/nest/src/lib/mqtt/validator/mqtt.validator.service.ts
+++ libs/nest/src/lib/mqtt/validator/mqtt.validator.service.ts
@@ -1,23 +1,45 @@
 import { z } from 'zod';
 
-export const MqttTargetSchema = z.enum(['public', 'cold', 'ui']);
-export const MqttActionSchema = z.enum(['create', 'update', 'delete']);
+export const MqttTargetSchema = z.enum(['public', 'cold', 'ui', 'replyTo']);
+export const MqttActionSchema = z.enum(['get', 'create', 'update', 'delete']);
 export const MqttStatusSchema = z.enum(['complete', 'failed', 'active', 'queued', 'delayed', 'stalled']);
 
 export const MqttBasePayloadSchema = z
   .object({
-    swr_key: z.string(),
     action: MqttActionSchema,
     status: MqttStatusSchema,
     data: z.any().optional(),
   })
   .strip();
 
+export const MqttSocketAPIPayloadSchema = MqttBasePayloadSchema.extend({
+  reply_to: z.string().optional(),
+  compliance_set_name: z.string(),
+  user: z.object({
+    coldclimate_claims: z.object({
+      email: z.string().email(),
+      roles: z.array(z.string()),
+    }),
+  }),
+  token: z.string(),
+  resource: z.string().optional(),
+  method: z.enum(['get', 'post', 'put', 'delete', 'patch']).optional(),
+  org_id: z.string().startsWith('org_'),
+}).strip();
+
 export const MqttUIPayloadSchema = MqttBasePayloadSchema.extend({
+  swr_key: z.string(),
   org_id: z.string(),
   user: z.union([z.string().email(), z.record(z.unknown())]),
 }).strip();
 
+export const MqttAPIComplianceSectionPayloadSchema = MqttSocketAPIPayloadSchema.extend({
+  compliance_section_group_id: z.string().startsWith('csg_'),
+  compliance_section_id: z.string().startsWith('cs_').optional(),
+});
+
+export type MqttAPIComplianceSectionPayload = z.infer<typeof MqttAPIComplianceSectionPayloadSchema>;
+export type MqttSocketAPIPayload = z.infer<typeof MqttSocketAPIPayloadSchema>;
 export type MqttTarget = z.infer<typeof MqttTargetSchema>;
 export type MqttSystemPayload = z.infer<typeof MqttBasePayloadSchema>;
 export type MqttBasePayload = z.infer<typeof MqttBasePayloadSchema>;
