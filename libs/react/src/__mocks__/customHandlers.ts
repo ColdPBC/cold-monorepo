7b3c60f5b7494e49d4313642d8e027e847f5dd27
Qaalib Farah <qaalib1598@gmail.com>
2024-05-20 15:51:02 -0500

    chore: remove console logging

--- libs/react/src/__mocks__/customHandlers.ts
+++ libs/react/src/__mocks__/customHandlers.ts
@@ -245,8 +245,6 @@ export const getActionHandler = {
 export const getCompliancePageHandler = {
   default: [
     rest.get(getApiUrl('/compliance_definitions'), (req, res, ctx) => {
-      const list = getDefaultCompliancePageMock();
-      console.log('list', list);
       return res(ctx.json(getDefaultCompliancePageMock()));
     }),
     rest.get(getApiUrl('/compliance_definitions/organizations/:orgId'), (req, res, ctx) => {
