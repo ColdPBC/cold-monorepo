7fa0955d2afc29e3321b316c79d496755495e02d
Troy Morvant <troy.morvant@gmail.com>
2024-05-21 17:10:07 -0500

    feat: Add createAiResponses method to compliance-ai-responses.repository.ts and relocate file to new directory
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- libs/nest/src/lib/repositories/compliance/compliance-ai-responses/compliance-ai-responses.repository.ts
+++ libs/nest/src/lib/repositories/compliance/compliance-ai-responses/compliance-ai-responses.repository.ts
@@ -8,6 +8,21 @@ export class ComplianceAiResponsesRepository extends BaseWorker {
     super(ComplianceAiResponsesRepository.name);
   }
 
+  async createAiResponses(organization: { name: any; id: any; display_name: any }, compliance: { compliance_definition_name: any; id: any }, aiResponses: any, user: any) {
+    this.logger.info(`Creating ai_responses for ${organization.name}: ${compliance.compliance_definition_name}`, {
+      user,
+      ...compliance,
+      organization: { id: organization.id, name: organization.name, display_name: organization.display_name },
+    });
+
+    await this.prisma.organization_compliance_ai_responses.createMany({
+      data: aiResponses.map((aiResponse: any) => ({
+        organization_id: organization.id,
+        organization_compliance_id: compliance.id,
+        ...aiResponse,
+      })),
+    });
+  }
   async deleteAiResponses(organization: { name: any; id: any; display_name: any }, compliance: { compliance_definition_name: any; id: any }, user: any) {
     this.logger.info(`Clearing previous ai_responses for ${organization.name}: ${compliance.compliance_definition_name}`, {
       user,
