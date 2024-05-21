bbf65fc428a3686e129c178790e79d44ac8e9d17
Troy Morvant <troy.morvant@gmail.com>
2024-05-21 16:00:56 -0500

    refactor: change parameter in getSectionGroupList from org_id to compliance_definition_name
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-api/src/platform/modules/resources/organizations/organization_compliance/organization_compliance_responses/organizations_compliance_responses.rabbit.ts
+++ apps/cold-api/src/platform/modules/resources/organizations/organization_compliance/organization_compliance_responses/organizations_compliance_responses.rabbit.ts
@@ -17,9 +17,9 @@ export class OrganizationComplianceResponseRabbit extends BaseWorker {
   async processRPCMessages(msg): Promise<any> {
     const data = msg.data as any;
     const groups = await this.groupList.getSectionGroupList({
-      org_id: data.organization.id,
-      compliance_set_name: data.compliance_set,
+      compliance_definition_name: data.compliance_set,
     });
+
     this.mqtt.replyTo(`ui/${process.env.NODE_ENV}/${data.organization.id}/${data.compliance_set}`, groups);
 
     const sections = await this.repository.getFilteredQuestionList({
