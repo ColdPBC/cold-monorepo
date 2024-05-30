00bdc17e440dfe1cbb955a3ad040b0b9498a66b0
Troy Morvant <troy.morvant@gmail.com>
2024-05-30 13:35:53 -0500

    refactor: remove FacilitiesModule import from organization.integrations.module.ts
    - Removed the unused importing of FacilitiesModule from the organization.integrations.module.ts
    - Modified the @Module decorator's imports array to reflect this change
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-api/src/platform/modules/resources/organizations/integrations/organization.integrations.module.ts
+++ apps/cold-api/src/platform/modules/resources/organizations/integrations/organization.integrations.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrganizationIntegrationsController } from './organization.integrations.controller';
 import { OrganizationIntegrationsService } from './organization.integrations.service';
-import { FacilitiesModule } from '../facilities/facilities.module';
+
 import { OrganizationHelper } from '../helpers/organization.helper';
 
 @Module({
-  imports: [FacilitiesModule],
+  imports: [],
   providers: [OrganizationIntegrationsService, OrganizationHelper],
   controllers: [OrganizationIntegrationsController],
 })
