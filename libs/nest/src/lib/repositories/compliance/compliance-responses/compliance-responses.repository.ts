5e37c89a7d48489e13e548b85248bda320f440e7
Troy Morvant <troy.morvant@gmail.com>
2024-05-30 13:25:12 -0500

    feat: Add improvements to ComplianceResponsesRepository
    - Refactor 'saveComplianceResponse' method to 'upsertComplianceResponse' providing both creation and updates
    - Introduce error handling and logging for better exception control
    - Extend organizations and compliance_responses in the getComplianceResponses method to provide additional context
    - Create getComplianceResponseById method to retrieve a specific compliance response by its id
    - Improve deleteComplianceResponses by adding error logging and wrapping in a try..catch block
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- libs/nest/src/lib/repositories/compliance/compliance-responses/compliance-responses.repository.ts
+++ libs/nest/src/lib/repositories/compliance/compliance-responses/compliance-responses.repository.ts
@@ -1,127 +1,394 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { BaseWorker } from '../../../worker';
 import { PrismaService } from '../../../prisma';
-import { ComplianceAiResponsesRepository } from '../compliance-ai-responses';
-import { organization_compliance } from '@prisma/client';
-import { CreateComplianceReponsesPayload } from './compliance-reponses.types';
+import { organization_compliance, organization_compliance_ai_responses, organization_compliance_responses, organizations } from '@prisma/client';
 import { Cuid2Generator } from '../../../utility';
+import { IAuthenticatedUser } from '../../../primitives';
 
 @Injectable()
 export class ComplianceResponsesRepository extends BaseWorker {
-  constructor(readonly prisma: PrismaService, readonly cairr: ComplianceAiResponsesRepository) {
+  constructor(readonly prisma: PrismaService) {
     super(ComplianceResponsesRepository.name);
   }
 
-  async saveComplianceResponse({ organization, compliance, compliance_section_id, compliance_section_group_id, user, org_response, ai_response }: CreateComplianceReponsesPayload) {
-    this.logger.info(`Saving response for ${organization.name}: ${compliance.compliance_definition_name}`, {
-      user,
-      org_response,
-      ai_response,
-      ...compliance,
-      organization: { id: organization.id, name: organization.name, display_name: organization.display_name },
-    });
+  /**
+   * Create or Update a compliance response
+   * @param orgId
+   * @param compliance_definition_name
+   * @param compliance_section_id
+   * @param compliance_section_group_id
+   * @param compliance_question_id
+   * @param user
+   * @param user_response
+   * @param ai_response
+   */
+  async upsertComplianceResponse(
+    orgId: string,
+    compliance_definition_name: string,
+    compliance_section_group_id: string,
+    compliance_section_id: string,
+    compliance_question_id: string,
+    user: IAuthenticatedUser,
+    user_response?: organization_compliance_responses,
+    ai_response?: organization_compliance_ai_responses,
+  ) {
+    let organization, compliance;
 
-    let orgResponseEntity, aiResponseEntity;
+    try {
+      organization = await this.prisma.extended.organizations.findUnique({
+        where: { id: orgId },
+      });
+      if (!organization) {
+        throw new NotFoundException(`Organization ${orgId} not found`);
+      }
 
-    if (ai_response?.answer || ai_response?.justification) {
-      aiResponseEntity = await this.prisma.organization_compliance_ai_responses.upsert({
+      compliance = await this.prisma.extended.organization_compliance.findUnique({
         where: {
-          orgCompQuestId: {
-            compliance_question_id: ai_response.compliance_question_id,
-            organization_compliance_id: compliance.id,
+          orgIdCompNameKey: {
+            organization_id: orgId,
+            compliance_definition_name,
           },
         },
-        create: {
-          id: new Cuid2Generator('ocair').scopedId,
+      });
+
+      if (!compliance) {
+        throw new NotFoundException(`Organization Compliance Definition ${compliance_definition_name} not found for organization ${organization.name}`);
+      }
+
+      if (!user_response?.value && (!ai_response?.answer || !ai_response?.justification)) {
+        throw new BadRequestException(`No User or AI response provided for ${organization.name}: ${compliance.compliance_definition_name}, nothing to do`);
+      }
+
+      if (!ai_response?.compliance_question_id || (!user_response?.compliance_question_id && !compliance_question_id)) {
+        throw new BadRequestException(`No compliance question id provided for ${organization.name}: ${compliance.compliance_definition_name}`);
+      }
+
+      this.logger.info(`Saving response for ${organization.name}: ${compliance.compliance_definition_name}`, {
+        user,
+        user_response: user_response,
+        ai_response,
+        ...compliance,
+        organization: { id: organization.id, name: organization.name, display_name: organization.display_name },
+      });
+
+      let orgResponseEntity, aiResponseEntity;
+
+      if (ai_response?.answer || ai_response?.justification) {
+        aiResponseEntity = await this.prisma.extended.organization_compliance_ai_responses.upsert({
+          where: {
+            orgCompQuestId: {
+              compliance_question_id: ai_response.compliance_question_id,
+              organization_compliance_id: compliance.id,
+            },
+          },
+          create: {
+            id: new Cuid2Generator('ocair').scopedId,
+            organization_id: organization.id,
+            organization_compliance_id: compliance.id,
+            compliance_question_id: compliance_question_id || ai_response.compliance_question_id,
+            answer: ai_response.answer as any,
+            justification: ai_response.justification,
+            sources: ai_response.sources as any,
+            references: ai_response.references as any,
+            additional_context: ai_response.additional_context as any,
+          },
+          update: {
+            answer: ai_response.answer as any,
+            justification: ai_response.justification,
+            sources: ai_response.sources as any,
+            references: ai_response.references as any,
+            additional_context: ai_response.additional_context as any,
+          },
+        });
+      }
+
+      if (user_response?.value) {
+        orgResponseEntity = await this.prisma.extended.organization_compliance_responses.upsert({
+          where: {
+            orgCompQuestId: {
+              organization_compliance_id: compliance.id,
+              compliance_question_id: compliance_question_id || user_response.compliance_question_id,
+            },
+          },
+          create: {
+            id: new Cuid2Generator('ocr').scopedId,
+            organization_compliance_id: compliance.id,
+            compliance_question_id: user_response.compliance_question_id,
+            value: user_response.value,
+          },
+          update: {
+            value: user_response.value,
+          },
+        });
+      }
+
+      if (orgResponseEntity || aiResponseEntity) {
+        const crData = {
+          compliance_question_id: compliance_question_id || ai_response.compliance_question_id,
+          compliance_section_id: compliance_section_id,
+          compliance_section_group_id: compliance_section_group_id,
           organization_id: organization.id,
+          compliance_definition_name: compliance.compliance_definition_name,
           organization_compliance_id: compliance.id,
-          compliance_question_id: ai_response.compliance_question_id,
-          answer: ai_response.answer as any,
-          justification: ai_response.justification,
-          sources: ai_response.sources as any,
-          references: ai_response.references as any,
-          additional_context: ai_response.additional_context as any,
-        },
-        update: {
-          answer: ai_response.answer as any,
-          justification: ai_response.justification,
-          sources: ai_response.sources as any,
-          references: ai_response.references as any,
-          additional_context: ai_response.additional_context as any,
-        },
+          organization_compliance_ai_response_id: aiResponseEntity.id,
+          organization_compliance_response_id: orgResponseEntity?.id,
+        };
+        await this.prisma.extended.compliance_responses.upsert({
+          where: {
+            orgCompQuestId: {
+              organization_compliance_id: compliance.id,
+              compliance_question_id: compliance_question_id || ai_response.compliance_question_id,
+            },
+          },
+          create: crData,
+          update: crData,
+        });
+      }
+    } catch (error) {
+      this.logger.error(`Error saving response for ${organization.name}: ${compliance.compliance_definition_name}`, {
+        user,
+        user_response: user_response,
+        ai_response,
+        compliance,
+        organization,
+        error,
       });
+      throw error;
     }
-    if (org_response?.value && aiResponseEntity.id) {
-      orgResponseEntity = await this.prisma.organization_compliance_responses.upsert({
-        where: {
-          orgCompQuestId: {
-            organization_compliance_id: compliance.id,
-            compliance_question_id: org_response.compliance_question_id,
+  }
+
+  async getComplianceResponses(orgId: string, compliance_definition_name: string, user: IAuthenticatedUser) {
+    try {
+      const organization = await this.prisma.extended.organizations.findUnique({
+        where: { name: orgId },
+        select: {
+          id: true,
+          name: true,
+          display_name: true,
+          organization_compliance: {
+            where: { compliance_definition_name },
+            select: {
+              id: true,
+              question_bookmarks: {
+                select: {
+                  id: true,
+                  deleted: true,
+                },
+              },
+              notes: {
+                select: {
+                  id: true,
+                  note: true,
+                  deleted: true,
+                  files: true,
+                  links: true,
+                },
+              },
+              compliance_responses: {
+                where: {
+                  organization_id: orgId,
+                  compliance_definition_name,
+                },
+                select: {
+                  id: true,
+                  org_response: true,
+                  ai_response: {
+                    select: {
+                      id: true,
+                      answer: true,
+                      justification: true,
+                      sources: true,
+                      references: true,
+                      additional_context: true,
+                      deleted: true,
+                      files: true,
+                    },
+                  },
+                  compliance_section_group: {
+                    select: {
+                      id: true,
+                      title: true,
+                      order: true,
+                      deleted: true,
+                      compliance_sections: {
+                        select: {
+                          id: true,
+                          key: true,
+                          title: true,
+                          order: true,
+                          deleted: true,
+                          compliance_section_dependency_chains: true,
+                          compliance_questions: {
+                            select: {
+                              id: true,
+                              key: true,
+                              order: true,
+                              component: true,
+                              tooltip: true,
+                              placeholder: true,
+                              rubric: true,
+                              options: true,
+                              question_summary: true,
+                              coresponding_question: true,
+                              additional_context: true,
+                              deleted: true,
+                            },
+                          },
+                        },
+                      },
+                    },
+                  },
+                  deleted: true,
+                },
+              },
+              deleted: true,
+            },
           },
         },
-        create: {
-          id: new Cuid2Generator('ocr').scopedId,
-          organization_compliance_id: compliance.id,
-          compliance_question_id: org_response.compliance_question_id,
-          value: org_response.value,
-        },
-        update: {
-          value: org_response.value,
+      });
+
+      if (!organization) {
+        throw new NotFoundException(`Organization ${orgId} not found`);
+      }
+
+      return { ...organization };
+    } catch (error) {
+      this.logger.error(`Error getting responses for organization: ${orgId}: ${compliance_definition_name}`, {
+        user,
+        error,
+      });
+      throw error;
+    }
+  }
+
+  async getComplianceResponseById(orgId: string, compliance_definition_name: string, user: IAuthenticatedUser, id: number) {
+    try {
+      const organization = await this.prisma.extended.organizations.findUnique({
+        where: { id: orgId },
+        select: {
+          id: true,
+          name: true,
+          display_name: true,
+          organization_compliance: {
+            where: { compliance_definition_name },
+            select: {
+              id: true,
+              question_bookmarks: {
+                select: {
+                  id: true,
+                  deleted: true,
+                },
+              },
+              notes: {
+                select: {
+                  id: true,
+                  note: true,
+                  deleted: true,
+                  files: true,
+                  links: true,
+                },
+              },
+              compliance_responses: {
+                where: {
+                  id,
+                },
+                select: {
+                  id: true,
+                  org_response: true,
+                  ai_response: {
+                    select: {
+                      id: true,
+                      answer: true,
+                      justification: true,
+                      sources: true,
+                      references: true,
+                      additional_context: true,
+                      deleted: true,
+                      files: true,
+                    },
+                  },
+                  compliance_section_group: {
+                    select: {
+                      id: true,
+                      title: true,
+                      order: true,
+                      deleted: true,
+                      compliance_sections: {
+                        select: {
+                          id: true,
+                          key: true,
+                          title: true,
+                          order: true,
+                          deleted: true,
+                          compliance_section_dependency_chains: true,
+                          compliance_questions: {
+                            select: {
+                              id: true,
+                              key: true,
+                              order: true,
+                              component: true,
+                              tooltip: true,
+                              placeholder: true,
+                              rubric: true,
+                              options: true,
+                              question_summary: true,
+                              coresponding_question: true,
+                              additional_context: true,
+                              deleted: true,
+                            },
+                          },
+                        },
+                      },
+                    },
+                  },
+                  deleted: true,
+                },
+              },
+              deleted: true,
+            },
+          },
         },
       });
+
+      if (!organization) {
+        throw new NotFoundException(`Organization ${orgId} not found`);
+      }
+
+      return { ...organization };
+    } catch (error) {
+      this.logger.error(`Error getting responses for organization: ${orgId}: ${compliance_definition_name}`, {
+        user,
+        error,
+      });
+      throw error;
     }
-
-    await this.prisma.compliance_responses.upsert({
-      where: {
-        orgCompQuestId: {
-          organization_compliance_id: compliance.id,
-          compliance_question_id: ai_response.compliance_question_id,
-        },
-      },
-      create: {
-        compliance_question_id: ai_response.compliance_question_id,
-        compliance_section_id: compliance_section_id,
-        compliance_section_group_id: compliance_section_group_id,
-        organization_id: organization.id,
-        compliance_definition_name: compliance.compliance_definition_name,
-        organization_compliance_id: compliance.id,
-        organization_compliance_ai_response_id: aiResponseEntity.id,
-        organization_compliance_response_id: orgResponseEntity?.id,
-      },
-      update: {
-        compliance_question_id: ai_response.compliance_question_id,
-        compliance_section_id: compliance_section_id,
-        compliance_section_group_id: compliance_section_group_id,
-        organization_id: organization.id,
-        compliance_definition_name: compliance.compliance_definition_name,
-        organization_compliance_id: compliance.id,
-        organization_compliance_ai_response_id: aiResponseEntity.id,
-        organization_compliance_response_id: orgResponseEntity?.id,
-      },
-    });
   }
 
-  async getResponses(organization, compliance: organization_compliance) {
-    return this.prisma.compliance_responses.findMany({
-      where: { organization_compliance_id: compliance.id },
-    });
-  }
+  async deleteComplianceResponses(organization: organizations, compliance: organization_compliance, user, includeUserResponses = false) {
+    try {
+      this.logger.info(`Clearing previous responses for ${organization.name}: ${compliance.compliance_definition_name}`, {
+        user,
+        ...compliance,
+        organization: { id: organization.id, name: organization.name, display_name: organization.display_name },
+      });
 
-  async deleteComplianceResponses(organization, compliance: organization_compliance, user, includeUserResponses = false) {
-    this.logger.info(`Clearing previous responses for ${organization.name}: ${compliance.compliance_definition_name}`, {
-      user,
-      ...compliance,
-      organization: { id: organization.id, name: organization.name, display_name: organization.display_name },
-    });
-
-    await this.prisma.compliance_responses.deleteMany({
-      where: { organization_compliance_id: compliance.id },
-    });
-
-    if (includeUserResponses) {
-      await this.prisma.organization_compliance_responses.deleteMany({
+      await this.prisma.extended.compliance_responses.deleteMany({
         where: { organization_compliance_id: compliance.id },
       });
+
+      if (includeUserResponses) {
+        await this.prisma.extended.organization_compliance_responses.deleteMany({
+          where: { organization_compliance_id: compliance.id },
+        });
+      }
+    } catch (error) {
+      this.logger.error(`Error clearing previous responses for ${organization.name}: ${compliance.compliance_definition_name}`, {
+        user,
+        compliance,
+        organization,
+        error,
+      });
+      throw error;
     }
   }
 }
