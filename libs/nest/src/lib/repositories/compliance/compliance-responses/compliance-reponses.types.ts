2f6e81c89910687a2caa78cc5d7db8b685736038
Troy Morvant <troy.morvant@gmail.com>
2024-05-30 13:20:28 -0500

    refactor: Update compliance responses types in `compliance-reponses.types.ts`
    - Relocated the import location of `AuthenticatedUser` from `@coldpbc/nest` to `../../../primitives`
    - Renamed `org_response` to `user_response` in `CreateComplianceReponsesPayload`
    - Added `compliance_question_id` to `CreateComplianceReponsesPayload`
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- libs/nest/src/lib/repositories/compliance/compliance-responses/compliance-reponses.types.ts
+++ libs/nest/src/lib/repositories/compliance/compliance-responses/compliance-reponses.types.ts
@@ -1,12 +1,13 @@
-import { AuthenticatedUser } from '@coldpbc/nest';
 import { organization_compliance, organization_compliance_ai_responses, organization_compliance_responses, organizations } from '@prisma/client';
 
+import { AuthenticatedUser } from '../../../primitives';
 export type CreateComplianceReponsesPayload = {
   organization: organizations;
   compliance: organization_compliance;
   user: AuthenticatedUser;
-  org_response: organization_compliance_responses;
+  user_response: organization_compliance_responses;
   ai_response: organization_compliance_ai_responses;
+  compliance_question_id: string;
   compliance_section_id: string;
   compliance_section_group_id: string;
 };
