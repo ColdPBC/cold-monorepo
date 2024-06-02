3f5f5c4dce85d2b615dbafc373fe92d8f527f7ee
Troy Morvant <troy.morvant@gmail.com>
2024-06-02 11:25:49 -0500

    feat: Improve organization identifier handling in compliance AI response file repository
    - Add private function to handle organization id or name
    - Handle NotFoundException for missing organization in private function
    - Implement try/catch blocks to handle errors in main repository functions
    - Throw `UnprocessableEntityException` on error in repository functions
    - Refactor organization id retrieval in all repository functions using the new private function
    - Add error logging entries for all exceptions
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- libs/nest/src/lib/repositories/compliance/compliance-ai-response-files-repository/compliance-ai-response-files.repository.ts
+++ libs/nest/src/lib/repositories/compliance/compliance-ai-response-files-repository/compliance-ai-response-files.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
 import { BaseWorker } from '../../../worker';
 import { IAuthenticatedUser } from '../../../primitives';
 import { PrismaService } from '../../../prisma';
@@ -9,90 +9,150 @@ export class ComplianceAiResponseFilesRepository extends BaseWorker {
     super(ComplianceAiResponseFilesRepository.name);
   }
 
-  async createAiResponseFile(orgId: string, complianceName: string, aiResponseId: string, aiResponseFileData: any, user: IAuthenticatedUser) {
-    return this.prisma.extended.organization_compliance_ai_response_files.create({
-      data: {
-        organization_compliance_ai_response: {
-          connect: {
-            id: aiResponseId,
-          },
-        },
-        organization_compliance: {
-          connect: {
-            name: complianceName,
-            organization_id: orgId,
-          },
-        },
-        ...aiResponseFileData,
-      },
+  private async getOrg(idOrName) {
+    let where;
+
+    const byId = idOrName.startsWith('org_');
+    if (byId) {
+      where = { id: idOrName };
+    } else {
+      where = { name: idOrName };
+    }
+
+    const org = this.prisma.extended.organizations.findUnique({
+      where,
     });
+
+    if (!org) {
+      throw new NotFoundException({ ...where }, `Organization not found: ${idOrName}`);
+    }
+
+    return org;
+  }
+
+  async createAiResponseFile(orgId: string, complianceName: string, aiResponseId: string, aiResponseFileData: any, user: IAuthenticatedUser) {
+    try {
+      const org = await this.getOrg(orgId);
+
+      return this.prisma.extended.organization_compliance_ai_response_files.create({
+        data: {
+          organization_compliance_ai_response: {
+            connect: {
+              id: aiResponseId,
+            },
+          },
+          organization_compliance: {
+            connect: {
+              name: complianceName,
+              organization_id: org?.id,
+            },
+          },
+          ...aiResponseFileData,
+        },
+      });
+    } catch (error) {
+      this.logger.error(`Error creating file for compliance response ${aiResponseId}`, { orgId, complianceName, aiResponseId, user, error });
+
+      throw new UnprocessableEntityException({ orgId, complianceName, aiResponseId, user }, `Error creating file for compliance response ${aiResponseId}`);
+    }
   }
 
   async findAllAiResponseFiles(orgId: string, complianceName: string, aiResponseId: string, user: IAuthenticatedUser) {
-    const files = await this.prisma.extended.organization_compliance_ai_response_files.findMany({
-      where: {
-        organization_compliance_ai_response_id: aiResponseId,
-        organization_compliance: {
-          compliance_definition_name: complianceName,
-          organization_id: orgId,
+    try {
+      const org = await this.getOrg(orgId);
+
+      const files = await this.prisma.extended.organization_compliance_ai_response_files.findMany({
+        where: {
+          organization_compliance_ai_response_id: aiResponseId,
+          organization_compliance: {
+            compliance_definition_name: complianceName,
+            organization_id: org?.id,
+          },
         },
-      },
-    });
+      });
 
-    this.logger.info(`Found ${files.length} files for compliance response ${aiResponseId}`, { orgId, complianceName, aiResponseId, user });
+      this.logger.info(`Found ${files.length} files for compliance response ${aiResponseId}`, { orgId, complianceName, aiResponseId, user });
 
-    return files;
+      return files;
+    } catch (error) {
+      this.logger.error(`Error creating file for compliance response ${aiResponseId}`, { orgId, complianceName, aiResponseId, user, error });
+
+      throw new UnprocessableEntityException({ orgId, complianceName, aiResponseId, user }, `Error creating file for compliance response ${aiResponseId}`);
+    }
   }
 
   async findOneAiResponseFile(orgId: string, complianceName: string, aiResponseId: string, id: string, user: IAuthenticatedUser) {
-    const file = await this.prisma.extended.organization_compliance_ai_response_files.findUnique({
-      where: {
-        organization_compliance_ai_response_id: aiResponseId,
-        organization_compliance: {
-          compliance_definition_name: complianceName,
-          organization_id: orgId,
+    try {
+      const org = await this.getOrg(orgId);
+      const file = await this.prisma.extended.organization_compliance_ai_response_files.findUnique({
+        where: {
+          organization_compliance_ai_response_id: aiResponseId,
+          organization_compliance: {
+            compliance_definition_name: complianceName,
+            organization_id: org?.id,
+          },
+          id,
         },
-        id,
-      },
-    });
+      });
 
-    this.logger.info(`Found file ${id} for compliance response ${aiResponseId}`, { orgId, complianceName, aiResponseId, id, user });
+      this.logger.info(`Found file ${id} for compliance response ${aiResponseId}`, { orgId, complianceName, aiResponseId, id, user });
 
-    return file;
+      return file;
+    } catch (error) {
+      this.logger.error(`Error finding file ${id} for compliance response ${aiResponseId}`, { orgId, complianceName, aiResponseId, id, user, error });
+
+      throw new UnprocessableEntityException({ orgId, complianceName, aiResponseId, id, user }, `Error finding file ${id} for compliance response ${aiResponseId}`);
+    }
   }
 
   async updateAiResponseFile(orgId: string, complianceName: string, aiResponseId: string, id: string, aiResponseFileData: any, user: IAuthenticatedUser) {
-    const file = await this.prisma.extended.organization_compliance_ai_response_files.update({
-      where: {
-        organization_compliance_ai_response_id: aiResponseId,
-        organization_compliance: {
-          compliance_definition_name: complianceName,
-          organization_id: orgId,
+    try {
+      const org = await this.getOrg(orgId);
+
+      const file = await this.prisma.extended.organization_compliance_ai_response_files.update({
+        where: {
+          organization_compliance_ai_response_id: aiResponseId,
+          organization_compliance: {
+            compliance_definition_name: complianceName,
+            organization_id: org?.id,
+          },
+          id,
         },
-        id,
-      },
-      data: aiResponseFileData,
-    });
+        data: aiResponseFileData,
+      });
 
-    this.logger.info(`Updated file ${id} for compliance response ${aiResponseId}`, { orgId, complianceName, aiResponseId, id, user });
+      this.logger.info(`Updated file ${id} for compliance response ${aiResponseId}`, { orgId, complianceName, aiResponseId, id, user });
 
-    return file;
+      return file;
+    } catch (error) {
+      this.logger.error(`Error finding file ${id} for compliance response ${aiResponseId}`, { orgId, complianceName, aiResponseId, id, user, error });
+
+      throw new UnprocessableEntityException({ orgId, complianceName, aiResponseId, id, user }, `Error finding file ${id} for compliance response ${aiResponseId}`);
+    }
   }
 
   async removeAiResponseFile(orgId: string, complianceName: string, aiResponseId: string, id: string, user: IAuthenticatedUser) {
-    const file = await this.prisma.extended.organization_compliance_ai_response_files.delete({
-      where: {
-        organization_compliance_ai_response_id: aiResponseId,
-        organization_compliance: {
-          compliance_definition_name: complianceName,
-          organization_id: orgId,
+    try {
+      const org = await this.getOrg(orgId);
+
+      const file = await this.prisma.extended.organization_compliance_ai_response_files.delete({
+        where: {
+          organization_compliance_ai_response_id: aiResponseId,
+          organization_compliance: {
+            compliance_definition_name: complianceName,
+            organization_id: org?.id,
+          },
+          id,
         },
-        id,
-      },
-    });
+      });
 
-    this.logger.info(`Deleted file ${id} for compliance response ${aiResponseId}`, { orgId, complianceName, aiResponseId, id, user });
+      this.logger.info(`Deleted file ${id} for compliance response ${aiResponseId}`, { orgId, complianceName, aiResponseId, id, user });
 
-    return file;
+      return file;
+    } catch (error) {
+      this.logger.error(`Error finding file ${id} for compliance response ${aiResponseId}`, { orgId, complianceName, aiResponseId, id, user, error });
+
+      throw new UnprocessableEntityException({ orgId, complianceName, aiResponseId, id, user }, `Error finding file ${id} for compliance response ${aiResponseId}`);
+    }
   }
 }
