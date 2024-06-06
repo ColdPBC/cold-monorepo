c262bfb359a6402ad2bd095aefb71b30df159241
Troy Morvant <troy.morvant@gmail.com>
2024-06-06 10:55:45 -0500

    feat: Enhance OrganizationComplianceNotesController routing and functionality
    Changes include:
    
    - Rewriting the base routing schema with path parameters.
    - Adding findAllBySectionGroup, findAllBySection, and findOneByQuestion get routes with path parameters.
    - Updating the current CRUD operations while passing req as a parameter.
    - Overhauling the POST create function to handle path parameters and additional req data.
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-api/src/platform/modules/resources/organizations/organization_compliance/organization_compliance_notes/organization_compliance_notes.controller.ts
+++ apps/cold-api/src/platform/modules/resources/organizations/organization_compliance/organization_compliance_notes/organization_compliance_notes.controller.ts
@@ -1,33 +1,63 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Req } from '@nestjs/common';
 import { OrganizationComplianceNotesService } from './organization_compliance_notes.service';
 import { organization_compliance_notes } from '@prisma/client';
 
-@Controller('organization-compliance-notes')
+@Controller('compliance/:name/organizations/:orgId/notes')
 export class OrganizationComplianceNotesController {
   constructor(private readonly organizationComplianceNotesService: OrganizationComplianceNotesService) {}
 
-  @Post()
-  create(@Body() createOrganizationComplianceNoteDto: organization_compliance_notes) {
+  @Get()
+  findAllByCompliance(@Req() req: any, @Param('name') name: string, @Param('orgId') orgId: string) {
+    //return this.organizationComplianceNotesService.findAllByCompliance(req, name, orgId);
+  }
+
+  @Get('section_groups/:sgId')
+  findAllBySectionGroup(@Req() req: any, @Param('orgId') orgId: string, @Param('name') name: string, @Param('sgId') sgId: string) {
+    //return this.organizationComplianceNotesService.findAllBySectionGroup(req, orgId, name, sgId);
+  }
+
+  @Get('section_groups/:sgId/sections/:sId')
+  findAllBySection(@Req() req: any, @Param('orgId') orgId: string, @Param('name') name: string, @Param('sgId') sgId: string, @Param('sId') sId: string) {
+    //return this.organizationComplianceNotesService.findAllBySection(req, orgId, name, sgId, sId);
+  }
+
+  @Get('section_groups/:sgId/sections/:sId/questions/:qId')
+  findOneByQuestion(
+    @Req() req: any,
+    @Param('orgId') orgId: string,
+    @Param('name') name: string,
+    @Param('sgId') sgId: string,
+    @Param('sId') sId: string,
+    @Param('qId') qId: string,
+  ) {
+    //return this.organizationComplianceNotesService.findOneByQuestion(req, orgId, name, sgId, sId, qId);
+  }
+
+  @Post('section_groups/:sgId/sections/:sId/questions/:qId')
+  create(
+    @Req() req: any,
+    @Param('name') name: string,
+    @Param('orgId') orgId: string,
+    @Param('sgId') sgId: string,
+    @Param('sId') sId: string,
+    @Param('qId') qId: string,
+    @Body() createOrganizationComplianceNoteDto: organization_compliance_notes,
+  ) {
     return this.organizationComplianceNotesService.create(createOrganizationComplianceNoteDto);
   }
 
-  @Get()
-  findAll() {
-    return this.organizationComplianceNotesService.findAll();
-  }
-
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Req() req: any, @Param('id') id: string) {
     return this.organizationComplianceNotesService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOrganizationComplianceNoteDto: organization_compliance_notes) {
+  update(@Req() req: any, @Param('id') id: string, @Body() updateOrganizationComplianceNoteDto: organization_compliance_notes) {
     return this.organizationComplianceNotesService.update(+id, updateOrganizationComplianceNoteDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Req() req: any, @Param('id') id: string) {
     return this.organizationComplianceNotesService.remove(+id);
   }
 }
