ffd32926557b0da0f3a85abbb530b195abe4d7b1
Troy Morvant <troy.morvant@gmail.com>
2024-06-12 11:59:01 -0500

    feat: Add FilteringModule to ScoringModule in nest library
    - FilteringModule has been imported in the ScoringModule file.
    - FilteringModule has been added to the module imports.
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- libs/nest/src/lib/compliance/scoring/scoring.module.ts
+++ libs/nest/src/lib/compliance/scoring/scoring.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ScoringService } from './scoring.service';
+import { FilteringModule } from '../filtering';
 
 @Module({
+  imports: [FilteringModule],
   providers: [ScoringService],
   exports: [ScoringService],
 })
