a351ba56d13855671daab99f02d935e64ad3edea
Troy Morvant <troy.morvant@gmail.com>
2024-06-13 18:40:06 -0500

    feat: Update organization compliance notes controller
    - Add new imports for guards, filters, and swagger decorators
    - Enhance controller annotation with guards and filters
    - Refactor route endpoints and their corresponding methods
    - Remove unused endpoints
    - Update handling of requests in create, update, and remove methods
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-api/src/platform/modules/resources/organizations/organization_compliance/organization_compliance_notes/organization_compliance_notes.controller.ts
+++ apps/cold-api/src/platform/modules/resources/organizations/organization_compliance/organization_compliance_notes/organization_compliance_notes.controller.ts
@@ -1,63 +1,51 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Req } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Req, UseFilters, UseGuards } from '@nestjs/common';
 import { OrganizationComplianceNotesService } from './organization_compliance_notes.service';
-import { organization_compliance_notes } from '@prisma/client';
+import { HttpExceptionFilter, JwtAuthGuard, RolesGuard } from '@coldpbc/nest';
+import { ApiParam, ApiTags } from '@nestjs/swagger';
 
-@Controller('compliance/:name/organizations/:orgId/notes')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@UseFilters(new HttpExceptionFilter(OrganizationComplianceNotesController.name))
+@ApiTags('Compliance', 'Organization', 'Question', 'Notes')
+@ApiParam({
+  name: 'orgId',
+  required: true,
+  type: 'string',
+  example: `{{test_organization_id}}`,
+})
+@ApiParam({
+  name: 'name',
+  required: true,
+  type: 'string',
+  example: 'b_corp_2024',
+})
+@ApiParam({
+  name: 'qId',
+  required: true,
+  description: 'Question Id',
+  type: 'string',
+  example: 'cq_', // Example value
+})
+@Controller('compliance/:name/organizations/:orgId')
 export class OrganizationComplianceNotesController {
   constructor(private readonly organizationComplianceNotesService: OrganizationComplianceNotesService) {}
 
-  @Get()
-  findAllByCompliance(@Req() req: any, @Param('name') name: string, @Param('orgId') orgId: string) {
-    //return this.organizationComplianceNotesService.findAllByCompliance(req, name, orgId);
+  @Get('/questions/:qId/notes')
+  findAllByQuestion(@Req() req: any, @Param('name') name: string, @Param('qId') qId: string) {
+    return this.organizationComplianceNotesService.findAll(name, qId, req);
   }
 
-  @Get('section_groups/:sgId')
-  findAllBySectionGroup(@Req() req: any, @Param('orgId') orgId: string, @Param('name') name: string, @Param('sgId') sgId: string) {
-    //return this.organizationComplianceNotesService.findAllBySectionGroup(req, orgId, name, sgId);
+  @Post('/questions/:qId/notes')
+  create(@Req() req: any, @Param('name') name: string, @Param('qId') qId: string, @Body() body: { note: string }) {
+    return this.organizationComplianceNotesService.create(name, qId, body.note, req);
   }
 
-  @Get('section_groups/:sgId/sections/:sId')
-  findAllBySection(@Req() req: any, @Param('orgId') orgId: string, @Param('name') name: string, @Param('sgId') sgId: string, @Param('sId') sId: string) {
-    //return this.organizationComplianceNotesService.findAllBySection(req, orgId, name, sgId, sId);
+  @Patch('/questions/:qId/notes/:id')
+  update(@Req() req: any, @Param('name') name: string, @Param('id') id: string, @Param('qId') qId: string, @Body() body: { note: string }) {
+    return this.organizationComplianceNotesService.update(name, qId, id, body.note, req);
   }
 
-  @Get('section_groups/:sgId/sections/:sId/questions/:qId')
-  findOneByQuestion(
-    @Req() req: any,
-    @Param('orgId') orgId: string,
-    @Param('name') name: string,
-    @Param('sgId') sgId: string,
-    @Param('sId') sId: string,
-    @Param('qId') qId: string,
-  ) {
-    //return this.organizationComplianceNotesService.findOneByQuestion(req, orgId, name, sgId, sId, qId);
-  }
-
-  @Post('section_groups/:sgId/sections/:sId/questions/:qId')
-  create(
-    @Req() req: any,
-    @Param('name') name: string,
-    @Param('orgId') orgId: string,
-    @Param('sgId') sgId: string,
-    @Param('sId') sId: string,
-    @Param('qId') qId: string,
-    @Body() createOrganizationComplianceNoteDto: organization_compliance_notes,
-  ) {
-    return this.organizationComplianceNotesService.create(createOrganizationComplianceNoteDto);
-  }
-
-  @Get(':id')
-  findOne(@Req() req: any, @Param('id') id: string) {
-    return this.organizationComplianceNotesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Req() req: any, @Param('id') id: string, @Body() updateOrganizationComplianceNoteDto: organization_compliance_notes) {
-    return this.organizationComplianceNotesService.update(+id, updateOrganizationComplianceNoteDto);
-  }
-
-  @Delete(':id')
-  remove(@Req() req: any, @Param('id') id: string) {
-    return this.organizationComplianceNotesService.remove(+id);
+  @Delete('/questions/:qId/notes/:id')
+  remove(@Req() req: any, @Param('name') name: string, @Param('id') id: string, @Param('qId') qId: string) {
+    return this.organizationComplianceNotesService.remove(name, qId, id, req);
   }
 }
