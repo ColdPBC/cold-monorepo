7c71ccc5c0d8adf87b345b86e44e3f4c56fa9fd5
Troy Morvant <troy.morvant@gmail.com>
2024-06-28 17:17:44 -0500

    feat: Expand ComplianceDefinitionsRepository methods
    - Added new method `getComplianceDefinitionsByOrgId`
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- libs/nest/src/lib/compliance/repositories/compliance-definitions/compliance-definitions.repository.ts
+++ libs/nest/src/lib/compliance/repositories/compliance-definitions/compliance-definitions.repository.ts
@@ -1,12 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { BaseWorker } from '../../../worker';
-import { Cuid2Generator } from '../../../utility';
 import { PrismaService } from '../../../prisma';
+import { CacheService } from '../../../cache';
+import { compliance_definitions } from '@prisma/client';
+import { set } from 'lodash';
 
 @Injectable()
 export class ComplianceDefinitionsRepository extends BaseWorker {
-  constructor(readonly prisma: PrismaService) {
+  constructor(readonly prisma: PrismaService, readonly cache: CacheService) {
     super(ComplianceDefinitionsRepository.name);
+  }
+
+  async getComplianceDefinitionsByOrgId(orgId: string) {
+    const definitions = (await this.prisma.extended.compliance_definitions.findMany({
+      where: {
+        visible: true,
+      },
+      orderBy: {
+        order: 'asc',
+      },
+      select: {
+        id: true,
+        name: true,
+        logo_url: true,
+        title: true,
+        image_url: true,
+        metadata: true,
+        order: true,
+        version: true,
+      },
+    })) as compliance_definitions[];
+
+    if (Array.isArray(definitions) && definitions.length > 0) {
+      for (const def of definitions) {
+        const metrics = (await this.cache.get(`organization:${orgId}:compliance:${def.name}:counts`)) as { counts: { progress: number } };
+        if (metrics) {
+          set(def, 'progress', metrics.counts.progress);
+        }
+      }
+    } else {
+      throw new NotFoundException('No compliance definitions found');
+    }
+
+    return definitions;
   }
 
   async getComplianceSectionsByComplianceName(name: string) {
