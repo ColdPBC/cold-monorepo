535f8b5a04d3a48fdbd3e45484f3c5a0cb87c115
Qaalib Farah <qaalib1598@gmail.com>
2024-07-18 13:55:37 -0500

    feat: Refactor suppliersDataGrid to use mock data
    The data in the suppliersDataGrid component has been refactored. Instead of hardcoding the supplierData and certificateClaims data directly in the component, the data is now retrieved from mock functions. This allows for supplierData and certificateClaims to remain consistent across the application and simplifies future changes to the data set.

---
+++ libs/react/src/__mocks__/claimsMock.ts
@@ -0,0 +1,20 @@
+export const getClaimsMock = () => {
+  return [
+    {
+      name: 'pfas',
+      label: 'PFAS-Test',
+    },
+    {
+      name: 'lead',
+      label: 'Lead-Test',
+    },
+    {
+      name: 'phthalate',
+      label: 'Phthalate-Test',
+    },
+    {
+      name: 'bluesign',
+      label: 'Bluesign',
+    },
+  ];
+};
---
+++ libs/react/src/__mocks__/suppliersMock.ts
@@ -0,0 +1,144 @@
+import { addDays, subDays } from 'date-fns';
+
+export const getSupplierMock = () => {
+  return [
+    {
+      id: 1,
+      name: 'VietWear Garments Co., Ltd.',
+      country: 'Vietnam',
+      certificate_claims: {
+        pfas: {
+          expiration_date: addDays(new Date(), 5).toISOString(),
+        },
+        lead: {
+          expiration_date: null,
+        },
+        phthalate: {
+          expiration_date: null,
+        },
+        bluesign: {
+          expiration_date: addDays(new Date(), 5).toISOString(),
+        },
+      },
+    },
+    {
+      id: 2,
+      name: 'Pritt, Inc.',
+      country: 'China',
+      certificate_claims: {
+        pfas: {
+          expiration_date: addDays(new Date(), 5).toISOString(),
+        },
+        lead: {},
+        phthalate: {
+          expiration_date: addDays(new Date(), 5).toISOString(),
+        },
+        bluesign: {},
+      },
+    },
+    {
+      id: 3,
+      name: 'Smotherman, Inc.',
+      country: 'Vietnam',
+      certificate_claims: {
+        pfas: {
+          expiration_date: addDays(new Date(), 10).toISOString(),
+        },
+        lead: {
+          expiration_date: addDays(new Date(), 70).toISOString(),
+        },
+      },
+    },
+    {
+      id: 4,
+      name: 'Menzie, Inc.',
+      country: 'Vietnam',
+      certificate_claims: {
+        pfas: {
+          expiration_date: addDays(new Date(), 30).toISOString(),
+        },
+        bluesign: {
+          expiration_date: addDays(new Date(), 70).toISOString(),
+        },
+      },
+    },
+    {
+      id: 5,
+      name: 'Want, Inc.',
+      country: 'Vietnam',
+      certificate_claims: {
+        pfas: {
+          expiration_date: addDays(new Date(), 70).toISOString(),
+        },
+        bluesign: {
+          expiration_date: subDays(new Date(), 3).toISOString(),
+        },
+      },
+    },
+    {
+      id: 6,
+      name: 'Tattershall, Inc.',
+      country: 'Vietnam',
+      certificate_claims: {
+        lead: {
+          expiration_date: subDays(new Date(), 30).toISOString(),
+        },
+        phthalate: {
+          expiration_date: subDays(new Date(), 3).toISOString(),
+        },
+      },
+    },
+    {
+      id: 7,
+      name: 'Panek, Inc.',
+      country: 'Vietnam',
+      certificate_claims: {
+        pfas: {
+          expiration_date: addDays(new Date(), 60).toISOString(),
+        },
+        lead: {
+          expiration_date: addDays(new Date(), 30).toISOString(),
+        },
+        bluesign: {
+          expiration_date: addDays(new Date(), 70).toISOString(),
+        },
+      },
+    },
+    {
+      id: 8,
+      name: 'Faul, Inc.',
+      country: 'Vietnam',
+      certificate_claims: {
+        pfas: {
+          expiration_date: addDays(new Date(), 5).toISOString(),
+        },
+        lead: {
+          expiration_date: addDays(new Date(), 5).toISOString(),
+        },
+        phthalate: {},
+        bluesign: {
+          expiration_date: addDays(new Date(), 70).toISOString(),
+        },
+      },
+    },
+    {
+      id: 9,
+      name: 'Hushon, Inc.',
+      country: 'Vietnam',
+      certificate_claims: {
+        pfas: {
+          expiration_date: addDays(new Date(), 70).toISOString(),
+        },
+        lead: {
+          expiration_date: addDays(new Date(), 5).toISOString(),
+        },
+        phthalate: {
+          expiration_date: addDays(new Date(), 5).toISOString(),
+        },
+        bluesign: {
+          expiration_date: addDays(new Date(), 70).toISOString(),
+        },
+      },
+    },
+  ];
+};
