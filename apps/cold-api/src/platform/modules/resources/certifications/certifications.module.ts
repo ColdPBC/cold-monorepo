fecb18117d4674b045fe8641a75eb3a8d78a083b
Troy Morvant <troy.morvant@gmail.com>
2024-07-20 18:59:37 -0500

    feat: Add ClaimsModule to CertificationsModule
    - Added ClaimsModule import in CertificationsModule
    - Included ClaimsModule in the controllers and imports arrays of the CertificationsModule's @Module decorator
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-api/src/platform/modules/resources/certifications/certifications.module.ts
+++ apps/cold-api/src/platform/modules/resources/certifications/certifications.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { CertificationsController } from './certifications.controller';
 import { CertificationsService } from './certifications.service';
 import { CertificationsRepository } from '@coldpbc/nest';
+import { ClaimsModule } from './claims/claims.module';
 
 @Module({
-  controllers: [CertificationsController],
+  controllers: [CertificationsController, ClaimsModule],
   providers: [CertificationsService, CertificationsRepository],
   exports: [],
+  imports: [ClaimsModule],
 })
 export class CertificationsModule {}
