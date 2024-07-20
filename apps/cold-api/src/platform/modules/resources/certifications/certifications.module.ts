69e11819c63cdd94fdfd2707a3c9928815be0176
Troy Morvant <troy.morvant@gmail.com>
2024-07-19 20:53:05 -0500

    feat: Update certifications module in cold-api
    Changes include:
    - Reorder importations in the certifications module
    - Add CertificationsRepository to providers
    - Add exports field to @Module decorator in the certifications module
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-api/src/platform/modules/resources/certifications/certifications.module.ts
+++ apps/cold-api/src/platform/modules/resources/certifications/certifications.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CertificationsService } from './certifications.service';
 import { CertificationsController } from './certifications.controller';
+import { CertificationsService } from './certifications.service';
+import { CertificationsRepository } from '@coldpbc/nest';
 
 @Module({
   controllers: [CertificationsController],
-  providers: [CertificationsService],
+  providers: [CertificationsService, CertificationsRepository],
+  exports: [],
 })
 export class CertificationsModule {}
