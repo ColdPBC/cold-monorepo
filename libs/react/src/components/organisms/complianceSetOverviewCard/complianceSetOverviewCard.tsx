be9fe74d18ed8a5ca8ad7bfe0d12e97751440f5a
Qaalib Farah <qaalib1598@gmail.com>
2024-08-01 20:08:43 -0500

    feat: Add compliance activation and loading state handling
    Add a POST request in the compliance set overview card to activate compliance. Introduce a loading state to prevent multiple activations and display a spinner overlay during activation.

--- libs/react/src/components/organisms/complianceSetOverviewCard/complianceSetOverviewCard.tsx
+++ libs/react/src/components/organisms/complianceSetOverviewCard/complianceSetOverviewCard.tsx
@@ -3,7 +3,7 @@ import { axiosFetcher } from '@coldpbc/fetchers';
 import { AllCompliance, OrgCompliance, ToastMessage } from '@coldpbc/interfaces';
 import React, { useContext } from 'react';
 import { ColdCompliancePageContext } from '@coldpbc/context';
-import { ComplianceStatus, ErrorType } from '@coldpbc/enums';
+import { ComplianceStatus, ErrorType, GlobalSizes } from '@coldpbc/enums';
 import { Card, ComplianceStatusChip, ErrorFallback, Spinner } from '@coldpbc/components';
 import { differenceInDays, format, intlFormatDistance } from 'date-fns';
 import { isAxiosError } from 'axios';
@@ -22,6 +22,7 @@ const _ComplianceSetOverviewCard = ({ complianceSet }: { complianceSet: AllCompl
   const { logError } = useColdContext();
   const { addToastMessage } = useAddToastMessage();
   const { filter } = useContext(ColdCompliancePageContext);
+  const [complianceSetLoading, setComplianceSetLoading] = React.useState<boolean>(false);
 
   const getOrgComplianceURL = () => {
     if (ldFlags.showNewComplianceManagerCold711) {
@@ -163,8 +164,23 @@ const _ComplianceSetOverviewCard = ({ complianceSet }: { complianceSet: AllCompl
   };
 
   const onCardClick = async () => {
+    if (complianceSetLoading) return;
     if (ldFlags.showNewComplianceManagerCold711) {
-      navigate(`/compliance/${complianceSet.name}`);
+      if (complianceStatus === ComplianceStatus.inActive) {
+        // post to compliance/{complianceSet.name}/organizations/{orgId}
+        setComplianceSetLoading(true);
+        const response = await axiosFetcher([`/compliance/${complianceSet.name}/organizations/${orgId}`, 'POST']);
+        if (isAxiosError(response)) {
+          await addToastMessage({ message: 'Compliance could not be added', type: ToastMessage.FAILURE });
+          logError(response.message, ErrorType.AxiosError, response);
+        } else {
+          await addToastMessage({ message: 'Compliance activated', type: ToastMessage.SUCCESS });
+          navigate(`/compliance/${complianceSet.name}`);
+        }
+        setComplianceSetLoading(false);
+      } else {
+        navigate(`/compliance/${complianceSet.name}`);
+      }
     } else {
       if (!isAxiosError(orgComplianceSet) && orgComplianceSet !== undefined) {
         navigate(`/wizard/compliance/${complianceSet.name}`);
@@ -199,13 +215,26 @@ const _ComplianceSetOverviewCard = ({ complianceSet }: { complianceSet: AllCompl
     }
   };
 
+  const complianceSetLoadingBackground = () => {
+    // return an overlay with transparent background and spinner to put over the card
+    // put the spinner in the center of the card
+    return (
+      <div className={'absolute left-0 top-0 w-full h-full bg-bgc-accent bg-opacity-50 flex justify-center items-center'}>
+        <Spinner size={GlobalSizes.xLarge} />
+      </div>
+    );
+  };
+
   const shouldRender = checkFilter();
 
   if (!shouldRender) return null;
 
   return (
     <Card
-      className="w-full h-auto border-[1px] border-gray-60 cursor-pointer hover:bg-gray-50 hover:border-[1px] hover:border-white flex flex-row justify-start items-center"
+      className={
+        'w-full h-auto border-[1px] border-gray-60 flex flex-row justify-start items-center relative ' +
+        (complianceSetLoading ? '' : ' cursor-pointer hover:bg-gray-50 hover:border-[1px] hover:border-white')
+      }
       glow={false}
       onClick={onCardClick}>
       {orgComplianceSWR.isLoading ? (
@@ -218,6 +247,7 @@ const _ComplianceSetOverviewCard = ({ complianceSet }: { complianceSet: AllCompl
           {getComplianceDueDate()}
         </>
       )}
+      {complianceSetLoading && complianceSetLoadingBackground()}
     </Card>
   );
 };
