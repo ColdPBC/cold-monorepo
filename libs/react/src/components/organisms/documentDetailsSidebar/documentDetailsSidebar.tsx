9ec05a3c65af177fd14dbd1d569086f46ff19a1d
Qaalib Farah <qaalib1598@gmail.com>
2024-08-05 14:20:44 -0500

    feat: Add document download functionality
    Enhanced the document details sidebar to support downloading files by fetching signed URLs and updating the component with a corresponding download function. Modified the menu to include a download option and display a loading spinner when the URL is being fetched.

--- libs/react/src/components/organisms/documentDetailsSidebar/documentDetailsSidebar.tsx
+++ libs/react/src/components/organisms/documentDetailsSidebar/documentDetailsSidebar.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useRef } from 'react';
 import { Files, InputOption } from '@coldpbc/interfaces';
-import { ColdIcon, ErrorFallback, Input, Select, DocumentDetailsMenu } from '@coldpbc/components';
-import { FileTypes, IconNames, InputTypes } from '@coldpbc/enums';
-import { isEqual, toArray } from 'lodash';
+import { ColdIcon, ErrorFallback, Select, DocumentDetailsMenu, Spinner } from '@coldpbc/components';
+import { FileTypes, IconNames } from '@coldpbc/enums';
+import { toArray } from 'lodash';
 import capitalize from 'lodash/capitalize';
 import { DesktopDatePicker } from '@mui/x-date-pickers';
 import { withErrorBoundary } from 'react-error-boundary';
 import { HexColors } from '@coldpbc/themes';
-import { purple } from '@mui/material/colors';
 
 const _DocumentDetailsSidebar = (props: {
   file: Files | undefined;
@@ -15,8 +14,11 @@ const _DocumentDetailsSidebar = (props: {
   closeSidebar: () => void;
   innerRef: React.RefObject<HTMLDivElement>;
   deleteFile: (file: Files) => void;
+  isLoading: boolean;
+  downloadFile: (url: string | undefined) => void;
+  signedUrl: string | undefined;
 }) => {
-  const { file, closeSidebar, innerRef, updateFile, deleteFile } = props;
+  const { file, closeSidebar, innerRef, updateFile, deleteFile, isLoading, downloadFile, signedUrl } = props;
 
   const documentTypeOptions: InputOption[] = toArray(FileTypes).map((type, index) => {
     const name = capitalize(type.replace(/_/g, ' '));
@@ -50,136 +52,142 @@ const _DocumentDetailsSidebar = (props: {
               onMenuClick={item => {
                 if (item === 'delete') {
                   deleteFile(file);
+                } else if (item === 'download') {
+                  downloadFile(signedUrl);
                 }
               }}
             />
           </div>
-          <div className={'w-full flex flex-col gap-[20px]'}>
-            <div className={'w-full flex flex-col gap-[8px]'}>
-              <div className={'w-full text-tc-primary text-eyebrow'}>Category</div>
-              <Select
-                options={documentTypeOptions}
-                name={'type'}
-                value={capitalize(file.type.replace(/_/g, ' '))}
-                onChange={(e: InputOption) => {
-                  if (file) {
-                    updateFile({ ...file, type: FileTypes[e.value] });
-                  }
-                }}
-                buttonClassName={'w-full border-[1.5px] border-gray-90 rounded-[8px]'}
-              />
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <div className={'w-full flex flex-col gap-[20px]'}>
+              <div className={'w-full flex flex-col gap-[8px]'}>
+                <div className={'w-full text-tc-primary text-eyebrow'}>Category</div>
+                <Select
+                  options={documentTypeOptions}
+                  name={'type'}
+                  value={capitalize(file.type.replace(/_/g, ' '))}
+                  onChange={(e: InputOption) => {
+                    if (file) {
+                      updateFile({ ...file, type: FileTypes[e.value] });
+                    }
+                  }}
+                  buttonClassName={'w-full border-[1.5px] border-gray-90 rounded-[8px]'}
+                />
+              </div>
+              <div className={'w-full flex flex-col gap-[8px]'}>
+                <div className={'w-full text-tc-primary text-eyebrow'}>Valid From</div>
+                <DesktopDatePicker
+                  // @ts-ignore
+                  value={file.effective_start_date ? new Date(file.effective_start_date) : null}
+                  onChange={(date: Date | null) => {
+                    if (file) {
+                      updateFile({ ...file, effective_start_date: date ? date.toISOString() : null });
+                    }
+                  }}
+                  slotProps={{
+                    field: {
+                      clearable: true,
+                      onClear: () => {
+                        if (file) {
+                          updateFile({ ...file, effective_end_date: null });
+                        }
+                      },
+                    },
+                    popper: {
+                      container: innerRef.current,
+                    },
+                  }}
+                  sx={{
+                    '& .MuiInputBase-input': {
+                      backgroundColor: 'transparent',
+                      fontFamily: 'Inter',
+                      fontSize: '14px',
+                      padding: '16px',
+                      borderBottomLeftRadius: '8px',
+                      borderTopLeftRadius: '8px',
+                    },
+                    '& .MuiOutlinedInput-notchedOutline': {
+                      borderRadius: '8px',
+                      borderColor: HexColors.gray['90'],
+                      borderWidth: '1.5px',
+                    },
+                    '&  .MuiOutlinedInput-root': {
+                      borderRadius: '8px',
+                      '&:hover fieldset': {
+                        borderColor: HexColors.gray['90'],
+                        borderWidth: '1.5px',
+                      },
+                      '&:focus-within fieldset': {
+                        borderColor: HexColors.gray['90'],
+                        borderWidth: '1.5px',
+                      },
+                    },
+                    '& .MuiOutlinedInput-input:focus': {
+                      outline: 'none',
+                      boxShadow: 'none',
+                    },
+                  }}
+                />
+              </div>
+              <div className={'w-full flex flex-col gap-[8px]'}>
+                <div className={'w-full text-tc-primary text-eyebrow'}>Expiration Date</div>
+                <DesktopDatePicker
+                  // @ts-ignore
+                  value={file.effective_end_date ? new Date(file.effective_end_date) : null}
+                  onChange={(date: Date | null) => {
+                    if (file) {
+                      updateFile({ ...file, effective_end_date: date ? date.toISOString() : null });
+                    }
+                  }}
+                  slotProps={{
+                    field: {
+                      clearable: true,
+                      onClear: () => {
+                        if (file) {
+                          updateFile({ ...file, effective_end_date: null });
+                        }
+                      },
+                    },
+                    popper: {
+                      container: innerRef.current,
+                    },
+                  }}
+                  sx={{
+                    '& .MuiInputBase-input': {
+                      backgroundColor: 'transparent',
+                      fontFamily: 'Inter',
+                      fontSize: '14px',
+                      padding: '16px',
+                      borderBottomLeftRadius: '8px',
+                      borderTopLeftRadius: '8px',
+                    },
+                    '& .MuiOutlinedInput-notchedOutline': {
+                      borderRadius: '8px',
+                      borderColor: HexColors.gray['90'],
+                      borderWidth: '1.5px',
+                    },
+                    '&  .MuiOutlinedInput-root': {
+                      borderRadius: '8px',
+                      '&:hover fieldset': {
+                        borderColor: HexColors.gray['90'],
+                        borderWidth: '1.5px',
+                      },
+                      '&:focus-within fieldset': {
+                        borderColor: HexColors.gray['90'],
+                        borderWidth: '1.5px',
+                      },
+                    },
+                    '& .MuiOutlinedInput-input:focus': {
+                      outline: 'none',
+                      boxShadow: 'none',
+                    },
+                  }}
+                />
+              </div>
             </div>
-            <div className={'w-full flex flex-col gap-[8px]'}>
-              <div className={'w-full text-tc-primary text-eyebrow'}>Valid From</div>
-              <DesktopDatePicker
-                // @ts-ignore
-                value={file.effective_start_date ? new Date(file.effective_start_date) : null}
-                onChange={(date: Date | null) => {
-                  if (file) {
-                    updateFile({ ...file, effective_start_date: date ? date.toISOString() : null });
-                  }
-                }}
-                slotProps={{
-                  field: {
-                    clearable: true,
-                    onClear: () => {
-                      if (file) {
-                        updateFile({ ...file, effective_end_date: null });
-                      }
-                    },
-                  },
-                  popper: {
-                    container: innerRef.current,
-                  },
-                }}
-                sx={{
-                  '& .MuiInputBase-input': {
-                    backgroundColor: 'transparent',
-                    fontFamily: 'Inter',
-                    fontSize: '14px',
-                    padding: '16px',
-                    borderBottomLeftRadius: '8px',
-                    borderTopLeftRadius: '8px',
-                  },
-                  '& .MuiOutlinedInput-notchedOutline': {
-                    borderRadius: '8px',
-                    borderColor: HexColors.gray['90'],
-                    borderWidth: '1.5px',
-                  },
-                  '&  .MuiOutlinedInput-root': {
-                    borderRadius: '8px',
-                    '&:hover fieldset': {
-                      borderColor: HexColors.gray['90'],
-                      borderWidth: '1.5px',
-                    },
-                    '&:focus-within fieldset': {
-                      borderColor: HexColors.gray['90'],
-                      borderWidth: '1.5px',
-                    },
-                  },
-                  '& .MuiOutlinedInput-input:focus': {
-                    outline: 'none',
-                    boxShadow: 'none',
-                  },
-                }}
-              />
-            </div>
-            <div className={'w-full flex flex-col gap-[8px]'}>
-              <div className={'w-full text-tc-primary text-eyebrow'}>Expiration Date</div>
-              <DesktopDatePicker
-                // @ts-ignore
-                value={file.effective_end_date ? new Date(file.effective_end_date) : null}
-                onChange={(date: Date | null) => {
-                  if (file) {
-                    updateFile({ ...file, effective_end_date: date ? date.toISOString() : null });
-                  }
-                }}
-                slotProps={{
-                  field: {
-                    clearable: true,
-                    onClear: () => {
-                      if (file) {
-                        updateFile({ ...file, effective_end_date: null });
-                      }
-                    },
-                  },
-                  popper: {
-                    container: innerRef.current,
-                  },
-                }}
-                sx={{
-                  '& .MuiInputBase-input': {
-                    backgroundColor: 'transparent',
-                    fontFamily: 'Inter',
-                    fontSize: '14px',
-                    padding: '16px',
-                    borderBottomLeftRadius: '8px',
-                    borderTopLeftRadius: '8px',
-                  },
-                  '& .MuiOutlinedInput-notchedOutline': {
-                    borderRadius: '8px',
-                    borderColor: HexColors.gray['90'],
-                    borderWidth: '1.5px',
-                  },
-                  '&  .MuiOutlinedInput-root': {
-                    borderRadius: '8px',
-                    '&:hover fieldset': {
-                      borderColor: HexColors.gray['90'],
-                      borderWidth: '1.5px',
-                    },
-                    '&:focus-within fieldset': {
-                      borderColor: HexColors.gray['90'],
-                      borderWidth: '1.5px',
-                    },
-                  },
-                  '& .MuiOutlinedInput-input:focus': {
-                    outline: 'none',
-                    boxShadow: 'none',
-                  },
-                }}
-              />
-            </div>
-          </div>
+          )}
         </div>
       )}
     </div>
