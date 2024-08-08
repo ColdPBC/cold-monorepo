766072e75055798fce4215464df30e6ebe6d7d9d
Troy Morvant <troy.morvant@gmail.com>
2024-08-08 09:21:57 -0500

    test: rename claims to organization_claims and update references
    - Renamed `claims.service.spec.ts` to `organization_claims.service.spec.ts`.
    - Renamed `claims.controller.spec.ts` to `organization_claims.controller.spec.ts`.
    - Updated imports from `ClaimsService` to `OrganizationClaimsService`.
    - Updated imports from `ClaimsController` to `OrganizationClaimsController`.
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-api/src/platform/modules/resources/organizations/organization_claims/organization_claims.service.spec.ts
+++ apps/cold-api/src/platform/modules/resources/organizations/organization_claims/organization_claims.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ClaimsService } from './claims.service';
+import { ClaimsService } from './organization_claims_service';
 
 describe('ClaimsService', () => {
-  let service: ClaimsService;
+  let service: Organization_claimsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ClaimsService],
+      providers: [Organization_claimsService],
     }).compile();
 
-    service = module.get<ClaimsService>(ClaimsService);
+    service = module.get<Organization_claimsService>(Organization_claimsService);
   });
 
   it('should be defined', () => {
