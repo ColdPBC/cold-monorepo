99bd9419245ce24b3d6c98f85bb0797ebb92ba1c
Troy Morvant <troy.morvant@gmail.com>
2024-08-08 09:21:24 -0500

    refactor: rename Certifications module to Claims
    - Renamed `certifications.controller.ts` to `claims.controller.ts`
    - Updated imports from `CertificationsService` to `ClaimsService`
    - Changed `ApiTags` from 'Certifications' to 'Claims'
    - Adjusted filter to use `ClaimsController` instead of `CertificationsController`
    - Modified controller paths and types from `certifications` to `claims`
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-api/src/platform/modules/resources/claims/claims.controller.ts
+++ apps/cold-api/src/platform/modules/resources/claims/claims.controller.ts
@@ -1,29 +1,29 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, UseInterceptors, UseFilters, Req } from '@nestjs/common';
-import { CertificationsService } from './certifications.service';
+import { ClaimsService } from './claims.service';
 import { HttpExceptionFilter, JwtAuthGuard, OrgUserInterceptor, Role, Roles, RolesGuard } from '@coldpbc/nest';
 import { Span } from 'nestjs-ddtrace';
 import { ApiOAuth2, ApiTags } from '@nestjs/swagger';
-import { certifications } from '@prisma/client';
+import { claims } from '@prisma/client';
 
 @Span()
 @UseGuards(JwtAuthGuard, RolesGuard)
 @UseInterceptors(OrgUserInterceptor)
 @ApiOAuth2(['openid', 'email', 'profile'])
-@ApiTags('Certifications')
-@UseFilters(new HttpExceptionFilter(CertificationsController.name))
-@Controller('certifications')
-export class CertificationsController {
-  constructor(private readonly certificationsService: CertificationsService) {}
+@ApiTags('Claims')
+@UseFilters(new HttpExceptionFilter(ClaimsController.name))
+@Controller('claims')
+export class ClaimsController {
+  constructor(private readonly certificationsService: ClaimsService) {}
 
   @Post()
   @Roles(Role.ColdAdmin)
-  create(@Req() req: any, @Body() createCertificationDto: certifications) {
+  create(@Req() req: any, @Body() createCertificationDto: claims) {
     return this.certificationsService.create(req.organization, req.user, createCertificationDto);
   }
 
   @Patch(':id')
   @Roles(Role.ColdAdmin)
-  update(@Req() req: any, @Param('id') id: string, @Body() updateCertificationDto: certifications) {
+  update(@Req() req: any, @Param('id') id: string, @Body() updateCertificationDto: claims) {
     updateCertificationDto.id = id;
     return this.certificationsService.update(req.org, req.user, updateCertificationDto);
   }
