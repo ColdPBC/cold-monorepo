ab9aa5d54a4929e6f4cba1846c8482cafd5e513e
Troy Morvant <troy.morvant@gmail.com>
2024-08-16 16:48:21 -0500

    feat: update organization claims controller to use IRequest interface
    - Add `IRequest` interface import
    - Change `@Req()` parameter type from `any` to `IRequest` in multiple controller methods
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-api/src/platform/modules/resources/organizations/organization_claims/organization_claims.controller.ts
+++ apps/cold-api/src/platform/modules/resources/organizations/organization_claims/organization_claims.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseGuards, UseInterceptors, UseFilters } from '@nestjs/common';
 import { organization_claims } from '@prisma/client';
-import { allRoles, HttpExceptionFilter, JwtAuthGuard, OrgUserInterceptor, Roles, RolesGuard } from '@coldpbc/nest';
+import { allRoles, HttpExceptionFilter, IRequest, JwtAuthGuard, OrgUserInterceptor, Roles, RolesGuard } from '@coldpbc/nest';
 import { Span } from 'nestjs-ddtrace';
 import { ApiOAuth2, ApiTags } from '@nestjs/swagger';
 import { OrganizationClaimsService } from './organization_claims_service';
@@ -17,38 +17,38 @@ export class OrganizationClaimsController {
 
   @Roles(...allRoles)
   @Post()
-  create(@Req() req: any, @Param('cId') cId: string, @Param('orgId') orgId: string, @Body() createClaimDto: organization_claims) {
+  create(@Req() req: IRequest, @Param('cId') cId: string, @Param('orgId') orgId: string, @Body() createClaimDto: organization_claims) {
     createClaimDto.claim_id = cId;
     return this.organizationClaimsService.create(req.organization, req.user, createClaimDto);
   }
 
   @Roles(...allRoles)
   @Get()
-  findAll(@Req() req: any, @Param('cId') cId: string, @Param('orgId') orgId: string) {
+  findAll(@Req() req: IRequest, @Param('cId') cId: string, @Param('orgId') orgId: string) {
     return this.organizationClaimsService.findAll(req.organization, req.user);
   }
 
   @Roles(...allRoles)
   @Get(':id')
-  findById(@Req() req: any, @Param('cId') cId: string, @Param('orgId') orgId: string, @Param('id') id: string) {
+  findById(@Req() req: IRequest, @Param('cId') cId: string, @Param('orgId') orgId: string, @Param('id') id: string) {
     return this.organizationClaimsService.findByName(req.organization, req.user, id);
   }
 
   @Roles(...allRoles)
   @Get('name/:name')
-  findByName(@Req() req: any, @Param('cId') cId: string, @Param('orgId') orgId: string, @Param('name') name: string) {
+  findByName(@Req() req: IRequest, @Param('cId') cId: string, @Param('orgId') orgId: string, @Param('name') name: string) {
     return this.organizationClaimsService.findByName(req.organization, req.user, name);
   }
 
   @Roles(...allRoles)
   @Patch(':id')
-  update(@Req() req: any, @Param('cId') cId: string, @Param('orgId') orgId: string, @Param('id') id: string, @Body() updateClaimDto: organization_claims) {
+  update(@Req() req: IRequest, @Param('cId') cId: string, @Param('orgId') orgId: string, @Param('id') id: string, @Body() updateClaimDto: organization_claims) {
     return this.organizationClaimsService.update(req.organization, req.user, id, updateClaimDto);
   }
 
   @Roles(...allRoles)
   @Delete(':id')
-  remove(@Req() req: any, @Param('cId') cId: string, @Param('orgId') orgId: string, @Param('id') id: string) {
+  remove(@Req() req: IRequest, @Param('cId') cId: string, @Param('orgId') orgId: string, @Param('id') id: string) {
     return this.organizationClaimsService.remove(req.organization, req.user, id);
   }
 }
