c966553f936789201f0483225aeb11714fc1879c
Troy Morvant <troy.morvant@gmail.com>
2024-08-16 16:49:51 -0500

    refactor: replace 'any' with 'IRequest' in products controller and service
    - Updated imports to include IRequest in products controller and service files
    - Changed the type of req parameter from 'any' to 'IRequest' in various methods of products controller
    - Adjusted the methods in products service to reflect the type change from 'any' to 'IRequest'
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-api/src/platform/modules/resources/organizations/products/products.controller.ts
+++ apps/cold-api/src/platform/modules/resources/organizations/products/products.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseGuards, UseF
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { allRoles, HttpExceptionFilter, JwtAuthGuard, OrgUserInterceptor, Roles, RolesGuard } from '@coldpbc/nest';
+import { allRoles, HttpExceptionFilter, IRequest, JwtAuthGuard, OrgUserInterceptor, Roles, RolesGuard } from '@coldpbc/nest';
 import { Span } from 'nestjs-ddtrace';
 import { ApiOAuth2, ApiTags } from '@nestjs/swagger';
 
@@ -18,55 +18,55 @@ export class ProductsController {
 
   @Post()
   @Roles(...allRoles)
-  createOne(@Req() req: any, @Param('orgId') orgId: string, @Body() createProductDto: CreateProductDto) {
+  createOne(@Req() req: IRequest, @Param('orgId') orgId: string, @Body() createProductDto: CreateProductDto) {
     return this.productsService.create(req, createProductDto);
   }
 
   @Post('batch')
   @Roles(...allRoles)
-  createMany(@Req() req: any, @Param('orgId') orgId: string, @Body() createProductDto: CreateProductDto[]) {
+  createMany(@Req() req: IRequest, @Param('orgId') orgId: string, @Body() createProductDto: CreateProductDto[]) {
     return this.productsService.createMany(req, createProductDto);
   }
 
   @Get()
   @Roles(...allRoles)
-  findAll(@Req() req: any, @Param('orgId') orgId: string) {
+  findAll(@Req() req: IRequest, @Param('orgId') orgId: string) {
     return this.productsService.findAll(req);
   }
 
   @Get(':id')
   @Roles(...allRoles)
-  findOneById(@Req() req: any, @Param('orgId') orgId: string, @Param('id') id: string) {
+  findOneById(@Req() req: IRequest, @Param('orgId') orgId: string, @Param('id') id: string) {
     return this.productsService.findOne(req, { id });
   }
 
   @Get('name/:name')
   @Roles(...allRoles)
-  findOneByName(@Req() req: any, @Param('orgId') orgId: string, @Param('name') name: string) {
+  findOneByName(@Req() req: IRequest, @Param('orgId') orgId: string, @Param('name') name: string) {
     return this.productsService.findOne(req, { name });
   }
 
   @Patch(':id')
   @Roles(...allRoles)
-  updateById(@Req() req: any, @Param('orgId') orgId: string, @Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  updateById(@Req() req: IRequest, @Param('orgId') orgId: string, @Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(req, { id }, updateProductDto);
   }
 
   @Patch('name/:name')
   @Roles(...allRoles)
-  updateByName(@Req() req: any, @Param('orgId') orgId: string, @Param('name') name: string, @Body() updateProductDto: UpdateProductDto) {
+  updateByName(@Req() req: IRequest, @Param('orgId') orgId: string, @Param('name') name: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(req, { name }, updateProductDto);
   }
 
   @Delete(':id')
   @Roles(...allRoles)
-  removeById(@Req() req: any, @Param('orgId') orgId: string, @Param('id') id: string) {
+  removeById(@Req() req: IRequest, @Param('orgId') orgId: string, @Param('id') id: string) {
     return this.productsService.remove(req, { id });
   }
 
   @Delete('name/:name')
   @Roles(...allRoles)
-  removeByName(@Req() req: any, @Param('orgId') orgId: string, @Param('name') name: string) {
+  removeByName(@Req() req: IRequest, @Param('orgId') orgId: string, @Param('name') name: string) {
     return this.productsService.remove(req, { name });
   }
 }
