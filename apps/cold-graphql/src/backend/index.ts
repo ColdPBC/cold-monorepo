7a67f32d617f07a877202aeab1fd75575a8d7708
Troy Morvant <troy.morvant@gmail.com>
2024-08-28 14:02:37 -0500

    fix: update introspection configuration and clean up imports
    - Refactor import statements to single lines
    - Modify introspection setting based on environment
    - Ensure consistent use of single quotes in strings
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-graphql/src/backend/index.ts
+++ apps/cold-graphql/src/backend/index.ts
@@ -2,28 +2,22 @@
 import Graphweaver from '@exogee/graphweaver-server';
 import './schema';
 
-
-import {
-    AuthZero,
-    setAddUserToContext,
-    setImplicitAllow, setAdministratorRoleName, UserProfile,
-} from "@exogee/graphweaver-auth";
-import {UserProfileType} from "@exogee/graphweaver-auth/lib/user-profile";
-import {addUserToContext} from "./cold_profile";
+import { AuthZero, setAddUserToContext, setImplicitAllow, setAdministratorRoleName, UserProfile } from '@exogee/graphweaver-auth';
+import { UserProfileType } from '@exogee/graphweaver-auth/lib/user-profile';
+import { addUserToContext } from './cold_profile';
 
 export const authZero = new AuthZero();
 
 setAddUserToContext(addUserToContext);
-setAdministratorRoleName("cold:admin")
+setAdministratorRoleName('cold:admin');
 
 export const graphweaver = new Graphweaver({
-    apolloServerOptions: {
-        introspection: true
-    },
-    graphQLArmorOptions: {
-        maxAliases: { n: 50 },
-    }
+  apolloServerOptions: {
+    introspection: process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'staging',
+  },
+  graphQLArmorOptions: {
+    maxAliases: { n: 50 },
+  },
 });
 
 //export const handler = graphweaver.handler();
-
