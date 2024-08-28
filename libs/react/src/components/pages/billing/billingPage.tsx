b28cc12650e492a51bb56a6c3db313e035f4f938
Qaalib Farah <qaalib1598@gmail.com>
2024-08-28 11:31:05 -0500

    feat: integrate Stripe embedded checkout on the Billing page
    - Import `EmbeddedCheckout`, `EmbeddedCheckoutProvider`, and `loadStripe` from Stripe's library
    - Initialize Stripe with public key
    - Wrap `EmbeddedCheckout` with `EmbeddedCheckoutProvider` within `MainContent`

--- libs/react/src/components/pages/billing/billingPage.tsx
+++ libs/react/src/components/pages/billing/billingPage.tsx
@@ -1,5 +1,17 @@
 import { MainContent } from '@coldpbc/components';
+import { EmbeddedCheckout, EmbeddedCheckoutProvider } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_live_51NTmrdFjHElEUwepHr6fstop7tf00oYI0S6tHvYLdcK2j5J7Cf7zWh3oufzZfZgAm7UR70EFSaj1AnDFjQUjPIXY003YYApQiA', {});
 
 export const BillingPage = () => {
-  return <MainContent title="Billing"></MainContent>;
+  const fetchClientSecret;
+
+  return (
+    <MainContent title="Billing">
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </MainContent>
+  );
 };
