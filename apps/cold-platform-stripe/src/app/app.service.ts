9b8fbb0e256a7fb01a107b9ad433e0792c698d8b
Qaalib Farah <qaalib1598@gmail.com>
2024-09-04 09:50:01 -0500

    feat: enhance AppService with logging and extend BaseWorker
    - Add logging to customer related methods for better tracking
    - Extend BaseWorker and implement OnModuleInit for improved initialization
    - Log

--- apps/cold-platform-stripe/src/app/app.service.ts
+++ apps/cold-platform-stripe/src/app/app.service.ts
@@ -1,16 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import Stripe from 'stripe';
 import { ConfigService } from '@nestjs/config';
-import { IRequest, PrismaService } from '@coldpbc/nest';
+import { BaseWorker, IRequest, PrismaService } from '@coldpbc/nest';
 import { get } from 'lodash';
 
 @Injectable()
-export class AppService {
+export class AppService extends BaseWorker implements OnModuleInit {
   stripe: Stripe;
   key: string;
   frontendUrl: string;
 
   constructor(readonly config: ConfigService, readonly prisma: PrismaService) {
+    super(AppService.name);
     this.key = this.config.getOrThrow('STRIPE_API_KEY');
     this.stripe = new Stripe(this.key);
     this.frontendUrl = this.config.getOrThrow('STRIPE_FRONTEND_URL');
@@ -25,6 +26,10 @@ export class AppService {
     let currentSubscription: Stripe.Subscription | null = null;
     let paymentMethod: Stripe.PaymentMethod | null = null;
     if (customerId) {
+      this.logger.info(`Getting subscriptions for customer ${customerId}`, {
+        customerId,
+        organization: req.organization,
+      });
       const customer = (await this.stripe.customers.retrieve(customerId as string)) as Stripe.Response<Stripe.Customer>;
       const subscriptions = await this.stripe.subscriptions.list({
         customer: customerId as string,
@@ -32,6 +37,7 @@ export class AppService {
       const customerPaymentMethods = await this.stripe.customers.listPaymentMethods(customerId as string);
       // get subscription payment methods
       if (subscriptions.data.length > 0) {
+        this.logger.info(`Found ${subscriptions.data.length} subscriptions for customer ${customerId}`);
         currentSubscription = subscriptions.data[0];
         const defaultPaymentMethod = currentSubscription.default_payment_method as string | null;
         const defaultSource = currentSubscription.default_source as string | null;
@@ -41,6 +47,10 @@ export class AppService {
           defaultPaymentMethod || defaultSource || (customer.invoice_settings.default_payment_method as string | null) || (customer.default_source as string | null) || null;
         if (paymentMethodId) {
           paymentMethod = customerPaymentMethods.data.find(pm => pm.id === paymentMethodId) || null;
+          this.logger.info(`Found payment method for subscription ${currentSubscription.id} for customer ${customer.name}`, {
+            paymentMethod,
+            customer,
+          });
         }
       }
     }
@@ -54,8 +64,10 @@ export class AppService {
   async createPortalSession(req: IRequest) {
     const customerId = this.getStripeCustomerId(req.organization);
     if (!customerId) {
+      this.logger.info(`No customer id found for organization ${req.organization.name}. Not creating portal session.`);
       return new Error('No customer id found');
     }
+    this.logger.info(`Creating billing portal session for customer ${customerId}`);
     return await this.stripe.billingPortal.sessions.create({
       customer: customerId,
       return_url: `${this.frontendUrl}/settings/billing`,
@@ -66,6 +78,10 @@ export class AppService {
   }
 
   createCustomer(req: IRequest) {
+    this.logger.info('Creating customer', {
+      organization: req.organization,
+      body: req.body,
+    });
     return this.stripe.customers.create({
       ...req.body,
     });
@@ -74,6 +90,7 @@ export class AppService {
   deleteCustomer(req: IRequest) {
     const customerId = this.getStripeCustomerId(req.organization);
     if (!customerId) {
+      this.logger.error(`No customer id found for organization ${req.organization.name}. Not deleting customer`);
       return new Error('No customer id found');
     }
     return this.stripe.customers.del(customerId);
