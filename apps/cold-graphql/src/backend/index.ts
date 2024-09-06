a3e9b4aaa2174270a122eb88fc99c6a2a53b55c4
Troy Morvant <troy.morvant@gmail.com>
2024-09-06 15:48:54 -0500

    refactor: simplify winstonConfig setup and add useTabs to .prettierrc
    - Added `useTabs` option to .prettierrc
    - Simplified winstonConfig setup directly inside graphweaver logger configuration
    - Removed redundant function definition for winston configuration
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-graphql/src/backend/index.ts
+++ apps/cold-graphql/src/backend/index.ts
@@ -13,49 +13,44 @@ setAddUserToContext(addUserToContext);
 setAdministratorRoleName('cold:admin');
 
 const meta = { service: 'graphweaver', environment: process.env.NODE_ENV || 'development', version: '1.0.0' };
-const winstonConfig = (context: string, meta?: any) => {
-  const config = {
-    service: process.env['DD_SERVICE'] || meta?.service,
-    version: process.env['DD_VERSION'] || meta?.version,
-    environment: process.env['NODE_ENV'] || meta?.environment,
-    ...omit(meta, ['service', 'version', 'environment']),
-    context: context,
-    tags: ['nest.js', 'winston'],
-    transports: [
-      new winston.transports.Console({
-        format:
-          process.env['NODE_ENV'] === 'development' || process.env['LOG_FORMAT'] === 'pretty'
-            ? winston.format.combine(winston.format.combine(winston.format.timestamp(), nestWinstonModuleUtilities.format.nestLike()))
-            : winston.format.combine(winston.format.timestamp(), winston.format.json(), winston.format.align()),
-      }),
-    ],
-  };
 
-  //console.log('winstonConfig', config);
-
-  return config;
-};
 export const graphweaver = new Graphweaver({
-  apolloServerOptions: {
-    introspection: process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'staging',
-    status400ForVariableCoercionErrors: true,
-    hideSchemaDetailsFromClientErrors: process.env.NODE_ENV !== 'development',
-    includeStacktraceInErrorResponses: process.env.NODE_ENV === 'development',
-    formatError: (error, context) => {
-      return {
-        extensions: error.extensions,
-        message: error.message,
-        locations: error.locations,
-        path: error.path,
-        context,
-      };
-    },
-    logger: createLogger(winstonConfig(meta.service, meta)).child({
-      context: meta.service,
-      meta,
-    }),
-  },
-  graphQLArmorOptions: {
-    maxAliases: { n: 60 },
-  },
+	apolloServerOptions: {
+		introspection: process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'staging',
+		status400ForVariableCoercionErrors: true,
+		hideSchemaDetailsFromClientErrors: process.env.NODE_ENV !== 'development',
+		includeStacktraceInErrorResponses: process.env.NODE_ENV === 'development',
+		formatError: (error, context) => {
+			return {
+				extensions: error.extensions,
+				message: error.message,
+				locations: error.locations,
+				path: error.path,
+				context,
+			};
+		},
+		logger: createLogger({
+			defaultMeta: {
+				service: process.env['DD_SERVICE'] || meta?.service || 'graphweaver',
+				version: process.env['DD_VERSION'] || meta?.version,
+				environment: process.env['NODE_ENV'] || meta?.environment,
+				context: 'graphweaver',
+				tags: ['nest.js', 'winston'],
+			},
+			transports: [
+				new winston.transports.Console({
+					format:
+						process.env['NODE_ENV'] === 'development' || process.env['LOG_FORMAT'] === 'pretty'
+							? winston.format.combine(winston.format.combine(winston.format.timestamp(), nestWinstonModuleUtilities.format.nestLike()))
+							: winston.format.combine(winston.format.timestamp(), winston.format.json(), winston.format.align()),
+				}),
+			],
+		}).child({
+			context: meta.service,
+			meta,
+		}),
+	},
+	graphQLArmorOptions: {
+		maxAliases: { n: 60 },
+	},
 });
