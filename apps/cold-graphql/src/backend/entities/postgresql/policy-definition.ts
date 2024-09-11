4de9ecbdc2729c235144b140eb704706ed54f9e8
Troy Morvant <troy.morvant@gmail.com>
2024-09-11 12:16:45 -0500

    feat: integrate PolicyDefinitionHooks for entity lifecycle management
    - Added import statements for PolicyDefinitionHooks and related hook types.
    - Introduced sidecar property and initialized it in the constructor.
    - Implemented BEFORE_CREATE and AFTER_CREATE hooks for lifecycle management.
    - Added BEFORE_READ and AFTER_READ hooks to handle read operations.
    - Implemented BEFORE_UPDATE and AFTER_UPDATE hooks for update operations.
    - Added BEFORE_DELETE and AFTER_DELETE hooks for delete operations.
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-graphql/src/backend/entities/postgresql/policy-definition.ts
+++ apps/cold-graphql/src/backend/entities/postgresql/policy-definition.ts
@@ -1,3 +1,6 @@
+import { PolicyDefinitionHooks } from './policy-definition.hooks';
+import { Hook, HookRegister, CreateOrUpdateHookParams, ReadHookParams, DeleteHookParams } from '@exogee/graphweaver';
+
 import { Collection, Entity, OneToMany, PrimaryKey, Property } from '@mikro-orm/core';
 import { PolicyDatum } from './policy-datum';
 
@@ -8,6 +11,12 @@ import { OrgContext } from '../../acl_policies';
 @ApplyAccessControlList(read_only_acl)
 @Entity({ tableName: 'policy_definitions' })
 export class PolicyDefinition {
+	sidecar: PolicyDefinitionHooks;
+
+	constructor() {
+		this.sidecar = new PolicyDefinitionHooks();
+	}
+
 	@PrimaryKey({ type: 'integer' })
 	id!: number;
 
@@ -25,4 +34,68 @@ export class PolicyDefinition {
 
 	@OneToMany({ entity: () => PolicyDatum, mappedBy: 'policyDefinition' })
 	policyData = new Collection<PolicyDatum>(this);
+
+	@Hook(HookRegister.BEFORE_CREATE)
+	async beforeCreate(params: CreateOrUpdateHookParams<typeof PolicyDefinition, OrgContext>) {
+		if (!this.sidecar) {
+			this.sidecar = new PolicyDefinitionHooks();
+		}
+		return await this.sidecar.beforeCreateHook(params);
+	}
+
+	@Hook(HookRegister.AFTER_CREATE)
+	async afterCreate(params: CreateOrUpdateHookParams<typeof PolicyDefinition, OrgContext>) {
+		if (!this.sidecar) {
+			this.sidecar = new PolicyDefinitionHooks();
+		}
+		return await this.sidecar.afterCreateHook(params);
+	}
+
+	@Hook(HookRegister.BEFORE_READ)
+	async beforeRead(params: ReadHookParams<typeof PolicyDefinition, OrgContext>) {
+		if (!this.sidecar) {
+			this.sidecar = new PolicyDefinitionHooks();
+		}
+		return await this.sidecar.beforeReadHook(params);
+	}
+
+	@Hook(HookRegister.AFTER_READ)
+	async afterRead(params: ReadHookParams<typeof PolicyDefinition, OrgContext>) {
+		if (!this.sidecar) {
+			this.sidecar = new PolicyDefinitionHooks();
+		}
+		return await this.sidecar.afterReadHook(params);
+	}
+
+	@Hook(HookRegister.BEFORE_UPDATE)
+	async beforeUpdate(params: CreateOrUpdateHookParams<typeof PolicyDefinition, OrgContext>) {
+		if (!this.sidecar) {
+			this.sidecar = new PolicyDefinitionHooks();
+		}
+		return await this.sidecar.beforeUpdateHook(params);
+	}
+
+	@Hook(HookRegister.AFTER_UPDATE)
+	async afterUpdate(params: CreateOrUpdateHookParams<typeof PolicyDefinition, OrgContext>) {
+		if (!this.sidecar) {
+			this.sidecar = new PolicyDefinitionHooks();
+		}
+		return await this.sidecar.afterUpdateHook(params);
+	}
+
+	@Hook(HookRegister.BEFORE_DELETE)
+	async beforeDelete(params: DeleteHookParams<typeof PolicyDefinition, OrgContext>) {
+		if (!this.sidecar) {
+			this.sidecar = new PolicyDefinitionHooks();
+		}
+		return await this.sidecar.beforeDeleteHook(params);
+	}
+
+	@Hook(HookRegister.AFTER_DELETE)
+	async afterDelete(params: DeleteHookParams<typeof PolicyDefinition, OrgContext>) {
+		if (!this.sidecar) {
+			this.sidecar = new PolicyDefinitionHooks();
+		}
+		return await this.sidecar.afterDeleteHook(params);
+	}
 }
