14d2c095a7fb8b3e28285999fbf26b914e25764d
Troy Morvant <troy.morvant@gmail.com>
2024-09-11 12:16:54 -0500

    refactor: consolidate hooks into PolicyDefinitionHooks class
    - Replaced standalone hook functions with methods in the PolicyDefinitionHooks class.
    - Added logging in each hook method.
    - Inherits from BaseSidecar to maintain structure adherence.
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-graphql/src/backend/entities/postgresql/policy-definition.hooks.ts
+++ apps/cold-graphql/src/backend/entities/postgresql/policy-definition.hooks.ts
@@ -9,41 +9,48 @@ import { get, set } from 'lodash';
 import { MQTTPayloadType, MqttService } from '../../libs/mqtt/mqtt.service';
 import { PolicyDefinition } from './policy-definition';
 
-export const beforeReadHook = async (params: ReadHookParams<typeof PolicyDefinition, OrgContext>) => {
-	//this.logger.log('beforeReadHook', { user: params.context.user, arguments: params.args });
-	return params;
-};
+export class PolicyDefinitionHooks extends BaseSidecar {
+	constructor() {
+		super(PolicyDefinitionHooks.name, PolicyDefinition);
+	}
 
-export const afterReadHook = async (params: ReadHookParams<typeof PolicyDefinition, OrgContext>) => {
-	//this.logger.log('afterReadHook', { user: params.context.user, arguments: params.args });
-	return params;
-};
-export const beforeCreateHook = async (params: CreateOrUpdateHookParams<typeof PolicyDefinition, OrgContext>) => {
-	//this.logger.log('beforeCreateHook', { user: params.context.user, arguments: params.args });
-	return params;
-};
+	async beforeReadHook(params: ReadHookParams<typeof PolicyDefinition, OrgContext>) {
+		this.logger.log('beforeReadHook', { user: params.context.user, arguments: params.args });
+		return params;
+	}
 
-export const afterCreateHook = async (params: CreateOrUpdateHookParams<typeof PolicyDefinition, OrgContext>) => {
-	//this.logger.log('afterCreateHook', { user: params.context.user, arguments: params.args });
-	return params;
-};
+	async afterReadHook(params: ReadHookParams<typeof PolicyDefinition, OrgContext>) {
+		this.logger.log('afterReadHook', { user: params.context.user, arguments: params.args });
+		return params;
+	}
 
-export const beforeUpdateHook = async (params: CreateOrUpdateHookParams<typeof PolicyDefinition, OrgContext>) => {
-	//this.logger.log('beforeUpdateHook', { user: params.context.user, arguments: params.args });
-	return params;
-};
+	async beforeCreateHook(params: CreateOrUpdateHookParams<typeof PolicyDefinition, OrgContext>) {
+		this.logger.log('beforeCreateHook', { user: params.context.user, arguments: params.args });
+		return params;
+	}
 
-export const afterUpdateHook = async (params: CreateOrUpdateHookParams<typeof PolicyDefinition, OrgContext>) => {
-	//this.logger.log('afterUpdateHook', { user: params.context.user, arguments: params.args });
-	return params;
-};
+	async afterCreateHook(params: CreateOrUpdateHookParams<typeof PolicyDefinition, OrgContext>) {
+		this.logger.log('afterCreateHook', { user: params.context.user, arguments: params.args });
+		return params;
+	}
 
-export const beforeDeleteHook = async (params: DeleteHookParams<typeof PolicyDefinition, OrgContext>) => {
-	//this.logger.log('beforeDeleteHook', { user: params.context.user, arguments: params.args });
-	return params;
-};
+	async beforeUpdateHook(params: CreateOrUpdateHookParams<typeof PolicyDefinition, OrgContext>) {
+		this.logger.log('beforeUpdateHook', { user: params.context.user, arguments: params.args });
+		return params;
+	}
 
-export const afterDeleteHook = async (params: DeleteHookParams<typeof PolicyDefinition, OrgContext>) => {
-	//this.logger.log('afterDeleteHook', { user: params.context.user, arguments: params.args });
-	return params;
-};
+	async afterUpdateHook(params: CreateOrUpdateHookParams<typeof PolicyDefinition, OrgContext>) {
+		this.logger.log('afterUpdateHook', { user: params.context.user, arguments: params.args });
+		return params;
+	}
+
+	async beforeDeleteHook(params: DeleteHookParams<typeof PolicyDefinition, OrgContext>) {
+		this.logger.log('beforeDeleteHook', { user: params.context.user, arguments: params.args });
+		return params;
+	}
+
+	async afterDeleteHook(params: DeleteHookParams<typeof PolicyDefinition, OrgContext>) {
+		this.logger.log('afterDeleteHook', { user: params.context.user, arguments: params.args });
+		return params;
+	}
+}
