ad82c3ad6fa67c928e39cd670ce79a248c537e6b
Troy Morvant <troy.morvant@gmail.com>
2024-09-13 17:59:56 -0500

    feat: add seeding for sustainability attributes
    - Added `seedSustainabilityAttributes` to the `seedDB` function.
    - Adjusted indentation for better readability.
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- libs/nest/prisma/seeds/main.ts
+++ libs/nest/prisma/seeds/main.ts
@@ -8,29 +8,31 @@ import { seedComplianceDefinitions } from './seed_compliance_definitions';
 import { seedComplianceModels } from './seed_compliance_models';
 import { buildQuestionDependencyChains } from './seed_compliance_question_dependencies';
 import { buildSectionDependencyChains } from './seed_compliance_section_dependencies';
+import { seedSustainabilityAttributes } from './seed_sustainability_attributes';
 
 export const seedDB = async () => {
-  /**
-   * Seed the component-definitions table with the following:
-   */
-  await seedComponentDefinitions();
-  await seedCategoryDefinitions();
-  await seedPolicies();
-  await seedNews();
-  await seedUtilities();
-  await seedScopes();
-  await seedComplianceDefinitions();
-  if (process.env['LOCAL_SERVICE'] === 'seed') {
-    await seedComplianceModels();
-    await buildQuestionDependencyChains();
-    await buildSectionDependencyChains();
-  }
+	/**
+	 * Seed the component-definitions table with the following:
+	 */
+	await seedComponentDefinitions();
+	await seedCategoryDefinitions();
+	await seedPolicies();
+	await seedNews();
+	await seedUtilities();
+	await seedScopes();
+	await seedComplianceDefinitions();
+	await seedSustainabilityAttributes();
+	if (process.env['LOCAL_SERVICE'] === 'seed') {
+		await seedComplianceModels();
+		await buildQuestionDependencyChains();
+		await buildSectionDependencyChains();
+	}
 
-  /**
-   * Repeat the pattern above for any other tables you want to seed.
-   */
+	/**
+	 * Repeat the pattern above for any other tables you want to seed.
+	 */
 
-  console.log('db seeding complete');
+	console.log('db seeding complete');
 };
 
 seedDB();
