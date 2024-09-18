d053d1d3b370e98d43b45249639cbef5d3e04dc9
Troy Morvant <troy.morvant@gmail.com>
2024-09-18 12:59:52 -0500

    fix: fix code indentation in job.consumer.ts
    - Corrected the indentation for the entire file for consistency
    - Improved logging messages to include job data for better debugging
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-platform-openai/src/job.consumer.ts
+++ apps/cold-platform-openai/src/job.consumer.ts
@@ -13,140 +13,145 @@ import { PineconeService } from './pinecone/pinecone.service';
 @Injectable()
 @Processor('openai')
 export class JobConsumer extends BaseWorker {
-  client: OpenAI;
-  started: Date;
+	client: OpenAI;
+	started: Date;
 
-  constructor(
-    readonly config: ConfigService,
-    readonly appService: AppService,
-    readonly assistant: AssistantService,
-    readonly fileService: FileService,
-    readonly cache: CacheService,
-    readonly loader: PineconeService,
-    readonly darkly: DarklyService,
-    readonly chat: ChatService,
-    readonly mqtt: MqttService,
-    readonly complianceSectionsCacheRepository: ComplianceSectionsCacheRepository,
-  ) {
-    super(JobConsumer.name);
-    this.client = new OpenAI({
-      organization: this.config.getOrThrow('OPENAI_ORG_ID'),
-      apiKey: this.config.getOrThrow('OPENAI_API_KEY'),
-    });
-  }
+	constructor(
+		readonly config: ConfigService,
+		readonly appService: AppService,
+		readonly assistant: AssistantService,
+		readonly fileService: FileService,
+		readonly cache: CacheService,
+		readonly loader: PineconeService,
+		readonly darkly: DarklyService,
+		readonly chat: ChatService,
+		readonly mqtt: MqttService,
+		readonly complianceSectionsCacheRepository: ComplianceSectionsCacheRepository,
+	) {
+		super(JobConsumer.name);
+		this.client = new OpenAI({
+			organization: this.config.getOrThrow('OPENAI_ORG_ID'),
+			apiKey: this.config.getOrThrow('OPENAI_API_KEY'),
+		});
+	}
 
-  @Process('openai')
-  async process(job: Job) {
-    this.logger.info(`Received job ${job.id} of type ${job.name}`);
-    switch (job.name) {
-      case 'file.delete':
-        return this.deleteFileJob(job);
-      case 'file.uploaded':
-        return this.processFileJob(job);
-      case 'compliance_automation.enabled':
-        return this.processCompliance(job);
-      case 'survey':
-        return this.processCompliance(job);
-      default: {
-        const message = `Unknown job type ${job.name}`;
-        const apiError = new UnprocessableEntityError(422, undefined, message, undefined);
-        this.logger.error(apiError.message, { error: apiError, id: job.id, data: job.data });
-        throw apiError;
-      }
-    }
-  }
+	@Process('openai')
+	async process(job: Job) {
+		this.logger.info(`Received job ${job.id} of type ${job.name}`);
+		switch (job.name) {
+			case 'file.delete':
+				return this.deleteFileJob(job);
+			case 'file.uploaded':
+				return this.processFileJob(job);
+			case 'compliance_automation.enabled':
+				return this.processCompliance(job);
+			case 'survey':
+				return this.processCompliance(job);
+			default: {
+				const message = `Unknown job type ${job.name}`;
+				const apiError = new UnprocessableEntityError(422, undefined, message, undefined);
+				this.logger.error(apiError.message, { error: apiError, id: job.id, data: job.data });
+				throw apiError;
+			}
+		}
+	}
 
-  @Process('organization.created')
-  async processOrganizationCreated(job: Job) {
-    this.logger.info(`Received ${job.name} job: ${job.id} `);
-    return this.appService.createAssistant(job.data);
-  }
+	@Process('organization.created')
+	async processOrganizationCreated(job: Job) {
+		this.logger.info(`Received ${job.name} job: ${job.id} `);
+		return this.appService.createAssistant(job.data);
+	}
 
-  @Process('file.uploaded')
-  async processFileJob(job: Job) {
-    await this.loader.ingestData(job.data.user, job.data.organization, job.data.payload);
-    return this.fileService.uploadOrgFilesToOpenAI(job);
-  }
+	@Process('file.uploaded')
+	async processFileJob(job: Job) {
+		await this.loader.ingestData(job.data.user, job.data.organization, job.data.payload);
+		return this.fileService.uploadOrgFilesToOpenAI(job);
+	}
 
-  @Process('file.deleted')
-  async deleteFileJob(job: Job) {
-    const { vectors } = job.data.payload;
-    const index = await this.loader.getIndex();
+	@Process('file.deleted')
+	async deleteFileJob(job: Job) {
+		const { vectors } = job.data.payload;
+		const index = await this.loader.getIndex();
 
-    if (Array.isArray(vectors)) {
-      const deleted: any = [];
-      for (const vector of vectors) {
-        await index.namespace(job.data.organization.name).deleteOne(vector.id);
-        deleted.push(vector.id);
-      }
-    }
-    return this.fileService.deleteFile(job.data.user, job.data.integration.id, job.data.payload.key);
-  }
+		if (Array.isArray(vectors)) {
+			const deleted: any = [];
+			for (const vector of vectors) {
+				await index.namespace(job.data.organization.name).deleteOne(vector.id);
+				deleted.push(vector.id);
+			}
+		}
+		return this.fileService.deleteFile(job.data.user, job.data.integration.id, job.data.payload.key);
+	}
 
-  @Process('compliance_flow.enabled')
-  async processComplianceFlow(job: Job) {
-    try {
-      this.logger.info(`Received ${job.name} job: ${job.id} `);
-      await this.chat.process_compliance(job);
-    } catch (e) {
-      this.logger.error(e.message, e);
-      throw e;
-    }
-  }
+	@Process('compliance_flow.enabled')
+	async processComplianceFlow(job: Job) {
+		try {
+			this.logger.info(`Received ${job.name} job: ${job.id} `);
+			await this.chat.process_compliance(job);
+		} catch (e) {
+			this.logger.error(e.message, e);
+			throw e;
+		}
+	}
 
-  @Process('compliance_automation.enabled')
-  async processCompliance(job: Job) {
-    try {
-      this.logger.info(`Received ${job.name} job: ${job.id} `);
+	@Process('compliance_automation.enabled')
+	async processCompliance(job: Job) {
+		try {
+			this.logger.info(`Received ${job.name} job: ${job.id} `);
 
-      await this.chat.process_compliance_set(job);
-    } catch (e) {
-      this.logger.error(e.message, e);
-      throw e;
-    }
-  }
+			await this.chat.process_compliance_set(job);
+		} catch (e) {
+			this.logger.error(e.message, e);
+			throw e;
+		}
+	}
 
-  @OnQueueActive()
-  async onActive(job: Job) {
-    const message = `Processing ${job.name} | id: ${job.id} title: ${job.data.survey?.definition?.title} | started: ${new Date(job.processedOn || 0).toUTCString()}`;
-    await job.log(message);
-  }
+	@OnQueueActive()
+	async onActive(job: Job) {
+		const message = `Processing ${job.name} | id: ${job.id} | started: ${new Date(job.processedOn || 0).toUTCString()}`;
+		this.logger.info(message, { ...job.data });
+		await job.log(message);
+	}
 
-  @OnQueueFailed()
-  async onFailed(job: Job) {
-    const jobs = (await this.cache.get(`organizations:${job.data.organization.id}:jobs:${job.name}:${job.data.payload?.compliance?.compliance_id}`)) as number[];
-    if (Array.isArray(jobs) && jobs.length > 0) {
-      jobs.splice(jobs.indexOf(typeof job.id === 'number' ? job.id : parseInt(job.id)), 1);
-    }
-    await job.log(`Job FAILED | id: ${job.id} reason: ${job.failedReason} | ${this.getTimerString(job)}`);
-  }
+	@OnQueueFailed()
+	async onFailed(job: Job) {
+		const jobs = (await this.cache.get(`organizations:${job.data.organization.id}:jobs:${job.name}:${job.data.payload?.compliance?.compliance_id}`)) as number[];
+		if (Array.isArray(jobs) && jobs.length > 0) {
+			jobs.splice(jobs.indexOf(typeof job.id === 'number' ? job.id : parseInt(job.id)), 1);
+		}
+		const message = `Job FAILED | id: ${job.id} reason: ${job.failedReason} | ${this.getTimerString(job)}`;
+		this.logger.error(message, { ...job.data });
+		await job.log(message);
+	}
 
-  @OnQueueCompleted()
-  async onCompleted(job: Job) {
-    const jobs = (await this.cache.get(`organizations:${job.data.organization.id}:jobs:${job.name}:${job.data.payload.compliance?.compliance_id}`)) as number[];
-    if (jobs) {
-      jobs.splice(jobs.indexOf(typeof job.id === 'number' ? job.id : parseInt(job.id)), 1);
+	@OnQueueCompleted()
+	async onCompleted(job: Job) {
+		const jobs = (await this.cache.get(`organizations:${job.data.organization.id}:jobs:${job.name}:${job.data.payload.compliance?.compliance_id}`)) as number[];
+		if (jobs) {
+			jobs.splice(jobs.indexOf(typeof job.id === 'number' ? job.id : parseInt(job.id)), 1);
 
-      await this.cache.set(`organizations:${job.data.organization.id}:jobs:${job.name}::${job.data.payload.compliance?.compliance_id}`, jobs, { ttl: 60 * 60 * 24 * 7 });
-    }
+			await this.cache.set(`organizations:${job.data.organization.id}:jobs:${job.name}::${job.data.payload.compliance?.compliance_id}`, jobs, { ttl: 60 * 60 * 24 * 7 });
+		}
 
-    await job.log(`${job.name} Job COMPLETED | id: ${job.id} completed_on: ${new Date(job.finishedOn || 0).toUTCString()} | ${this.getTimerString(job)}`);
-  }
+		this.logger.info(`${job.name} Job COMPLETED | id: ${job.id} completed_on: ${new Date(job.finishedOn || 0).toUTCString()} | ${this.getTimerString(job)}`);
+	}
 
-  @OnQueueProgress()
-  async onProgress(job: Job) {
-    await job.log(`${job.name} Job PROGRESS | id: ${job.id} progress: ${(await job.progress()) * 100}% | ${this.getTimerString(job)}`);
-  }
+	@OnQueueProgress()
+	async onProgress(job: Job) {
+		const message = `${job.name} Job PROGRESS | id: ${job.id} progress: ${(await job.progress()) * 100}% | ${this.getTimerString(job)}`;
+		this.logger.info(message, { ...job.data });
+		await job.log(message);
+	}
 
-  getTimerString(job: Job) {
-    if (job.finishedOn) {
-      return `Duration: ${this.getDuration(job)} seconds`;
-    } else {
-      return `Elapsed: ${(new Date().getTime() - (job.processedOn || 0)) / 1000} seconds`;
-    }
-  }
+	getTimerString(job: Job) {
+		if (job.finishedOn) {
+			return `Duration: ${this.getDuration(job)} seconds`;
+		} else {
+			return `Elapsed: ${(new Date().getTime() - (job.processedOn || 0)) / 1000} seconds`;
+		}
+	}
 
-  getDuration(job: Job) {
-    return (job.finishedOn || 0) - (job.processedOn || 0) / 1000;
-  }
+	getDuration(job: Job) {
+		return (job.finishedOn || 0) - (job.processedOn || 0) / 1000;
+	}
 }
