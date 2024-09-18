aa67fd10a023a2e20edb1fc029d798900b91ba69
Troy Morvant <troy.morvant@gmail.com>
2024-09-18 16:15:22 -0500

    Revert "feat: enhance file classification and upload handling"
    This reverts commit 90b714c109e1dd8d9f95c3357cc44d64c8efd7f1.

--- apps/cold-api/src/platform/modules/resources/organizations/files/organization.files.controller.ts
+++ apps/cold-api/src/platform/modules/resources/organizations/files/organization.files.controller.ts
@@ -14,90 +14,90 @@ import { AnyFilesInterceptor } from '@nestjs/platform-express';
 @UseFilters(new HttpExceptionFilter(OrganizationFilesController.name))
 @Controller('organizations/:orgId/files')
 export class OrganizationFilesController implements OnModuleInit {
-	multerConfig: any;
+  multerConfig: any;
 
-	constructor(private readonly orgFiles: OrganizationFilesService, private readonly s3: S3Service) {}
+  constructor(private readonly orgFiles: OrganizationFilesService, private readonly s3: S3Service) {}
 
-	async onModuleInit() {
-		this.multerConfig = {
-			storage: multerS3({
-				s3: this.s3.client,
-				bucket: 'cold-api-uploaded-files',
-				contentType: multerS3.AUTO_CONTENT_TYPE,
-				metadata: async (req, file, cb) => {
-					const hash = await S3Service.calculateChecksum(file);
-					console.log(`${file.filename} Checksum: ${hash}`);
-					// Add custom metadata, such as the calculated hash
-					cb(null, { md5Hash: hash });
-				},
-				key: function (req, file, cb) {
-					const user = req['user'] as IAuthenticatedUser;
-					const orgId = req['orgId'];
-					file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf-8');
-					// Adjust this based on your actual user object structure
-					cb(null, `${process.env['NODE_ENV']}/${orgId}/${user.coldclimate_claims.email}/${file.originalname}`);
-				},
-			}),
-		};
-	}
+  async onModuleInit() {
+    this.multerConfig = {
+      storage: multerS3({
+        s3: this.s3.client,
+        bucket: 'cold-api-uploaded-files',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        metadata: async (req, file, cb) => {
+          const hash = await S3Service.calculateChecksum(file);
+          console.log(`${file.filename} Checksum: ${hash}`);
+          // Add custom metadata, such as the calculated hash
+          cb(null, { md5Hash: hash });
+        },
+        key: function (req, file, cb) {
+          const user = req['user'] as IAuthenticatedUser;
+          const orgId = req['orgId'];
 
-	@Get()
-	@Roles(...allRoles)
-	async getFiles(@Param('orgId') orgId: string, @Req() req: IRequest, @Query('bpc') bpc: boolean) {
-		return this.orgFiles.getFiles(req, orgId, bpc);
-	}
+          // Adjust this based on your actual user object structure
+          cb(null, `${process.env['NODE_ENV']}/${orgId}/${user.coldclimate_claims.email}/${file.originalname}`);
+        },
+      }),
+    };
+  }
 
-	@Get(':id/url')
-	@Roles(...allRoles)
-	async getUrl(
-		@Param('orgId') orgId: string,
-		@Param('id') fileId: string,
-		@Req()
-		req: IRequest,
-	) {
-		return this.orgFiles.getUrl(req, fileId);
-	}
+  @Get()
+  @Roles(...allRoles)
+  async getFiles(@Param('orgId') orgId: string, @Req() req: IRequest, @Query('bpc') bpc: boolean) {
+    return this.orgFiles.getFiles(req, orgId, bpc);
+  }
 
-	@Delete(':id')
-	@Roles(...coldAndCompanyAdmins)
-	async deleteFile(
-		@Param('orgId') orgId: string,
-		@Param('id') fileId: string,
-		@Req()
-		req: IRequest,
-	) {
-		return this.orgFiles.deleteFile(req, orgId, Array.isArray(fileId) ? fileId : [fileId]);
-	}
+  @Get(':id/url')
+  @Roles(...allRoles)
+  async getUrl(
+    @Param('orgId') orgId: string,
+    @Param('id') fileId: string,
+    @Req()
+    req: IRequest,
+  ) {
+    return this.orgFiles.getUrl(req, fileId);
+  }
 
-	@Post()
-	@Roles(...allRoles)
-	@UseInterceptors(AnyFilesInterceptor())
-	async uploadFile(
-		@Param('orgId') orgId: string,
-		@Query('bpc') bpc: boolean,
-		@UploadedFiles()
-		file: Array<Express.Multer.File>,
-		@Req()
-		req: IRequest,
-	) {
-		try {
-			return this.orgFiles.uploadFile(req, orgId, file, bpc);
-		} catch (error) {
-			console.error('Error uploading file:', error.message);
-			throw new Error('Failed to process the uploaded file.');
-		}
-	}
+  @Delete(':id')
+  @Roles(...coldAndCompanyAdmins)
+  async deleteFile(
+    @Param('orgId') orgId: string,
+    @Param('id') fileId: string,
+    @Req()
+    req: IRequest,
+  ) {
+    return this.orgFiles.deleteFile(req, orgId, Array.isArray(fileId) ? fileId : [fileId]);
+  }
 
-	@Patch(`:id`)
-	@Roles(...allRoles)
-	@UseInterceptors(AnyFilesInterceptor())
-	async updateFile(
-		@Param('orgId') orgId: string,
-		@Param('id') fileId: string,
-		@Body() data: { effective_end_date?: string; effective_start_date?: string; type?: string },
-		@Req()
-		req: IRequest,
-	) {
-		return this.orgFiles.update(req, fileId, data);
-	}
+  @Post()
+  @Roles(...allRoles)
+  @UseInterceptors(AnyFilesInterceptor())
+  async uploadFile(
+    @Param('orgId') orgId: string,
+    @Query('bpc') bpc: boolean,
+    @UploadedFiles()
+    file: Array<Express.Multer.File>,
+    @Req()
+    req: IRequest,
+  ) {
+    try {
+      return this.orgFiles.uploadFile(req, orgId, file, bpc);
+    } catch (error) {
+      console.error('Error uploading file:', error.message);
+      throw new Error('Failed to process the uploaded file.');
+    }
+  }
+
+  @Patch(`:id`)
+  @Roles(...allRoles)
+  @UseInterceptors(AnyFilesInterceptor())
+  async updateFile(
+    @Param('orgId') orgId: string,
+    @Param('id') fileId: string,
+    @Body() data: { effective_end_date?: string; effective_start_date?: string; type?: string },
+    @Req()
+    req: IRequest,
+  ) {
+    return this.orgFiles.update(req, fileId, data);
+  }
 }
