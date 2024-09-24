fa26cdf8229b94656244979a1f432c9dd48ab5a2
Qaalib Farah <qaalib1598@gmail.com>
2024-09-24 11:15:12 -0500

    feat: add logging to useGraphQLSWR for query success and error
    - Import `useColdContext` in `useGraphQLSWR` hook
    - Integrate `logBrowser` for logging successful and error query responses

--- libs/react/src/hooks/useGraphqlSWR.ts
+++ libs/react/src/hooks/useGraphqlSWR.ts
@@ -4,8 +4,10 @@ import { ColdApolloContext } from '@coldpbc/providers';
 import { ApolloQueryResult } from '@apollo/client';
 import { queries } from '@coldpbc/lib';
 import { get } from 'lodash';
+import {useColdContext} from "./useColdContext";
 
 export const useGraphQLSWR = <Data = any, Error = any>(key: string | null, variables?: any, config?: SWRConfiguration) => {
+  const {logBrowser} = useColdContext()
   const { client } = useContext(ColdApolloContext);
 
   return useSWR(
@@ -14,12 +16,26 @@ export const useGraphQLSWR = <Data = any, Error = any>(key: string | null, varia
       const query = get(queries, key, null);
       if (query) {
         return client
-          ?.query({
-            query: query,
-            variables: variables,
-          })
-          .then(response => response)
-          .catch(error => error);
+					?.query({
+						query: query,
+						variables: variables,
+					})
+					.then(response => {
+						logBrowser(`Successful fetching graphql query ${key}`, 'info', {
+							query: key,
+							variables,
+							response,
+						});
+						return response;
+					})
+					.catch(error => {
+            logBrowser(`Error fetching graphql query ${key}`, 'error', {
+              query: key,
+              variables,
+              error,
+            })
+						return error;
+					});
       } else {
         return undefined;
       }
