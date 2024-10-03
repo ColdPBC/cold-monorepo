20366e7366995cd7efb790c2c05ca3f4c1cc42d1
Qaalib Farah <qaalib1598@gmail.com>
2024-10-03 14:59:24 -0500

    chore: update MainContent stories with additional features and mock provider
    - Add `StoryMockProvider` for all stories
    - Introduce `WithPageButtons` story with save and cancel buttons
    - Add `title` and `breadcrumbs` to the `WithBreadCrumbs` story
    - Include additional necessary imports like `BaseButton`, `StoryMockProvider`, and `ButtonTypes`

--- libs/react/src/components/organisms/mainContent/mainContent.stories.tsx
+++ libs/react/src/components/organisms/mainContent/mainContent.stories.tsx
@@ -1,6 +1,9 @@
 import { withKnobs } from '@storybook/addon-knobs';
 import { Meta, StoryObj } from '@storybook/react';
-import { MainContent } from '@coldpbc/components';
+import {BaseButton, MainContent} from '@coldpbc/components';
+import {StoryMockProvider} from "@coldpbc/mocks";
+import {ButtonTypes} from "@coldpbc/enums";
+import React from "react";
 
 const meta = {
   title: 'Molecules/MainContent',
@@ -12,8 +15,58 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+export const WithPageButtons: Story = {
+	args: {
+		isLoading: true,
+		title: 'Main Content',
+		headerElement: (
+			<div className={'flex flex-row gap-[16px] h-full items-center'}>
+				<BaseButton
+					label={'Cancel'}
+					variant={ButtonTypes.warning}
+					className={'h-[40px]'}
+				/>
+				<BaseButton
+					label={'Save'}
+					variant={ButtonTypes.primary}
+					className={'h-[40px]'}
+				/>
+			</div>
+		),
+	},
+	render: args => {
+		return (
+			<StoryMockProvider>
+				<MainContent {...args} />
+			</StoryMockProvider>
+		);
+	},
+};
+
 export const Loading: Story = {
-  args: {
-    isLoading: true,
-  },
+	args: {
+		isLoading: true,
+		title: 'Main Content',
+	},
+	render: args => {
+		return (
+			<StoryMockProvider>
+				<MainContent {...args} />
+			</StoryMockProvider>
+		);
+	},
+};
+
+export const WithBreadCrumbs: Story = {
+	args: {
+		title: 'MainContent',
+		breadcrumbs: [{ label: 'Home', href: '/home' }, { label: 'Create New' }],
+	},
+	render: args => {
+		return (
+			<StoryMockProvider>
+				<MainContent {...args} />
+			</StoryMockProvider>
+		);
+	},
 };
