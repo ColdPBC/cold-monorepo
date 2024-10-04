a5f41a46986b719cc483d4ec6d2ac81aa0dd8c7d
Qaalib Farah <qaalib1598@gmail.com>
2024-10-04 11:56:31 -0500

    feat: extract Breadcrumbs component to separate file
    - Import `Breadcrumbs` in `MainContent` component
    - Remove inline breadcrumb logic from `MainContent`
    - Create `Breadcrumbs` component in a new file with existing breadcrumb logic
    - Ensure navigation functionality within `Breadcrumbs` component

---
+++ libs/react/src/components/molecules/breadcrumbs/breadcrumbs.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {useNavigate} from "react-router-dom";
+import opacity from "hex-color-opacity";
+import {HexColors} from "@coldpbc/themes";
+
+export const Breadcrumbs = (props: {
+  items: {
+    label: string;
+    href?: string;
+  }[];
+}) => {
+  const navigate = useNavigate();
+  const { items } = props;
+  const breadcrumbs =
+    items.map(
+      (item, index) => {
+        let classname = '';
+        if (item.href) {
+          classname = 'cursor-pointer hover:underline';
+        }
+        return (
+          <div key={`breadcrumb_${index}`} className={classname} onClick={() => {
+            if(item.href){
+              navigate(item.href);
+            }
+          }
+          }>
+            {item.label}
+          </div>
+        )
+      })
+  // interleave the breadcrumbs with '>' characters
+  const interleavedBreadcrumbs: JSX.Element[] = [];
+  breadcrumbs.forEach((breadcrumb, index) => {
+    interleavedBreadcrumbs.push(breadcrumb);
+    if (index < breadcrumbs.length - 1) {
+      interleavedBreadcrumbs.push(
+        <span key={`interleaved_${index}`} className={'w-[24px] h-[24px] flex items-center justify-center'}>{'>'}</span>
+      );
+    }
+  });
+
+  return (
+    <div
+      className={'w-full px-[16px] flex flex-row py-[8px] gap-[6px]'}
+      style={{
+        backgroundColor: opacity(HexColors.gray['30'], 0.5),
+      }}>
+      {
+        interleavedBreadcrumbs
+      }
+    </div>
+  );
+}
--- libs/react/src/components/organisms/mainContent/mainContent.tsx
+++ libs/react/src/components/organisms/mainContent/mainContent.tsx
@@ -1,11 +1,8 @@
 import React, { PropsWithChildren, ReactNode } from 'react';
-import { Spinner } from '@coldpbc/components';
+import {Breadcrumbs, Spinner} from '@coldpbc/components';
 import { GlobalSizes } from '@coldpbc/enums';
 import { twMerge } from 'tailwind-merge';
 import { useFlags } from 'launchdarkly-react-client-sdk';
-import opacity from "hex-color-opacity";
-import {HexColors} from "@coldpbc/themes";
-import {useNavigate} from "react-router-dom";
 
 export interface MainContentProps {
   title?: string;
@@ -21,49 +18,10 @@ export interface MainContentProps {
 
 export function MainContent(props: PropsWithChildren<MainContentProps>) {
   const ldFlags = useFlags();
-  const navigate = useNavigate();
   if(props.breadcrumbs){
-
-    const breadcrumbs =
-      props.breadcrumbs.map(
-        (breadcrumb, index) => {
-          let classname = '';
-          if (breadcrumb.href) {
-            classname = 'cursor-pointer hover:underline';
-          }
-          return (
-            <div key={`breadcrumb_${index}`} className={classname} onClick={() => {
-              if(breadcrumb.href){
-                navigate(breadcrumb.href);
-              }
-            }
-          }>
-          {breadcrumb.label}
-        </div>
-      )
-    })
-    // interleave the breadcrumbs with '>' characters
-    const interleavedBreadcrumbs: JSX.Element[] = [];
-    breadcrumbs.forEach((breadcrumb, index) => {
-      interleavedBreadcrumbs.push(breadcrumb);
-      if (index < breadcrumbs.length - 1) {
-        interleavedBreadcrumbs.push(
-          <span key={`interleaved_${index}`} className={'w-[24px] h-[24px] flex items-center justify-center'}>{'>'}</span>
-        );
-      }
-    });
-
     return (
       <div className={twMerge('w-[1129px] flex flex-col items-center gap-6 text-tc-primary', props.className)}>
-        <div
-          className={'w-full px-[16px] flex flex-row py-[8px] gap-[6px]'}
-          style={{
-            backgroundColor: opacity(HexColors.gray['30'], 0.5),
-          }}>
-          {
-            interleavedBreadcrumbs
-          }
-        </div>
+        <Breadcrumbs items={props.breadcrumbs}/>
         <div className={twMerge('w-full flex flex-col px-[64px] gap-[40px]', props.contentClassName)}>
           <div
             className={'w-full flex flex-row justify-between items-center'}>
