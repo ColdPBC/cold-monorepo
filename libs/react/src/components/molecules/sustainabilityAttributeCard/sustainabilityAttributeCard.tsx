3c28b23fcedd6d15e34e659b273da2fb76c94a67
Jason Mooty <jason.mooty@gmail.com>
2024-10-10 16:32:27 -0500

    chore: fetch more data and expand AttributeAssurance type

--- libs/react/src/components/molecules/sustainabilityAttributeCard/sustainabilityAttributeCard.tsx
+++ libs/react/src/components/molecules/sustainabilityAttributeCard/sustainabilityAttributeCard.tsx
@@ -3,11 +3,31 @@ import { withErrorBoundary } from 'react-error-boundary';
 import { ErrorFallback } from '../../application';
 
 export interface SustainabilityAttribute {
+  id: string;
   name: string;
   logoUrl?: string;
   level: 'MATERIAL' | 'ORGANIZATION' | 'PRODUCT' | 'SUPPLIER';
-  attributeAssurances: { id: string }[];
+  attributeAssurances: AttributeAssurance[];
+}
+
+interface AttributeAssurance {
   id: string;
+  effectiveEndDate: string | null;
+  material: {
+    id: string;
+  } | null;
+  organization: {
+    id: string;
+  } | null;
+  organizationFile: {
+    id: string;
+  } | null;
+  organizationFacility: {
+    id: string;
+  } | null;
+  product: {
+    id: string;
+  } | null;
 }
 
 interface SustainabilityAttributeCardProps {
