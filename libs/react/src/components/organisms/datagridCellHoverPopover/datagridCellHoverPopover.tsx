615eb916767b3b7ca0174ed17f2724a9959f9bbf
Qaalib Farah <qaalib1598@gmail.com>
2024-10-10 19:39:31 -0500

    fix: update popover resizing logic and style adjustments
    - Refactor popover content calculation logic using `ResizeObserver`
    - Adjust popover max-height and overflow styling
    - Add hidden container to measure actual width of elements
    - Clean up unused imports and add relevant new imports
    - Minor style and attribute adjustments for popover interaction

--- libs/react/src/components/organisms/datagridCellHoverPopover/datagridCellHoverPopover.tsx
+++ libs/react/src/components/organisms/datagridCellHoverPopover/datagridCellHoverPopover.tsx
@@ -1,7 +1,13 @@
-import React, {PropsWithChildren, ReactNode, useEffect, useRef} from "react";
-import { Popover } from '@mui/material';
+import React, {useEffect, useRef} from "react";
+import {Popover} from '@mui/material';
 import {HexColors} from "@coldpbc/themes";
 import {GridRenderCellParams, GridTreeNodeWithRender} from "@mui/x-data-grid";
+import {orderBy, sortBy} from "lodash";
+import {child} from "winston";
+
+// todo: set max width
+
+const MAX_WIDTH = 200;
 
 export const DataGridCellHoverPopover = (props: {
   params: GridRenderCellParams<any, any, any, GridTreeNodeWithRender>;
@@ -16,12 +22,13 @@ export const DataGridCellHoverPopover = (props: {
   const open = Boolean(anchorEl);
   const divRef = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
+  const popperRef = useRef<HTMLDivElement>(null);
 
   const getPopoverContent = () => {
 		// list values as a unordered list
     const otherValues = values.slice(valuesToDisplay.length);
 		return (
-			<div className={'rounded-[8px] bg-gray-50 border-[1px] border-lightblue-300 p-[8px] text-tc-primary text-body transition-none w-[275px]'}>
+			<div className={'rounded-[8px] bg-gray-50 border-[1px] border-lightblue-300 p-[8px] text-tc-primary text-body transition-none w-[275px] max-h-[200px] overflow-y-scroll'}>
 				<ul className={'list-disc ml-[20px]'}>
 					{otherValues.map((value, index) => (
 						<li key={index}>{value}</li>
@@ -43,14 +50,16 @@ export const DataGridCellHoverPopover = (props: {
 		if (values.length === valuesToDisplay.length) {
 			return null;
 		}
+
 		return (
 			<div
         className={'rounded-[32px] border-[1px] px-[16px] py-[4px] w-auto'}
         style={{
           borderColor: color,
         }}
+        onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}
       >
-        <div className={'h-full w-auto'} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+        <div className={'h-full w-auto'}>
           <span className={'text-body'}>{`+${values.length - valuesToDisplay.length}`}</span>
         </div>
         <Popover
@@ -67,43 +76,81 @@ export const DataGridCellHoverPopover = (props: {
             vertical: 'top',
             horizontal: 'left',
           }}
+          ref={popperRef}
           onClose={onMouseLeave}
-          disableRestoreFocus
+          disableRestoreFocus={true}
+          disableScrollLock={true}
+
         >
           {getPopoverContent()}
         </Popover>
-			</div>
+      </div>
 		);
-	};
+  };
 
   useEffect(() => {
-    if(divRef.current && containerRef.current) {
-      const divWidth = divRef.current.offsetWidth;
-      const totalWidth = containerRef.current.offsetWidth - 30;
-      if (totalWidth <= divWidth) {
-        const tempValues = [...values];
-        let tempWidth = divWidth;
-        while (totalWidth <= tempWidth) {
-          tempValues.pop();
-          tempWidth = tempValues.reduce((acc, value) => acc + (value.length * 10) + 24, 0);
-        }
-        setValuesToDisplay(tempValues);
+    const handleResize = () => {
+      let observer: ResizeObserver | null = null;
+      if(containerRef.current && divRef.current){
+        observer = new ResizeObserver(() => {
+          if(divRef.current && containerRef.current) {
+            const totalWidth = containerRef.current.getBoundingClientRect().width - 110
+            console.log('totalWidth', totalWidth);
+            console.log('divWidth', divRef.current.getBoundingClientRect().width)
+            console.log('values', values);
+            console.log('valuesToDisplay', valuesToDisplay);
+            const tempValues = [...values];
+            let tempWidth = 0;
+            let index = 0;
+            const itemWidths = Array.from(
+              divRef.current.children
+            ).map((value: Element) => value.getBoundingClientRect().width);
+
+            while (index < tempValues.length && tempWidth <= totalWidth) {
+              // Get width of each hidden item
+              const itemWidth = itemWidths[index];
+              if((tempWidth + itemWidth) > totalWidth) {
+                break;
+              }
+              tempWidth += itemWidth;
+              index++;
+            }
+
+            // Slice array to get items within calculated width
+            const itemsToFit = tempValues.slice(0, index);
+
+            console.log('itemsToFit', itemsToFit);
+            // If the length of the values to display is different from the length of itemsToFit
+            if(itemsToFit.length !== valuesToDisplay.length) {
+              setValuesToDisplay(itemsToFit);
+            }
+          }
+        });
+
+        observer.observe(containerRef.current);
+      }
+      return () => {
+        observer?.disconnect();
       }
     }
-  }, [valuesToDisplay]);
+    handleResize();
+  }, [containerRef, divRef, values, valuesToDisplay, setValuesToDisplay]);
 
   return (
     <div className={'h-full w-full flex items-center justify-start text-body text-tc-primary gap-[10px]'} ref={containerRef}>
       {
         valuesToDisplay.length > 0 && (
-          <div className={'h-full w-fit flex items-center gap-[10px]'} ref={divRef}>
-            {valuesToDisplay.map((value: string, index: number) => {
+          <div className={'h-full w-auto flex items-center gap-[10px]'}>
+            {
+              valuesToDisplay
+              .map((value: string, index: number) => {
               return (
                 <div
                   key={index}
-                  className={'rounded-[32px] border-[1px] px-[16px] py-[4px] w-auto whitespace-nowrap'}
+                  className={'rounded-[32px] border-[1px] px-[16px] py-[4px] whitespace-nowrap truncate w-auto'}
                   style={{
                     borderColor: color,
+                    maxWidth: MAX_WIDTH,
                   }}
                 >
                   <span className={'text-body'}>{value}</span>
@@ -116,6 +163,35 @@ export const DataGridCellHoverPopover = (props: {
       {
         getExtraContentElement()
       }
+      {/* This div is hidden offscreen and is used to calculate the actual width of the elements*/}
+      <div
+        style={{
+          position: 'absolute',
+          left: '-9999px',
+          // Ensures the items stay in a line and don't wrap to next line
+          whiteSpace: 'nowrap',
+        }}
+        ref={divRef}
+        className={'w-auto flex items-center gap-[10px]'}
+      >
+        {
+          values
+            .map((value: string, index: number) => {
+              return (
+                <div
+                  key={index}
+                  className={'rounded-[32px] border-[1px] px-[16px] py-[4px] whitespace-nowrap truncate w-auto'}
+                  style={{
+                    borderColor: color,
+                    maxWidth: MAX_WIDTH,
+                  }}
+                >
+                  <span className={'text-body'}>{value}</span>
+                </div>
+              );
+            })
+        }
+      </div>
     </div>
   );
 };
