1d2c8aa80a54100969c0d87280e120de415ec07b
Qaalib Farah <qaalib1598@gmail.com>
2024-10-15 10:43:12 -0500

    feat: enhance product mock data with materials and attribute assurances
    - Added `productMaterials` to products
    - Added `attributeAssurances` to products
    - Updated `seasonCode`, `upcCode`, and `styleCode` for products

--- libs/react/src/__mocks__/productsMock.ts
+++ libs/react/src/__mocks__/productsMock.ts
@@ -5,46 +5,108 @@ export const getProductsMock = (): ProductsQuery[] => {
     {
       id: '1',
       name: 'Product 1',
-      productMaterials: [],
-      attributeAssurances: [],
+      productMaterials: [
+        {
+          id: '1',
+          material: {
+            id: '1',
+            name: 'Material 1',
+          }
+        },
+        {
+          id: '2',
+          material: {
+            id: '2',
+            name: 'Material 2',
+          }
+        }
+      ],
+      attributeAssurances: [
+        {
+          id: '1',
+          sustainabilityAttribute: {
+            id: '1',
+            name: 'Sustainability Attribute 1',
+          }
+        },
+        {
+          id: '2',
+          sustainabilityAttribute: {
+            id: '2',
+            name: 'Sustainability Attribute 2',
+          }
+        }
+      ],
       organizationFacility: {
         id: '1',
         name: 'Facility 1',
       },
       metadata: null,
-      seasonCode: null,
+      seasonCode: '123456789',
       upcCode: '123456789',
-      styleCode: null,
+      styleCode: '123456789',
       productCategoryHeirarchy: null,
     },
     {
       id: '2',
       name: 'Product 2',
-      productMaterials: [],
-      attributeAssurances: [],
+      productMaterials: [
+        {
+          id: '1',
+          material: {
+            id: '1',
+            name: 'Material 1',
+          }
+        }
+      ],
+      attributeAssurances: [
+        {
+          id: '1',
+          sustainabilityAttribute: {
+            id: '1',
+            name: 'Sustainability Attribute 1',
+          }
+        }
+      ],
       organizationFacility: {
         id: '1',
         name: 'Facility 1',
       },
       metadata: null,
-      seasonCode: null,
+      seasonCode: '123456789',
       upcCode: '123456789',
-      styleCode: null,
+      styleCode: '123456789',
       productCategoryHeirarchy: null,
     },
     {
       id: '3',
       name: 'Product 3',
-      productMaterials: [],
-      attributeAssurances: [],
+      productMaterials: [
+        {
+          id: '1',
+          material: {
+            id: '1',
+            name: 'Material 1',
+          }
+        }
+      ],
+      attributeAssurances: [
+        {
+          id: '1',
+          sustainabilityAttribute: {
+            id: '1',
+            name: 'Sustainability Attribute 1',
+          }
+        }
+      ],
       organizationFacility: {
         id: '1',
         name: 'Facility 1',
       },
       metadata: null,
-      seasonCode: null,
+      seasonCode: '123456789',
       upcCode: '123456789',
-      styleCode: null,
+      styleCode: '123456789',
       productCategoryHeirarchy: null,
     },
   ]
