b408db16ca04e888c33b7ed0e85e422847bf60fd
Qaalib Farah <qaalib1598@gmail.com>
2024-10-15 09:59:55 -0500

    chore: add story for ProductsPage and update products mock
    - Added `productsPage.stories.tsx` for ProductsPage in Storybook
    - Introduced `withKnobs` decorator to the story
    - Wrapped ProductsPage with `StoryMockProvider`
    - Expanded `getProductsMock` to return objects conforming to `ProductsQuery` interface

--- libs/react/src/__mocks__/productsMock.ts
+++ libs/react/src/__mocks__/productsMock.ts
@@ -1,20 +1,51 @@
+import {ProductsQuery} from "@coldpbc/interfaces";
 
-export const getProductsMock = (): {
-  id: string;
-  name: string;
-}[] => {
+export const getProductsMock = (): ProductsQuery[] => {
   return [
     {
       id: '1',
       name: 'Product 1',
+      productMaterials: [],
+      attributeAssurances: [],
+      organizationFacility: {
+        id: '1',
+        name: 'Facility 1',
+      },
+      metadata: null,
+      seasonCode: null,
+      upcCode: '123456789',
+      styleCode: null,
+      productCategoryHeirarchy: null,
     },
     {
       id: '2',
       name: 'Product 2',
+      productMaterials: [],
+      attributeAssurances: [],
+      organizationFacility: {
+        id: '1',
+        name: 'Facility 1',
+      },
+      metadata: null,
+      seasonCode: null,
+      upcCode: '123456789',
+      styleCode: null,
+      productCategoryHeirarchy: null,
     },
     {
       id: '3',
       name: 'Product 3',
+      productMaterials: [],
+      attributeAssurances: [],
+      organizationFacility: {
+        id: '1',
+        name: 'Facility 1',
+      },
+      metadata: null,
+      seasonCode: null,
+      upcCode: '123456789',
+      styleCode: null,
+      productCategoryHeirarchy: null,
     },
   ]
 }
---
+++ libs/react/src/components/pages/products/productsPage.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from '@storybook/react';
+import {ProductsPage} from '@coldpbc/components';
+import { withKnobs } from '@storybook/addon-knobs';
+import { StoryMockProvider } from '@coldpbc/mocks';
+
+const meta: Meta<typeof ProductsPage> = {
+  title: 'Pages/ProductsPage',
+  component: ProductsPage,
+  tags: ['autodocs'],
+  decorators: [withKnobs],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <StoryMockProvider>
+        <ProductsPage />
+      </StoryMockProvider>
+    );
+  },
+};
