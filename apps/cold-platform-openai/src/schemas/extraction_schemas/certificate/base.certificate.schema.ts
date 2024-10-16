f09d4bc61edc992b97577f616753bdec14220998
Troy Morvant <troy.morvant@gmail.com>
2024-10-16 09:46:18 -0500

    fix: enhance supplier linking and classification logic
    - Link material and supplier in `materials.service.ts`
    - Import `baseMaterialSchema` in `base.certificate.schema.ts`
    - Simplify `default.schema.ts` by removing unnecessary certificate name field
    - Add suppliers logic and extend classification schema in `classification.service.ts`
    - Remove commented-out supplier creation code in `suppliers.service.ts`
    - Handle supplier IDs properly and extend certificate schema in `extraction.service.ts`
    - Update processing logic to include suppliers in `extraction.processor.service.ts`
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-platform-openai/src/schemas/extraction_schemas/certificate/base.certificate.schema.ts
+++ apps/cold-platform-openai/src/schemas/extraction_schemas/certificate/base.certificate.schema.ts
@@ -12,17 +12,12 @@ import {
 	postal_code,
 	country,
 } from '../../global.schema';
+import { baseMaterialSchema } from '../../materials.schema';
 
 export const baseCertificateSchema = z.object({
-	supplier: z.object({
-		name: z.string().describe('Look for the name of the company the certificate is awarded to and place it here if found.  If not found, leave blank'),
-		address_line_1,
-		address_line_2,
-		city,
-		state_province,
-		postal_code,
-		country,
-	}),
+	/*material: baseMaterialSchema
+		.optional()
+		.describe('If the document type is a certificate then look for the material that this certificate if for and place it here if found.  If not found, leave blank'),*/
 	certificate_number: z.string().describe('If the document type is a certificate then look for certificate number and place it here if found.  If not found, leave blank'),
 	effective_start_date: effective_start_date,
 	effective_end_date: effective_end_date,
