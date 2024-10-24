7663114e08ac9f6abd1f4b99ef867d49795f6240
Troy Morvant <troy.morvant@gmail.com>
2024-10-24 15:42:27 -0500

    feat: add Git commit SHA and repository URL to environment variables for tracing
    - Added environment variables `DD_GIT_COMMIT_SHA` and `DD_GIT_REPOSITORY_URL` if `FC_GIT_COMMIT_SHA` is set.
    - Updated tracer initialization to include these environment variables.
    - Added warning logs if `FC_GIT_COMMIT_SHA` is not set in `tracer.ts` files.
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- libs/nest/src/lib/tracer.ts
+++ libs/nest/src/lib/tracer.ts
@@ -4,33 +4,41 @@ import { BaseWorker } from './worker';
 
 console.log({ message: 'setting up tracer...' });
 const config = new ConfigService();
+
+if (!config.get('FC_GIT_COMMIT_SHA')) {
+	console.warn('FC_GIT_COMMIT_SHA is not set');
+} else {
+	process.env.DD_GIT_COMMIT_SHA = config.get('FC_GIT_COMMIT_SHA');
+	process.env.DD_GIT_REPOSITORY_URL = config.get('FC_GIT_REPOSITORY_URL') || 'github.com/ColdPBC/cold-monorepo';
+}
+
 const tracer = Tracer.init({
-  service: config.get('DD_SERVICE') || BaseWorker.getProjectName(),
-  env: config.getOrThrow('NODE_ENV'),
-  version: config.get('DD_VERSION', BaseWorker.getPkgVersion()),
-  logInjection: true,
-  hostname: '127.0.0.1',
-  profiling: true,
-  runtimeMetrics: true,
-  tags: {
-    service: config.getOrThrow('DD_SERVICE') || BaseWorker.getProjectName(),
-    env: config.getOrThrow('NODE_ENV'),
-    version: config.get('DD_VERSION', BaseWorker.getPkgVersion()),
-    environment: config.getOrThrow('NODE_ENV'),
-  },
-  dogstatsd: {
-    hostname: '127.0.0.1',
-    port: 8125,
-  },
-  logLevel: 'debug',
-  plugins: true,
-  dbmPropagationMode: 'full',
-  experimental: { iast: true, runtimeId: true },
-  appsec: { enabled: true, eventTracking: { mode: 'extended' } },
-  remoteConfig: {
-    pollInterval: 5,
-  },
-  clientIpEnabled: true,
+	service: config.get('DD_SERVICE') || BaseWorker.getProjectName(),
+	env: config.getOrThrow('NODE_ENV'),
+	version: config.get('DD_VERSION', BaseWorker.getPkgVersion()),
+	logInjection: true,
+	hostname: '127.0.0.1',
+	profiling: true,
+	runtimeMetrics: true,
+	tags: {
+		service: config.getOrThrow('DD_SERVICE') || BaseWorker.getProjectName(),
+		env: config.getOrThrow('NODE_ENV'),
+		version: config.get('DD_VERSION', BaseWorker.getPkgVersion()),
+		environment: config.getOrThrow('NODE_ENV'),
+	},
+	dogstatsd: {
+		hostname: '127.0.0.1',
+		port: 8125,
+	},
+	logLevel: 'debug',
+	plugins: true,
+	dbmPropagationMode: 'full',
+	experimental: { iast: true, runtimeId: true },
+	appsec: { enabled: true, eventTracking: { mode: 'extended' } },
+	remoteConfig: {
+		pollInterval: 5,
+	},
+	clientIpEnabled: true,
 });
 
 tracer.use('express');
