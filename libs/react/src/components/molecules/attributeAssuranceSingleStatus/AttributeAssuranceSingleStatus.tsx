e8f6724d8351ebdc9ee500857048ef9486a773a6
Jason Mooty <jason.mooty@gmail.com>
2024-10-25 10:38:35 -0500

    fix: expiring message updates from Liz

--- libs/react/src/components/molecules/attributeAssuranceSingleStatus/AttributeAssuranceSingleStatus.tsx
+++ libs/react/src/components/molecules/attributeAssuranceSingleStatus/AttributeAssuranceSingleStatus.tsx
@@ -17,23 +17,21 @@ const _AttributeAssuranceSingleStatus: React.FC<AttributeAssuranceStatusProps> =
   } = getAggregateStatusFromAttributeAssurances(sustainabilityAttribute.attributeAssurances);
 
   let iconName: IconNames;
-  let status: AttributeAssuranceStatus = assuranceStatus;
+  let statusMessage = toSentenceCase(assuranceStatus);
   let statusColorClass: string;
   let subStatusMessage: string;
-  let subStatusColorClass = 'text-tc-disabled';
 
-  switch (status) {
+  switch (assuranceStatus) {
     case AttributeAssuranceStatus.ACTIVE:
       iconName = IconNames.ColdCheckIcon;
       statusColorClass = 'text-green-200';
       subStatusMessage = !assuranceExpiration ? '' : `Expires ${format(assuranceExpiration, 'M/d/yy')}`;
       break;
     case AttributeAssuranceStatus.EXPIRING:
-      iconName = IconNames.ColdCheckIcon;
-      status = AttributeAssuranceStatus.ACTIVE; // use "Active" as label in lieu of "Expiring"
-      statusColorClass = 'text-green-200';
-      subStatusMessage = !assuranceExpiration ? '' : `Expires in ${differenceInDays(assuranceExpiration, new Date())} days`;
-      subStatusColorClass = 'text-yellow-200';
+      iconName = IconNames.ColdExpiringIcon;
+      statusMessage = !assuranceExpiration ? 'Expiring' : `${differenceInDays(assuranceExpiration, new Date())} days`
+      statusColorClass = 'text-yellow-200';
+      subStatusMessage = !assuranceExpiration ? '' : `Expires ${format(assuranceExpiration, 'M/d/yy')}`;
       break;
     case AttributeAssuranceStatus.EXPIRED:
       iconName = IconNames.ColdCalendarCloseIcon;
@@ -58,9 +56,9 @@ const _AttributeAssuranceSingleStatus: React.FC<AttributeAssuranceStatusProps> =
     <div className="w-full flex items-center justify-between">
       <div className="flex items-center gap-2">
         <ColdIcon name={iconName} color="currentColor" className={statusColorClass} />
-        <span className={`text-body ${statusColorClass}`}>{toSentenceCase(status)}</span>
+        <span className={`text-body ${statusColorClass}`}>{statusMessage}</span>
       </div>
-      <span className={`text-body ${subStatusColorClass}`}>{subStatusMessage}</span>
+      <span className='text-body text-tc-disabled'>{subStatusMessage}</span>
     </div>
   );
 };
