6ec11f503be1dbadd61c21599527ac161ad18188
Jason Mooty <jason.mooty@gmail.com>
2024-10-28 13:30:24 -0500

    refactor: extract AttributeAssuranceStatusLabel into its own component

--- libs/react/src/components/molecules/attributeAssuranceSingleStatus/AttributeAssuranceSingleStatus.tsx
+++ libs/react/src/components/molecules/attributeAssuranceSingleStatus/AttributeAssuranceSingleStatus.tsx
@@ -1,10 +1,9 @@
-import { getAggregateStatusFromAttributeAssurances, toSentenceCase } from '@coldpbc/lib';
-import { ColdIcon, ErrorFallback } from '@coldpbc/components';
+import { AttributeAssuranceStatusLabel, ErrorFallback } from '@coldpbc/components';
 import React from 'react';
 import { SustainabilityAttribute, SustainabilityAttributeAssurance } from '@coldpbc/interfaces';
 import { withErrorBoundary } from 'react-error-boundary';
-import { differenceInDays, format } from 'date-fns';
-import { AttributeAssuranceStatus, IconNames } from '@coldpbc/enums';
+import { format } from 'date-fns';
+import { AttributeAssuranceStatus } from '@coldpbc/enums';
 
 interface AttributeAssuranceStatusProps {
   sustainabilityAttribute: SustainabilityAttribute;
@@ -22,36 +21,18 @@ const _AttributeAssuranceSingleStatus: React.FC<AttributeAssuranceStatusProps> =
     attributeAssurance = sustainabilityAttribute.attributeAssurances[0];
   }
 
-  let iconName: IconNames;
-  let statusMessage = toSentenceCase(attributeAssurance.status);
-  let statusColorClass: string;
   let subStatusMessage: string;
 
   switch (attributeAssurance.status) {
     case AttributeAssuranceStatus.ACTIVE:
-      iconName = IconNames.ColdCheckIcon;
-      statusColorClass = 'text-green-200';
-      subStatusMessage = !attributeAssurance.effectiveEndDate ? '' : `Expires ${format(attributeAssurance.effectiveEndDate, 'M/d/yy')}`;
-      break;
     case AttributeAssuranceStatus.EXPIRING:
-      iconName = IconNames.ColdExpiringIcon;
-      statusMessage = !attributeAssurance.effectiveEndDate ? 'Expiring' : `${differenceInDays(attributeAssurance.effectiveEndDate, new Date())} days`
-      statusColorClass = 'text-yellow-200';
       subStatusMessage = !attributeAssurance.effectiveEndDate ? '' : `Expires ${format(attributeAssurance.effectiveEndDate, 'M/d/yy')}`;
       break;
     case AttributeAssuranceStatus.EXPIRED:
-      iconName = IconNames.ColdCalendarCloseIcon;
-      statusColorClass = 'text-gray-200';
       subStatusMessage = !attributeAssurance.effectiveEndDate ? '' : `Expired on ${format(attributeAssurance.effectiveEndDate, 'M/d/yy')}`;
       break;
     case AttributeAssuranceStatus.MISSING_DATE:
-      iconName = IconNames.ColdUnknownIcon;
-      statusColorClass = 'text-gray-200';
-      subStatusMessage = 'Expiration Unknown';
-      break;
     case AttributeAssuranceStatus.NOT_DOCUMENTED:
-      iconName = IconNames.ColdDangerIcon;
-      statusColorClass = 'text-red-200';
       subStatusMessage = 'Expiration Unknown';
       break;
     default:
@@ -60,10 +41,7 @@ const _AttributeAssuranceSingleStatus: React.FC<AttributeAssuranceStatusProps> =
 
   return (
     <div className="w-full flex items-center justify-between">
-      <div className="flex items-center gap-2">
-        <ColdIcon name={iconName} color="currentColor" className={statusColorClass} />
-        <span className={`text-body ${statusColorClass}`}>{statusMessage}</span>
-      </div>
+      <AttributeAssuranceStatusLabel status={attributeAssurance.status} effectiveEndDate={attributeAssurance.effectiveEndDate} />
       <span className='chromatic-ignore text-body text-tc-disabled'>{subStatusMessage}</span>
     </div>
   );
