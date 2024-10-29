b894490880c77dbb5bbef7ebc985182bcf117ca9
Jason Mooty <jason.mooty@gmail.com>
2024-10-29 10:50:15 -0500

    chore: remove FF showCreateSupplierPageCold1014

--- libs/react/src/components/application/routes/supplierRoutes.tsx
+++ libs/react/src/components/application/routes/supplierRoutes.tsx
@@ -8,7 +8,7 @@ export const SupplierRoutes = () => {
   return (
 		<>
 			{ldFlags.showSuppliersPageCold890 && <Route path={'/suppliers'} element={<SuppliersPage />} />}
-			{ldFlags.showCreateSupplierPageCold1014 && <Route path={'/suppliers/new'} element={<CreateSupplierPage />} />}
+			<Route path={'/suppliers/new'} element={<CreateSupplierPage />} />
 		</>
 	);
 }
