619b38d0094e1065f270458233645d5ac7bd63f3
Jason Mooty <jason.mooty@gmail.com>
2024-10-30 10:56:50 -0500

    feat: query for supplier on SupplierDetail and present breadcrumbs, name

--- libs/react/src/components/pages/supplierDetail/supplierDetail.tsx
+++ libs/react/src/components/pages/supplierDetail/supplierDetail.tsx
@@ -1,14 +1,51 @@
-import { ErrorFallback } from '@coldpbc/components';
 import React from 'react';
+import { ErrorFallback, ErrorPage, MainContent, Spinner } from '@coldpbc/components';
 import { withErrorBoundary } from 'react-error-boundary';
+import { useColdContext, useGraphQLSWR } from '@coldpbc/hooks';
+import { useParams } from 'react-router-dom';
+import { SupplierGraphQL } from '@coldpbc/interfaces';
+import { get, isError } from 'lodash';
 
 export const _SupplierDetail = () => {
-  return null;
+	const { id: supplierId } = useParams();
+	const { logBrowser } = useColdContext();
+	const supplierQuery = useGraphQLSWR<{
+		organizationFacility: SupplierGraphQL | null;
+	}>('GET_SUPPLIER', {
+		id: supplierId,
+	});
+
+	if (supplierQuery.isLoading) {
+		return <Spinner />;
+	}
+
+	if (isError(supplierQuery.data)) {
+		const supplierError = get(supplierQuery.data, 'error', null);
+		if (supplierError) {
+			logBrowser('Error fetching supplier', 'error', { supplierError }, supplierError);
+		}
+
+		return <ErrorPage error={'An error occurred'} showLogout={false} />;
+	}
+
+	const supplier = get(supplierQuery.data, 'data.organizationFacility');
+
+	if (supplier === null || supplier === undefined) {
+		return null;
+	}
+
+	return (
+		<MainContent
+			title={supplier.name}
+			subTitle={`Tier ${supplier.supplierTier}`}
+			breadcrumbs={[{ label: 'Suppliers', href: '/suppliers' }, { label: supplier.name }]}
+			className={'w-[calc(100%)]'}></MainContent>
+	);
 };
 
 export const SupplierDetail = withErrorBoundary(_SupplierDetail, {
-  FallbackComponent: props => <ErrorFallback {...props} />,
-  onError: (error, info) => {
-    console.error('Error occurred in SupplierDetail: ', error);
-  },
+	FallbackComponent: props => <ErrorFallback {...props} />,
+	onError: (error, info) => {
+		console.error('Error occurred in SupplierDetail: ', error);
+	},
 });
