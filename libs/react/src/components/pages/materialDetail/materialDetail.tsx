358e1954bc40ee8c3733c6eb7eabee4fd0a66ab6
Jason Mooty <jason.mooty@gmail.com>
2024-10-30 09:33:48 -0500

    feat: query for material on MaterialPage and present breadcrumbs, name

--- libs/react/src/components/pages/materialDetail/materialDetail.tsx
+++ libs/react/src/components/pages/materialDetail/materialDetail.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
-import { ErrorFallback } from '@coldpbc/components';
+import { ErrorFallback, ErrorPage, MainContent, Spinner } from '@coldpbc/components';
 import { withErrorBoundary } from 'react-error-boundary';
+import { useColdContext, useGraphQLSWR } from '@coldpbc/hooks';
+import { useParams } from 'react-router-dom';
+import { MaterialGraphQL } from '@coldpbc/interfaces';
+import { get, isError } from 'lodash';
 
 const _MaterialDetail: React.FC = () => {
-  return null;
+	const { id: materialId } = useParams();
+	const { logBrowser } = useColdContext();
+	const materialQuery = useGraphQLSWR<{
+		material: MaterialGraphQL | null;
+	}>('GET_MATERIAL', {
+		id: materialId,
+	});
+
+	if (materialQuery.isLoading) {
+		return <Spinner />;
+	}
+
+	if (isError(materialQuery.data)) {
+		const materialError = get(materialQuery.data, 'error', null);
+		if (materialError) {
+			logBrowser('Error fetching Material', 'error', { materialError }, materialError);
+		}
+
+		return <ErrorPage error={'An error occurred'} showLogout={false} />;
+	}
+
+	const material = get(materialQuery.data, 'data.material');
+
+	if (material === null || material === undefined) {
+		return null;
+	}
+
+	const subTitle = [material.materialCategory, material.materialSubcategory].filter(val => !!val).join(' | ');
+
+	return (
+		<MainContent
+			title={material.name}
+			subTitle={subTitle}
+			breadcrumbs={[{ label: 'Material', href: '/materials' }, { label: material.name }]}
+			className={'w-[calc(100%)]'}></MainContent>
+	);
 };
 
 export const MaterialDetail = withErrorBoundary(_MaterialDetail, {
-  FallbackComponent: props => <ErrorFallback {...props} />,
+	FallbackComponent: props => <ErrorFallback {...props} />,
 });
