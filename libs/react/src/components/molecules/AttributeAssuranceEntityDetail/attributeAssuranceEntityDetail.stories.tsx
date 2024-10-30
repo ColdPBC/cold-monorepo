c11820e9058fa699f27f4783ea60799a5aee6d3d
Jason Mooty <jason.mooty@gmail.com>
2024-10-30 15:23:03 -0500

    chore: update stories to include one specific to suppliers page

--- libs/react/src/components/molecules/AttributeAssuranceEntityDetail/attributeAssuranceEntityDetail.stories.tsx
+++ libs/react/src/components/molecules/AttributeAssuranceEntityDetail/attributeAssuranceEntityDetail.stories.tsx
@@ -26,6 +26,25 @@ export const Default: Story = {
       ],
       level: EntityLevel.MATERIAL,
     },
+    displayedOnEntityLevel: EntityLevel.PRODUCT,
+    expanded: false,
+    onClick: () => {},
+  }
+};
+
+export const MaterialsOnASuppliersPage: Story = {
+  args: {
+    sustainabilityAttribute: {
+      id: 'a',
+      name: 'Default Material Attribute',
+      attributeAssurances: [
+        ...Array(7).fill(null).map((_, index) => AttributeAssuranceMock({ entity: EntityLevel.MATERIAL, status: AttributeAssuranceStatus.ACTIVE, index })),
+        ...Array(8).fill(null).map((_, index) => AttributeAssuranceMock({ entity: EntityLevel.MATERIAL, status: AttributeAssuranceStatus.EXPIRED, index: (index + 7) })),
+        ...Array(12).fill(null).map((_, index) => AttributeAssuranceMock({ entity: EntityLevel.MATERIAL, status: AttributeAssuranceStatus.NOT_DOCUMENTED, index: (index + 15) }))
+      ],
+      level: EntityLevel.MATERIAL,
+    },
+    displayedOnEntityLevel: EntityLevel.SUPPLIER,
     expanded: false,
     onClick: () => {},
   }
@@ -39,6 +58,7 @@ export const EmptyList: Story = {
       attributeAssurances: [],
       level: EntityLevel.MATERIAL,
     },
+    displayedOnEntityLevel: EntityLevel.PRODUCT,
     expanded: false,
     onClick: () => {},
   }
@@ -52,6 +72,7 @@ export const OnlyNotDocumented: Story = {
       attributeAssurances: Array(10).fill(null).map((_, index) => AttributeAssuranceMock({ entity: EntityLevel.SUPPLIER, status: AttributeAssuranceStatus.NOT_DOCUMENTED, index })),
       level: EntityLevel.SUPPLIER,
     },
+    displayedOnEntityLevel: EntityLevel.PRODUCT,
     expanded: false,
     onClick: () => {},
   }
