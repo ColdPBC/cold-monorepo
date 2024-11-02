cf0a20fa660c9f6ece58f9122ed07fe53475f9ae
Troy Morvant <troy.morvant@gmail.com>
2024-11-01 22:21:42 -0500

    fix: remove unused Rollup alias plugin in Vite configs
    - Removed alias plugin from `libs/react/vite.config.ts`
    - Standardized indentation in Vite config files
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-ui/vite.config.ts
+++ apps/cold-ui/vite.config.ts
@@ -5,57 +5,58 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/cold-ui',
-  server: {
-    port: 4200,
-    host: 'localhost',
-    fs: {
-      // Allow serving files from one level up to the project root
-      allow: [searchForWorkspaceRoot(process.cwd()), '../../libs/react'],
-    },
-  },
+	root: __dirname,
+	cacheDir: '../../node_modules/.vite/cold-ui',
+	server: {
+		port: 4200,
+		host: 'localhost',
+		fs: {
+			// Allow serving files from one level up to the project root
+			allow: [searchForWorkspaceRoot(process.cwd()), '../../libs/react'],
+		},
+	},
 
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-  plugins: [
-    react(),
-    nxViteTsPaths(),
-    nodePolyfills({
-      // To exclude specific polyfills, add them to this list.
-      exclude: [],
-      // Whether to polyfill `node:` protocol imports.
-      protocolImports: true,
-    }),
-  ],
-  build: {
-    manifest: true,
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
-  },
+	preview: {
+		port: 4300,
+		host: 'localhost',
+	},
+	plugins: [
+		react(),
+		nxViteTsPaths(),
+		nodePolyfills({
+			// To exclude specific polyfills, add them to this list.
+			exclude: [],
+			// Whether to polyfill `node:` protocol imports.
+			protocolImports: true,
+		}),
+	],
+	build: {
+		manifest: true,
+		sourcemap: true,
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return id.toString().split('node_modules/')[1].split('/')[0].toString();
+					}
+				},
+			},
+		},
+	},
 
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
+	// Uncomment this if you are using workers.
+	// worker: {
+	//  plugins: [ nxViteTsPaths() ],
+	// },
 
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  test: {
-    globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest',
-    },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	test: {
+		globals: true,
+		cache: {
+			dir: '../../node_modules/.vitest',
+		},
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+	},
 });
--- libs/react/vite.config.ts
+++ libs/react/vite.config.ts
@@ -7,73 +7,55 @@ import * as path from 'path';
 import alias from '@rollup/plugin-alias';
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/react',
-  plugins: [
-    dts({
-      entryRoot: 'src',
-    }),
-    react(),
-    nxViteTsPaths(),
-  ],
+	cacheDir: '../../node_modules/.vite/react',
+	plugins: [
+		dts({
+			entryRoot: 'src',
+		}),
+		react(),
+		nxViteTsPaths(),
+	],
 
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
+	// Uncomment this if you are using workers.
+	// worker: {
+	//  plugins: [ nxViteTsPaths() ],
+	// },
 
-  // Configuration for building your library.
-  // See: https://vitejs.dev/guide/build.html#library-mode
-  build: {
-    sourcemap: true,
-    lib: {
-      // Could also be a dictionary or array of multiple entry points.
-      entry: 'src/index.ts',
-      name: 'react',
-      fileName: 'library',
-      // Change this to the formats you want to support.
-      // Don't forget to update your package.json as well.
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      plugins: [
-        alias({
-          entries: {
-            '@coldpbc/animations': path.resolve(__dirname, './src/animations/index.ts'),
-            '@coldpbc/assets': path.resolve(__dirname, './src/assets/index.ts'),
-            '@coldpbc/components': path.resolve(__dirname, './src/components'),
-            '@coldpbc/context': path.resolve(__dirname, './src/context'),
-            '@coldpbc/enums': path.resolve(__dirname, './src/enums'),
-            '@coldpbc/fetchers': path.resolve(__dirname, './src/fetchers'),
-            '@coldpbc/hooks': path.resolve(__dirname, './src/hooks'),
-            '@coldpbc/interfaces': path.resolve(__dirname, './src/interfaces'),
-            '@coldpbc/lib': path.resolve(__dirname, './src/lib'),
-            '@coldpbc/mocks': path.resolve(__dirname, './src/__mocks__'),
-            '@coldpbc/providers': path.resolve(__dirname, './src/providers'),
-            '@coldpbc/routes': path.resolve(__dirname, './src/routes'),
-            '@coldpbc/styles': path.resolve(__dirname, './src/styles'),
-            '@coldpbc/themes': path.resolve(__dirname, './src/themes'),
-          },
-        }),
-      ],
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-      // External packages that should not be bundled into your library.
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
-    },
-  },
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  test: {
-    globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest',
-    },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
+	// Configuration for building your library.
+	// See: https://vitejs.dev/guide/build.html#library-mode
+	build: {
+		sourcemap: true,
+		lib: {
+			// Could also be a dictionary or array of multiple entry points.
+			entry: 'src/index.ts',
+			name: 'react',
+			fileName: 'library',
+			// Change this to the formats you want to support.
+			// Don't forget to update your package.json as well.
+			formats: ['es', 'cjs'],
+		},
+		rollupOptions: {
+			plugins: [],
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return id.toString().split('node_modules/')[1].split('/')[0].toString();
+					}
+					return null;
+				},
+			},
+			// External packages that should not be bundled into your library.
+			external: ['react', 'react-dom', 'react/jsx-runtime'],
+		},
+	},
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	test: {
+		globals: true,
+		cache: {
+			dir: '../../node_modules/.vitest',
+		},
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+	},
 });
