b281c88d5cac18c3a0f638527bc6b2bbccd1316e
Troy Morvant <troy.morvant@gmail.com>
2024-11-13 10:05:41 -0600

    fix: enhance classification logic and schema
    - Refactor classification schema with detailed document classification rules
    - Add `author`, `reasoning`, and `sustainability_attributes` to classification schema
    - Extend classification logic for better document authorship and type determination
    - Update classification prompts with detailed content and steps
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-platform-openai/src/classification/classification.service.ts
+++ apps/cold-platform-openai/src/classification/classification.service.ts
@@ -13,17 +13,16 @@ import { OpenAiBase64ImageUrl } from '../pinecone/pinecone.service';
 export class ClassificationService extends BaseWorker {
 	private openAi;
 	sus_attributes: sustainability_attributes[];
-
 	classificationSchema = z.object({
-		type: z.nativeEnum(file_types).describe('The type of document'),
-		testing_company: z.enum(['intertek', 'tuvRheinland', 'SGS', 'other']).describe('The name of the testing company'), // move this to the extraction service
-		// sustainability_attribute is added in the init method
+		type: z.nativeEnum(file_types).describe('The type classification of document'),
+		reasoning: z.string().describe('Provide detailed reasoning for the classification'),
+		summary: summary,
 		extraction_prompt: z
 			.string()
+			.optional()
 			.describe(
 				'Generate a prompt to instruct the model on how to accurately extract data from the document.  The purpose of this prompt is to extract data from the document.  Make sure you include instructions that the model is NOT to invent or hallucinate any data.  All data must be extracted from the document precisely as it appears in the document and the number of parameters must be less than 100.  Do not include more than 100 parameters in the prompt.',
 			),
-		summary: summary,
 	});
 
 	constructor(readonly config: ConfigService, private readonly prisma: PrismaService, readonly s3: S3Service, readonly mqtt: MqttService) {
@@ -60,33 +59,84 @@ export class ClassificationService extends BaseWorker {
 
 		// extend existing schema to add sustainability attributes
 		this.classificationSchema = this.classificationSchema.extend({
-			type: z.nativeEnum(file_types).describe(`
-			Classify the type of document from the list of file_types defined in this schema.  Do not use any other file type that is not listed here.  If you are unsure of the file type then consider these additional rules:
-			- Restricted Substance Lists should be classified as a POLICY
-      - Only classify a certificate as a SCOPE_CERTIFICATE if 'Scope' is found in the content.
-      - Only classify a certificate as a TRANSACTION_CERTIFICATE if 'Transaction Certificate' is found in the content.
-      - if the content is a statement , classify it as a STATEMENT
-      - if the content is a bill of materials or BOM, classify it as a BILL_OF_MATERIALS.  If it does not contain 'BOM' or 'Bill Of Materials' anywhere in the content, then DO NOT CLASSIFY IT AS A BILL_OF_MATERIALS.
-      - if the document is a purchase order or list of purchase orders, classify it as a PURCHASE_ORDER.  A purchase order must have a "PO NUMBER", and Ship To information.  If it does not contain 'Purchase Order' anywhere in the content, then DO NOT CLASSIFY IT AS A PURCHASE_ORDER
-      - if the content is an impact assessment from ${organization.display_name}, classify it as a STATEMENT
-      
-      Otherwise do your best to classify the document type from the list of file_types defined in this schema.
-      `),
+			type: z.nativeEnum(file_types).describe('The type of document'),
+			author: z.string().describe('The author of the document'),
+			sustainability_attributes: z
+				.array(z.string())
+				.describe(
+					`Select all sustainability attributes that matches any standards referenced in the document according to the following list: ${attributes.join(
+						', ',
+					)}.  It is important that you do not use any other sustainability attribute that is not listed here.`,
+				),
 			sustainability_attribute: z
 				.string()
 				.describe(
-					`You are a helpful assistant who has expert knowledge in sustainability compliance and manufacturing processes of both products and materials. Select the sustainability attribute that best matches the document according to the following list: Unknown, ${attributes.join(
+					`Select the sustainability attribute that best matches the purpose of the document according to the following list: Unknown, ${attributes.join(
 						', ',
 					)}.  It is important that you do not use any other sustainability attribute that is not listed here.`,
 				),
 		});
 
-		return `You are a helpful assistant for ${organization.display_name} who has expert knowledge in sustainability compliance and manufacturing processes of both products and materials.
-    `;
+		return `Classify the document type using the following rules and steps:
+
+Definition of terms:
+	- supplier: A company or individual from the following list that provides materials.
+	- the brand: ${organization.display_name}; The organization that owns the brand.
+	- certifying_entity: An organization that is NOT a supplier and is not ${organization.display_name} that provides compliance documentation.
+	
+Rules for Classification:
+  1. Authorship Rules:
+    - In determining authorship, consider the following in order:
+        1. The logo or letterhead of the document.
+        2. Company name located in the footer or header.
+        3. The body of the document.
+    - If a document is authored by the ${organization.display_name} then only the following types are valid: SUPPLIER_AGREEMENT, POLICY, PURCHASE_ORDER, BILL_OF_MATERIALS, or OTHER.
+    - If a document is authored by a 'supplier' then it must be classified as a SUPPLIER_STATEMENT.
+    - If a document is not authored a 'supplier' or ${organization.display_name} then only the following types are valid: AUDIT_REPORT, TEST_REPORT, SCOPE_CERTIFICATE, TRANSACTION_CERTIFICATE, or OTHER.
+  2. STATEMENT:
+    - MUST be authored by ${organization.display_name}.  If it is not authored by ${organization.display_name} then it is not a STATEMENT.
+    - MUST not contain any mention of a specific supplier, purchase orders, or bill of materials.
+	3. SUPPLIER_STATEMENT:
+		-	Use if the document is authored by a supplier and declares compliance or non-compliance with specific practices, standards, or regulations.
+		- It should be assumed that all suppliers provide materials in the production of goods under the ${organization.display_name} brand.
+		- Do not classify as ‘SUPPLIER_STATEMENT’ if the document is authored by ${organization.display_name}.
+	4. OTHER:
+		- Classify as ‘OTHER’ only if no other category applies after thoroughly reviewing all options.
+		- Example: Use for ambiguous or unclassifiable documents that do not align with any defined category.
+	5. Review your classification to ensure it is accurate and aligns with the document’s content.
+
+Steps for Classification:
+
+	1.	Identify Authorship:
+				•	Examine the document to determine if it is authored by:
+				•	A supplier or supplier representative
+				•	${organization.display_name}.
+				•	Another entity that is not a supplier or ${organization.display_name}.
+	2.	Assess Content Purpose:
+		  Determine whether the document is:
+				•	Authored by ${organization.display_name} and is intended to be an agreement between ${organization.display_name} and a supplier (SUPPLIER_AGREEMENT).
+				•	Authored by supplier and not by ${organization.display_name} declares compliance or non-compliance (SUPPLIER_STATEMENT).
+				•	Authored by ${organization.display_name} and defines organizational guidelines, standards, or restricted substance lists (POLICY).
+				•	Authored by ${organization.display_name} and explicitly declares that it is a purchase order or PO (PURCHASE_ORDER).
+				•	Authored by ${organization.display_name} and explicitly declares that it is a bill of materials or BOM (BILL_OF_MATERIALS).
+				•	Authored by another entity and contains reports on inspections, or audits,(AUDIT_REPORT).
+				•	Authored by another entity and contains results from material or product testing (TEST_REPORT).
+				•	Authored by another entity and explicitly declares that it is a scope certificate (SCOPE_CERTIFICATE).
+				•	Authored by another entity and explicitly declares that it is a transaction certificate (TRANSACTION_CERTIFICATE).
+	3.	Match Content to Categories:
+				•	Review the document against all category definitions. Prioritize categories based on:
+				•	Content intent (e.g., compliance declaration fits SUPPLIER_STATEMENT).
+				•	Specificity (e.g., only use OTHER if no precise match exists).
+	4.	Exclude Incorrect Categories:
+				•	Ensure the document does not fit restricted or invalid categories:
+				•	Example: Avoid using ‘CERTIFICATE’ as it is explicitly not allowed.
+	5.	Apply the Fallback Rule:
+				•	Use ‘OTHER’ only as a last resort. If the document fits SUPPLIER_STATEMENT, do not classify it as ‘OTHER.’
+	`;
 	}
 
 	async classifyImageUrls(images: OpenAiBase64ImageUrl[], user: IAuthenticatedUser, orgFile: organization_files, organization: organizations) {
-		const classifyPrompt = `${this.getClassifyPrompt(organization)} from the provided image urls.`;
+		const classifyPrompt = `${await this.getClassifyPrompt(organization)} from the provided image urls.`;
 
 		// add the classification prompt to the beginning of the images array
 		images.unshift({
@@ -105,8 +155,6 @@ export class ClassificationService extends BaseWorker {
 			response_format: zodResponseFormat(this.classificationSchema, 'content_classification'),
 		});
 
-		set(classifyResponse.choices[0].message?.parsed, 'attributes', this.sus_attributes);
-
 		const suppliers = (await this.prisma.organization_facilities.findMany({
 			where: {
 				organization_id: organization.id,
@@ -118,7 +166,12 @@ export class ClassificationService extends BaseWorker {
 			},
 		})) as organization_facilities[];
 
-		set(classifyResponse.choices[0].message?.parsed, 'suppliers', suppliers.map(k => k.name) as readonly string[]);
+		// remove the classification prompt from the beginning of the images array
+		images.shift();
+
+		await set(classifyResponse.choices[0].message?.parsed, 'suppliers', suppliers.map(k => k.name) as readonly string[]);
+		await set(classifyResponse.choices[0].message?.parsed, 'attributes', this.sus_attributes);
+		await set(classifyResponse.choices[0].message?.parsed, 'images', images);
 
 		return classifyResponse.choices[0].message?.parsed;
 	}
@@ -139,8 +192,8 @@ export class ClassificationService extends BaseWorker {
 			const start = new Date();
 
 			const classifyPrompt = this.contentIsUrl(content)
-				? `${this.getClassifyPrompt(organization)} from the following image urls`
-				: `${this.getClassifyPrompt(organization)} from the following context: ${Array.isArray(content) ? content.join(' ') : content}`;
+				? `${await this.getClassifyPrompt(organization)} from the following image urls`
+				: `${await this.getClassifyPrompt(organization)} \n\n Content to be classified: ${Array.isArray(content) ? content.join(' ') : content}`;
 
 			const messageContent: { type: string; text?: string; image_url?: { url: string } }[] = [
 				{
@@ -160,7 +213,6 @@ export class ClassificationService extends BaseWorker {
 				},
 			];
 
-			// send to openAI for classification
 			const classifyResponse = await this.openAi.beta.chat.completions.parse({
 				model: 'gpt-4o-2024-08-06',
 				messages: [
