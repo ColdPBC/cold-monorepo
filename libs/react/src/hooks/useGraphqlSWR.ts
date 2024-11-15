2446f8cbdc845a3e78cc62ffff403a4c3a7923be
Jason Mooty <jason.mooty@gmail.com>
2024-11-15 13:47:00 -0600

    fix: graphql needs to have variables in key to know new page is new query

--- libs/react/src/hooks/useGraphqlSWR.ts
+++ libs/react/src/hooks/useGraphqlSWR.ts
@@ -4,42 +4,40 @@ import { ColdApolloContext } from '@coldpbc/providers';
 import { ApolloQueryResult } from '@apollo/client';
 import { queries } from '@coldpbc/lib';
 import { get } from 'lodash';
-import {useColdContext} from "./useColdContext";
+import { useColdContext } from './useColdContext';
 
 export const useGraphQLSWR = <Data = any, Error = any>(key: string | null, variables?: any, config?: SWRConfiguration) => {
-  const {logBrowser} = useColdContext()
-  const { client } = useContext(ColdApolloContext);
+	const { logBrowser } = useColdContext();
+	const { client } = useContext(ColdApolloContext);
 
-  return useSWR(
-    key,
-    (key: string) => {
-      const query = get(queries, key, null);
-      if (query) {
-        return client
-					?.query({
-						query: query,
-						variables: variables,
-					})
-					.then(response => {
-						logBrowser(`Successful fetching graphql query ${key}`, 'info', {
-							query: key,
-							variables,
-							response,
-						});
-						return response;
-					})
-					.catch(error => {
-            logBrowser(`Error fetching graphql query ${key}`, 'error', {
-              query: key,
-              variables,
-              error,
-            })
-						return error;
-					});
-      } else {
-        return undefined;
-      }
-    },
-    config,
-  ) as SWRResponse<ApolloQueryResult<Data>, Error, any>;
+	return useSWR(
+		key ? [key, JSON.stringify(variables)] : null,
+		async ([queryKey]) => {
+			const query = get(queries, queryKey, null);
+			if (!query || !client) return undefined;
+
+			try {
+				const response = await client.query({
+					query,
+					variables,
+				});
+
+				logBrowser(`Successful fetching graphql query ${queryKey}`, 'info', {
+					query: queryKey,
+					variables,
+					response,
+				});
+
+				return response;
+			} catch (error) {
+				logBrowser(`Error fetching graphql query ${queryKey}`, 'error', {
+					query: queryKey,
+					variables,
+					error,
+				});
+				throw error;
+			}
+		},
+		config,
+	) as SWRResponse<ApolloQueryResult<Data>, Error, any>;
 };
