879449892cf29912aaf78396f8cd78273b79754b
Jason Mooty <jason.mooty@gmail.com>
2024-11-20 12:35:58 -0600

    feat: new product carbon footprint graphs

--- libs/react/src/components/organisms/productDetailsTab/productDetailsTab.tsx
+++ libs/react/src/components/organisms/productDetailsTab/productDetailsTab.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { ErrorFallback, ProductDetailsCard, ProductSustainabilityAttributesCard } from '@coldpbc/components';
+import {
+  ErrorFallback,
+  ProductCarbonFootprintDonut,
+  ProductDetailsCard,
+  ProductSustainabilityAttributesCard,
+} from '@coldpbc/components';
 import type { ProductsQuery } from '@coldpbc/interfaces';
 import { withErrorBoundary } from 'react-error-boundary';
 
@@ -10,12 +15,15 @@ interface ProductDetailsTabProps {
 
 const _ProductDetailsTab: React.FC<ProductDetailsTabProps> = ({ product, setShowUpdateAttributesModal }) => {
   return (
-    <div className='w-full h-full flex gap-6 items-start mt-4 mb-20'>
-      <ProductDetailsCard product={product} />
-      <ProductSustainabilityAttributesCard
-        product={product}
-        setShowUpdateAttributesModal={setShowUpdateAttributesModal}
-      />
+    <div className='w-full h-full flex flex-col gap-10 mt-4 mb-20'>
+      <ProductCarbonFootprintDonut product={product} />
+      <div className='w-full h-full flex gap-6 items-start'>
+        <ProductDetailsCard product={product} />
+        <ProductSustainabilityAttributesCard
+          product={product}
+          setShowUpdateAttributesModal={setShowUpdateAttributesModal}
+        />
+      </div>
     </div>
   );
 };
