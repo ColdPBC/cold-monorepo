5001abb3c166be609fff3fbf2dc6bf174c880c52
Troy Morvant <troy.morvant@gmail.com>
2024-12-03 14:56:11 -0600

    fix: enhance error handling in AppService initialization
    - Added error handling for service registration failures with custom exceptions
    - Updated package.json parsing method in `onModuleInit`
    - Log and handle 404 and client errors with appropriate exception messages
    - Introduced `handleError` method for centralized error processing
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-platform-climatiq/src/app.service.ts
+++ apps/cold-platform-climatiq/src/app.service.ts
@@ -1,17 +1,37 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, NotFoundException, OnModuleInit, UnprocessableEntityException } from '@nestjs/common';
 import { BaseWorker, ColdRabbitService } from '@coldpbc/nest';
+import { service_definitions } from '@prisma/client';
 
 @Injectable()
 export class AppService extends BaseWorker implements OnModuleInit {
-  constructor(private rabbit: ColdRabbitService) {
-    super(AppService.name);
-  }
+	service: service_definitions;
 
-  async onModuleInit(): Promise<void> {
-    const pkg = await BaseWorker.getParsedJSON('apps/cold-platform-climatiq/package.json');
+	constructor(private rabbit: ColdRabbitService) {
+		super(AppService.name);
+	}
 
-    await this.rabbit.register_service(pkg.service);
+	async onModuleInit(): Promise<void> {
+		const pkg = await BaseWorker.getParsedJSON('package.json', false);
 
-    this.logger.log('Climatiq Service initialized');
-  }
+		try {
+			this.service = (await this.rabbit.register_service(pkg.service)) as service_definitions;
+		} catch (e) {
+			this.logger.error(e.message, e);
+			try {
+				this.logger.warn('Unable to register service with RabbitMQ, retrieved service definition from database.');
+			} catch (e) {
+				this.handleError(e);
+			}
+		}
+	}
+
+	handleError(e, meta?) {
+		if (e.status === 404) {
+			throw new NotFoundException(e.message);
+		}
+		if (e.status > 399 && e.status < 500) {
+			this.logger.error(e.message, meta);
+			throw new UnprocessableEntityException(e.message);
+		}
+	}
 }
