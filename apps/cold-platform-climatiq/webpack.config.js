370d5d58d34ebc38f31157cd123d676c9ed1ae16
Troy Morvant <troy.morvant@gmail.com>
2024-12-03 14:37:08 -0600

    chore: update build configurations and ESLint rules
    - Modified webpack config to set entry point and enable source maps.
    - Added build and lint plugins in nx.json.
    - Disabled TypeScript 'no-explicit-any' rule in ESLint config.
    - Enhanced nest-cli.json with project configurations and compiler options.
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-platform-climatiq/webpack.config.js
+++ apps/cold-platform-climatiq/webpack.config.js
@@ -2,7 +2,9 @@ const { composePlugins, withNx } = require('@nx/webpack');
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), config => {
-  // Update the webpack config as needed here.
-  // e.g. `config.plugins.push(new MyPlugin())`
-  return config;
+	// Update the webpack config as needed here.
+	// e.g. `config.plugins.push(new MyPlugin())`
+	config.entry = './src/main.ts';
+	config.devtool = 'inline-source-map';
+	return config;
 });
