2d4aa772157df8be20426a122c4b12ca84d4ee74
Jason Mooty <jason.mooty@gmail.com>
2024-12-04 10:02:42 -0600

    design feedback: reduce padding on dropdowns (note: this will probably have broader storybook impact)

--- libs/react/src/components/molecules/dropdown/dropdown.tsx
+++ libs/react/src/components/molecules/dropdown/dropdown.tsx
@@ -24,7 +24,7 @@ export const Dropdown = (props: DropdownProps) => {
         props.onSelect(option.value);
       }}>
       <div className={twMerge('relative w-auto mt-1', props.containerClassName)}>
-        <Listbox.Button className="relative w-full flex flex-row justify-between cursor-default rounded-lg bg-transparent p-[16px] text-left text-tc-secondary text-body border-[1.5px] border-bgc-accent active:border-primary">
+        <Listbox.Button className="relative w-full flex flex-row justify-between cursor-default rounded-lg bg-transparent p-[12px] text-left text-tc-secondary text-body border-[1.5px] border-bgc-accent active:border-primary">
           <span className="block truncate">{props.options.find(option => option.value === props.selected)?.label}</span>
           <span className="pointer-events-none flex items-center p-[8px] w-[24px] h-[24px]">
             <ColdIcon name={IconNames.ColdChevronDownIcon} />
@@ -33,7 +33,7 @@ export const Dropdown = (props: DropdownProps) => {
         <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
           <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-lg bg-bgc-elevated py-1 text-body text-tc-secondary border-[1.5px] border-bgc-accent z-10">
             {props.options.map((option, index) => (
-              <Listbox.Option key={index} className={({ active }) => `relative cursor-pointer select-none p-[16px] border-[0] text-left hover:bg-bgc-accent`} value={option}>
+              <Listbox.Option key={index} className={({ active }) => `relative cursor-pointer select-none p-[12px] border-[0] text-left hover:bg-bgc-accent`} value={option}>
                 {({ selected }) => <span className={`block truncate`}>{option.label}</span>}
               </Listbox.Option>
             ))}
