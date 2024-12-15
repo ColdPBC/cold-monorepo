a4f74a3f66645b25dfee9b2bf438666bb52c6d28
Troy Morvant <troy.morvant@gmail.com>
2024-12-15 16:03:19 -0600

    chore: update dependencies in yarn.lock
    - Removed unused or outdated dependencies (`@apollo/client@3.11.10`, `@apollo/server@4.11.2`, and others).
    - Updated versions and checksums for multiple dependencies (e.g., `@aws-cdk/asset-awscli-v1@2.2.214`, `@babel/core@7.26.0`).
    - Added or re-added updated dependencies (`@aws-sdk/client-secrets-manager@3.699.0`).
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- libs/nest/src/lib/authorization/auth0-token.service.ts
+++ libs/nest/src/lib/authorization/auth0-token.service.ts
@@ -7,127 +7,127 @@ import { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { get, set } from 'lodash';
 
 export interface Auth0APIOptions {
-  baseURL: string;
-  headers: { 'content-type': string; Authorization: string | null };
-  validateStatus: any;
+	baseURL: string;
+	headers: { 'content-type': string; Authorization: string | null };
+	validateStatus: any;
 }
 
 @Injectable()
 export class Auth0TokenService extends BaseWorker implements OnModuleInit {
-  tokenBody: {
-    client_id: string;
-    client_secret: string;
-    audience: string;
-    grant_type: string;
-    scope?: string;
-  };
-  expiresAt: number = 0;
+	tokenBody: {
+		client_id: string;
+		client_secret: string;
+		audience: string;
+		grant_type: string;
+		scope?: string;
+	};
+	expiresAt = 0;
 
-  options: {
-    baseURL: string;
-    headers: { 'content-type': string; Authorization: string | null };
-    validateStatus: any;
-  };
+	options: {
+		baseURL: string;
+		headers: { 'content-type': string; Authorization: string | null };
+		validateStatus: any;
+	};
 
-  httpService: HttpService;
+	httpService: HttpService;
 
-  constructor(private cache: CacheService, private config: ConfigService) {
-    super(Auth0TokenService.name);
-    this.httpService = new HttpService();
+	constructor(private cache: CacheService, private config: ConfigService) {
+		super(Auth0TokenService.name);
+		this.httpService = new HttpService();
 
-    this.tokenBody = {
-      client_id: this.config['internalConfig']['AUTH0_CLIENT_ID'],
-      client_secret: this.config['internalConfig']['AUTH0_CLIENT_SECRET'],
-      audience: `https://${this.config['internalConfig']['AUTH0_DOMAIN']}/api/v2/`,
-      grant_type: 'client_credentials',
-    };
+		this.tokenBody = {
+			client_id: this.config['internalConfig']['AUTH0_CLIENT_ID'],
+			client_secret: this.config['internalConfig']['AUTH0_CLIENT_SECRET'],
+			audience: `https://${this.config['internalConfig']['AUTH0_DOMAIN']}/api/v2/`,
+			grant_type: 'client_credentials',
+		};
 
-    this.options = {
-      baseURL: `https://${this.config['internalConfig']['AUTH0_DOMAIN']}/api/v2`,
-      headers: { 'content-type': 'application/json', Authorization: null },
-      validateStatus: (status: any) => {
-        return status >= 200 && status < 400;
-      },
-    };
-  }
+		this.options = {
+			baseURL: `https://${this.config['internalConfig']['AUTH0_DOMAIN']}/api/v2`,
+			headers: { 'content-type': 'application/json', Authorization: null },
+			validateStatus: (status: any) => {
+				return status >= 200 && status < 400;
+			},
+		};
+	}
 
-  override async onModuleInit() {
-    await this.init();
-  }
+	override async onModuleInit() {
+		await this.init();
+	}
 
-  /**
-   * Initialize Auth0 Management API
-   * @returns {Promise<AxiosRequestConfig<any>>}
-   * @constructor
-   * @throws Error
-   */
-  async init(): Promise<AxiosRequestConfig<any>> {
-    try {
-      if (this.cache) {
-        let token: any = await this.cache.get(`auth0-management-token-${process.env['NODE_ENV']}`);
+	/**
+	 * Initialize Auth0 Management API
+	 * @returns {Promise<AxiosRequestConfig<any>>}
+	 * @constructor
+	 * @throws Error
+	 */
+	async init(): Promise<AxiosRequestConfig<any>> {
+		try {
+			if (this.cache) {
+				let token: any = await this.cache.get(`auth0-management-token-${process.env['NODE_ENV']}`);
 
-        if (!token || token['data']['expiresAt'] < new Date().getMilliseconds() / 1000) {
-          this.logger.error('Token Expired', { token, compared_to: new Date().getMilliseconds() / 1000 });
-          token = await this.getManagementToken();
-        }
+				if (!token || token['data']['expiresAt'] < new Date().getMilliseconds() / 1000) {
+					this.logger.error('Token Expired', { token, compared_to: new Date().getMilliseconds() / 1000 });
+					token = await this.getManagementToken();
+				}
 
-        await this.setOptions(token as AxiosResponse);
-      }
+				await this.setOptions(token as AxiosResponse);
+			}
 
-      if (!this.options.headers.Authorization || this.expiresAt < new Date().getSeconds()) {
-        //this.logger = new WorkerLogger(Auth0UtilityService.name);
-        await this.getManagementToken();
-        return this.options;
-      }
+			if (!this.options.headers.Authorization || this.expiresAt < new Date().getSeconds()) {
+				//this.logger = new WorkerLogger(Auth0UtilityService.name);
+				await this.getManagementToken();
+				return this.options;
+			}
 
-      return this.options;
-    } catch (e: any) {
-      if (e.response) {
-        this.logger.error(e.response?.data?.message, { error: e.response?.data });
-      }
-      this.logger.error(e, { error: e.response });
+			return this.options;
+		} catch (e: any) {
+			if (e.response) {
+				this.logger.error(e.response?.data?.message, { error: e.response?.data });
+			}
+			this.logger.error(e, { error: e.response });
 
-      throw e;
-    }
-  }
+			throw e;
+		}
+	}
 
-  /***
-   * Get Management Token from Auth0
-   */
-  async getManagementToken() {
-    const tokenBody = Object.assign({}, this.tokenBody);
-    tokenBody.client_id = this.config['internalConfig']['AUTH0_CLIENT_ID'] || '';
+	/***
+	 * Get Management Token from Auth0
+	 */
+	async getManagementToken() {
+		const tokenBody = Object.assign({}, this.tokenBody);
+		tokenBody.client_id = this.config['internalConfig']['AUTH0_CLIENT_ID'] || '';
 
-    const tokenResponse = await this.httpService.axiosRef.post(`https://${this.config['internalConfig']['AUTH0_DOMAIN']}/oauth/token`, tokenBody, this.options);
+		const tokenResponse = await this.httpService.axiosRef.post(`https://${this.config['internalConfig']['AUTH0_DOMAIN']}/oauth/token`, tokenBody, this.options);
 
-    if (tokenResponse.data['access_token']) {
-      this.logger.info('Requested Auth0 Management Token');
-      await this.setOptions(tokenResponse);
+		if (tokenResponse.data['access_token']) {
+			this.logger.info('Requested Auth0 Management Token');
+			await this.setOptions(tokenResponse);
 
-      if (this.cache) {
-        await this.cache.set(
-          `auth0-management-token-${process.env['NODE_ENV']}`,
-          { data: tokenResponse.data },
-          {
-            ttl: 1000 * 60 * 60 * 5,
-            update: true,
-          },
-        );
-      }
+			if (this.cache) {
+				await this.cache.set(
+					`auth0-management-token-${process.env['NODE_ENV']}`,
+					{ data: tokenResponse.data },
+					{
+						ttl: 1000 * 60 * 60 * 5,
+						update: true,
+					},
+				);
+			}
 
-      return tokenResponse as AxiosResponse;
-    } else {
-      this.logger.error('Unable to get Auth0 Management Token', { error: tokenResponse.data });
-      throw new Error('Unable to get Auth0 Management Token');
-    }
-  }
+			return tokenResponse as AxiosResponse;
+		} else {
+			this.logger.error('Unable to get Auth0 Management Token', { error: tokenResponse.data });
+			throw new Error('Unable to get Auth0 Management Token');
+		}
+	}
 
-  async setOptions(tokenResponse: AxiosResponse<any>) {
-    this.options.headers.Authorization = `Bearer ${get(tokenResponse, 'data.access_token', get(tokenResponse, 'access_token'))}`;
-    //auth0-management-token
-    this.expiresAt = new Date().setSeconds(get(tokenResponse, 'data.expires_in', get(tokenResponse, 'expires_in')));
-    set(tokenResponse, 'data.expiresAt', this.expiresAt);
+	async setOptions(tokenResponse: AxiosResponse<any>) {
+		this.options.headers.Authorization = `Bearer ${get(tokenResponse, 'data.access_token', get(tokenResponse, 'access_token'))}`;
+		//auth0-management-token
+		this.expiresAt = new Date().setSeconds(get(tokenResponse, 'data.expires_in', get(tokenResponse, 'expires_in')));
+		set(tokenResponse, 'data.expiresAt', this.expiresAt);
 
-    return this.options;
-  }
+		return this.options;
+	}
 }
