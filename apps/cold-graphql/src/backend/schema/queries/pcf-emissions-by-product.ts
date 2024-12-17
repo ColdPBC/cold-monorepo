84e874e6520bda8281cc4d3b56e7ab855cd5b877
Troy Morvant <troy.morvant@gmail.com>
2024-12-17 10:49:23 -0600

    feat: implement cache_pcf_emissions query to enhance emission data processing
    - Added `cache_pcf_emissions` function to process and cache emission data for products and categories.
    - Replaced `pcfEmissionsByProduct` query with `cache_pcf_emissions` for improved functionality.
    - Used `uniqBy` to identify distinct product categories.
    - Enhanced error handling and logging for missing products or emissions.
    - Updated GraphQL query configuration to reflect new `cache_pcf_emissions` functionality.
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-graphql/src/backend/schema/queries/pcf-emissions-by-product.ts
+++ apps/cold-graphql/src/backend/schema/queries/pcf-emissions-by-product.ts
@@ -3,11 +3,99 @@ import { getConnection } from '../../database.config';
 import { MikroBackendProvider } from '@exogee/graphweaver-mikroorm';
 import { Product } from '../product';
 import { OrganizationProductMaterialEmissions } from '../views/organization-product-material-emissions';
-import { set } from 'lodash';
+
 import { GraphQLJSONObject } from '@exogee/graphweaver-scalars';
+import { uniqBy } from 'lodash';
+
+export async function cache_pcf_emissions(options: ResolverOptions) {
+	try {
+		const emissionProvider = new MikroBackendProvider(OrganizationProductMaterialEmissions, getConnection());
+
+		const categories: any = [];
+
+		const filter = {
+			organization: {
+				id: options.args.organizationId,
+			},
+		};
+
+		const productProvider = new MikroBackendProvider(Product, getConnection());
+
+		const products = await productProvider.find(filter);
+
+		if (!products || products.length < 1) {
+			return { error: 'No products found' };
+		}
+
+		const distinctCategories = uniqBy(products, 'productCategory').map((category: any) => category.productCategory);
+
+		for (const category of distinctCategories) {
+			const productsInCategory = products.filter((product: any) => product.productCategory === category);
+
+			const categoryEmissions = await emissionProvider.find({
+				productCategory: category ? category : null,
+				organizationId: options.args.oprganizationId,
+			});
+
+			const totalCategoryEmissionsFactor = categoryEmissions.reduce((sum, emission: any) => {
+				if (emission.emissionsFactor) {
+					return sum + emission.emissionsFactor || 0;
+				}
+
+				return sum;
+			}, 0);
+
+			const averageCategoryEmissionsFactor = totalCategoryEmissionsFactor / productsInCategory.length;
+
+			const emission_stats = {
+				category,
+				products_in_category: productsInCategory.length,
+				total_category_c02e: totalCategoryEmissionsFactor,
+				average_category_c02e: averageCategoryEmissionsFactor,
+			};
+
+			categories.push(emission_stats);
+
+			for (const product of productsInCategory) {
+				const productEmissions = categoryEmissions.filter((emission: any) => {
+					if (emission.product.id === product.id) {
+						return emission;
+					}
+
+					return null;
+				});
+
+				const pcf = productEmissions.reduce((sum, emission: any) => sum + emission.emissionsFactor || 0, 0);
+
+				if (!productEmissions || productEmissions.length < 1) {
+					console.log(`No emissions found for ${product.name}(id: ${product.id})`);
+				}
+
+				const stats = {
+					total_product_c02e: pcf,
+					...emission_stats,
+				};
+
+				console.log(`Updating emission stats for ${product.name}(id: ${product.id})`, stats);
+
+				await productProvider.em.upsert('Product', {
+					id: product.id,
+					emissionStats: pcf ? stats : undefined,
+				});
+			}
+
+			await productProvider.em.flush();
+		}
+
+		return { emissions: categories };
+	} catch (e) {
+		console.error(e);
+		throw e;
+	}
+}
 
 graphweaverMetadata.addQuery({
-	name: 'pcfEmissionsByProduct',
+	name: 'cache_pcf_emissions',
 	getType: () => GraphQLJSONObject,
 	intentionalOverride: true,
 	args: {
@@ -19,61 +107,8 @@ graphweaverMetadata.addQuery({
 		productId: {
 			type: () => String,
 			description: 'Product ID',
-			nullable: false,
+			nullable: true,
 		},
 	},
-	resolver: async options => {
-		try {
-			const productProvider = new MikroBackendProvider(Product, getConnection());
-			const product = await productProvider.findOne({
-				organization: {
-					id: options.args.organizationId,
-				},
-				id: options.args.productId,
-			});
-
-			if (!product?.id) {
-				return product;
-			}
-
-			const emissionProvider = new MikroBackendProvider(OrganizationProductMaterialEmissions, getConnection());
-
-			const emissions = await emissionProvider.find({
-				organizationId: options.args.organizationId,
-				productId: product.id,
-			});
-
-			const category = product.productCategory;
-
-			const totalEmissionsFactor = emissions.reduce((sum, emission: any) => sum + emission.emissionsFactor || 0, 0);
-
-			const categoryEmissions = await emissionProvider.find({
-				productCategory: category,
-				organizationId: options.args.oprganizationId,
-			});
-
-			let categoryEmissionEntries = 0;
-			const totalCategoryEmissionsFactor = categoryEmissions.reduce((sum, emission: any) => {
-				if (emission.emissionsFactor) {
-					categoryEmissionEntries++;
-					return sum + emission.emissionsFactor || 0;
-				}
-
-				return sum;
-			}, 0);
-
-			const averageCategoryEmissionsFactor = totalCategoryEmissionsFactor / categoryEmissionEntries;
-
-			return {
-				total_product_c02e: totalEmissionsFactor,
-				product_category: category,
-				products_in_category: categoryEmissions.length,
-				total_category_c02e: totalCategoryEmissionsFactor,
-				average_category_c02e: averageCategoryEmissionsFactor,
-			};
-		} catch (e) {
-			console.error(e);
-			throw e;
-		}
-	},
+	resolver: async options => await cache_pcf_emissions(options),
 });
