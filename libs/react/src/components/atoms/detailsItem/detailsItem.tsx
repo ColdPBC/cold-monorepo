93bcc2b52a26a7e7957ec7037805235eb39c050a
Jason Mooty <jason.mooty@gmail.com>
2024-12-19 10:17:50 -0600

    chore: details item can have a CTA

--- libs/react/src/components/atoms/detailsItem/detailsItem.tsx
+++ libs/react/src/components/atoms/detailsItem/detailsItem.tsx
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { withErrorBoundary } from 'react-error-boundary';
 import { ErrorFallback } from '../../application';
+import { ColdIcon } from '@coldpbc/components';
+import { IconNames } from '@coldpbc/enums';
 
 interface DetailsItemProps {
-  category: string;
+  category: string | ReactNode;
   value: string | null | undefined;
   href?: string;
+  cta?: () => void;
+  ctaIconName?: IconNames;
 }
 
-const _DetailsItem: React.FC<DetailsItemProps> = ({ category, value, href }) => {
+const _DetailsItem: React.FC<DetailsItemProps> = ({ category, value, href, cta, ctaIconName }) => {
   const labelText = value || 'None';
-  const label = href ? <a href={href} className='hover:underline'>{labelText}</a> : labelText;
+  const label = href ? (
+    <a href={href} className="hover:underline">
+      {labelText}
+    </a>
+  ) : (
+    labelText
+  );
 
   return (
     <div className="w-full h-fit flex flex-col gap-2 items-start">
-      <span className='text-eyebrow text-tc-disabled'>{category}</span>
-      <span className='text-body text-white text-wrap'>{label}</span>
+      <div className="text-eyebrow text-tc-disabled">{category}</div>
+      <div className="w-full h-fit flex items-center justify-start gap-2">
+        <span className="text-body text-white text-wrap">{label}</span>
+        {cta && ctaIconName && <ColdIcon name={ctaIconName} onClick={cta} />}
+      </div>
     </div>
   );
 };
 
-
 export const DetailsItem = withErrorBoundary(_DetailsItem, {
-  FallbackComponent: props => <ErrorFallback {...props} />,
-  onError: (error, info) => {
-    console.error('Error occurred in DetailsItem: ', error);
-  },
+	FallbackComponent: props => <ErrorFallback {...props} />,
+	onError: (error, info) => {
+		console.error('Error occurred in DetailsItem: ', error);
+	},
 });
