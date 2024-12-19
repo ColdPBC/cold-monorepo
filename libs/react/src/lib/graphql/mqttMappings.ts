b29cc331442935eeaa5e9ccb12185d6bc4173401
Qaalib Farah <qaalib1598@gmail.com>
2024-12-19 11:59:38 -0600

    fix: update `isOrgKey` logic to include `organizationId` and adjust query mapping filtering
    - Updated `isOrgKey` to account for `organizationId` in addition to `organization`.
    - Modified query mapping filtering logic to consider `organizationId` alongside `organization` for cache keys.

--- libs/react/src/lib/graphql/mqttMappings.ts
+++ libs/react/src/lib/graphql/mqttMappings.ts
@@ -5,14 +5,14 @@ export const mqttMappings: {
   attribute_assurances: ['GET_ALL_FILES', 'GET_ALL_SUS_ATTRIBUTES', 'GET_ALL_MATERIALS_TO_ADD_ASSURANCE_TO_DOCUMENT', 'GET_ALL_SUPPLIERS_TO_ADD_ASSURANCE_TO_DOCUMENT'],
 };
 
-const isOrgKey = (key: string, orgId: string) => key.includes('organization') && key.includes(orgId);
+const isOrgKey = (key: string, orgId: string) => (key.includes('organization') || key.includes('organizationId')) && key.includes(orgId);
 
 const hasQuery = (key: string, query: string) => key.includes(query);
 
 export const getQueryMappingsForKey = (key: string, cacheKeys: string[], orgId: string): string[] => {
-  // Check the swr key against the mqttMappings object. the key example is 'organization_files.uploaded'.
-  // Get the first word separated by a dot.
   const firstPart = key.split('.')[0];
   const queryKeys = mqttMappings[firstPart] || [];
-  return queryKeys.flatMap(query => cacheKeys.filter(cacheKey => (isOrgKey(cacheKey, orgId) || !cacheKey.includes('organization')) && hasQuery(cacheKey, query)));
+  return queryKeys.flatMap(query =>
+		cacheKeys.filter(cacheKey => (isOrgKey(cacheKey, orgId) || !(cacheKey.includes('organization') || cacheKey.includes('organizationId'))) && hasQuery(cacheKey, query)),
+	);
 };
