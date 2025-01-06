a381faafbe28b6107c6cfe53125adf81e2402c88
Troy Morvant <troy.morvant@gmail.com>
2025-01-06 15:34:50 -0600

    feat: enhance MaterialHooks with product emission stats caching
    - Replaced `cache_pcf_emissions` with `cache_product_emission_stats` for updated emission caching logic.
    - Introduced `get_product_args_from_material` to retrieve product information based on materials.
    - Updated `afterCreateHook` to cache product-related emission stats.
    - Enabled `afterUpdateHook` and `afterDeleteHook` to utilize `cache_product_emission_stats`.
    - Removed unused imports and refactored dependencies for clarity.
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-graphql/src/backend/entities/hooks/material.hooks.ts
+++ apps/cold-graphql/src/backend/entities/hooks/material.hooks.ts
@@ -1,10 +1,9 @@
 // Material Hooks
-import { CreateOrUpdateHookParams, ReadHookParams, DeleteHookParams, ResolverOptions } from '@exogee/graphweaver';
+import { CreateOrUpdateHookParams, ReadHookParams, DeleteHookParams } from '@exogee/graphweaver';
 import { BaseSidecar } from '../base.sidecar';
 import { OrgContext } from '../../libs/acls/acl_policies';
-import { Material } from '../postgresql';
-import { cache_pcf_emissions } from '../../services/emissions/cache_pcf_emissions';
-import { MqttActionEnum, MqttStatusEnum } from '../../libs/mqtt/mqtt.service';
+import { Material, Product } from '../postgresql';
+import { cache_product_emission_stats, get_product_args_from_material } from '../services/product_emission_stats';
 
 export class MaterialHooks extends BaseSidecar {
 	constructor() {
@@ -20,6 +19,20 @@ export class MaterialHooks extends BaseSidecar {
 	// Overrride AfterCreateHook here:
 	override async afterCreateHook(params: CreateOrUpdateHookParams<Material, OrgContext>) {
 		//await this.cache_emissions(params);
+		const materials = params?.entities as Material[];
+		if (!materials || materials.length < 1) {
+			return params;
+		}
+
+		const products = await get_product_args_from_material(materials[0]);
+		const prodParams: CreateOrUpdateHookParams<any, OrgContext> = {
+			context: params.context as OrgContext,
+			args: params.args,
+			entities: products,
+			fields: params.fields,
+			transactional: false,
+		};
+		await cache_product_emission_stats(prodParams);
 		await super.afterCreateHook(params);
 
 		return params;
@@ -29,8 +42,8 @@ export class MaterialHooks extends BaseSidecar {
 
 	// Overrride AfterUpdateHook here:
 	override async afterUpdateHook(params: CreateOrUpdateHookParams<Material, OrgContext>) {
-		//await cache_pcf_emissions(params);
-		//await super.afterUpdateHook(params);
+		await cache_product_emission_stats(params);
+		await super.afterUpdateHook(params);
 
 		return params;
 	}
@@ -38,8 +51,8 @@ export class MaterialHooks extends BaseSidecar {
 
 	// Overrride AfterDeleteHook here:
 	override async afterDeleteHook(params: DeleteHookParams<Material, OrgContext>) {
-		//await cache_pcf_emissions(params);
-		//await super.afterDeleteHook(params);
+		await cache_product_emission_stats(params);
+		await super.afterDeleteHook(params);
 
 		return params;
 	}
