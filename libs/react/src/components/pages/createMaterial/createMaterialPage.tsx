d345afd6810dd11c67572b5a376d121be8d3863c
Qaalib Farah <qaalib1598@gmail.com>
2025-01-15 12:45:08 -0600

    feat: Add validation for material name and improve UI error handling
    Introduced a validation function to check material name requirements, including uniqueness, and integrated error feedback into the UI. Added new states and queries to fetch existing materials and handle errors. Enhanced form interactions by displaying error messages for invalid inputs and improving spacing and button loading behaviors.

--- libs/react/src/components/pages/createMaterial/createMaterialPage.tsx
+++ libs/react/src/components/pages/createMaterial/createMaterialPage.tsx
@@ -32,10 +32,6 @@ const _CreateMaterialPage = () => {
 	const { orgId } = useAuth0Wrapper();
   const navigate = useNavigate();
 
-  const isFormValid = (materialState: MaterialCreate) => {
-    return materialState.name.trim() !== '';
-  }
-
   const placeHolderOption: InputOption = {
     id: -1,
     name: 'Select one',
@@ -61,6 +57,12 @@ const _CreateMaterialPage = () => {
   const [saveButtonLoading, setSaveButtonLoading] = useState(false);
   const [showCancelModal, setShowCancelModal] = useState(false);
   const [createModalType, setCreateModalType] = useState<'attributes' | undefined>(undefined);
+  const [errors, setErrors] = useState<Partial<Record<keyof MaterialCreate, string>>>({});
+  const [otherMaterials, setOtherMaterials] = useState<{
+    id: string;
+    name: string;
+  }[]>([]);
+
   const {mutateGraphQL: createMaterial} = useGraphQLMutation('CREATE_MATERIAL');
   const {mutateGraphQL: createAttributeAssurance} = useGraphQLMutation('CREATE_ATTRIBUTE_ASSURANCE_FOR_FILE');
   const {mutateGraphQL: createMaterialSupplier} = useGraphQLMutation('CREATE_MATERIAL_SUPPLIER');
@@ -89,9 +91,18 @@ const _CreateMaterialPage = () => {
     materialClassifications: {id: string; name: string;}[];
   }>('GET_ALL_MATERIAL_CLASSIFICATIONS');
 
+  const otherMaterialsQuery = useGraphQLSWR<{
+    materials: {
+      id: string;
+      name: string;
+    }[];
+  }>('GET_ALL_MATERIALS_TO_ADD_ASSURANCE_TO_DOCUMENT', {
+    organizationId: orgId,
+  });
+
   useEffect(() => {
-    setSaveButtonDisabled(!isFormValid(materialState));
-  }, [materialState]);
+    setSaveButtonDisabled(Object.values(errors).some(error => error !== null && error !== undefined));
+  }, [errors]);
 
 	useEffect(() => {
 		if (suppliersQuery.data) {
@@ -126,10 +137,20 @@ const _CreateMaterialPage = () => {
     }
   }, [materialClassificationsQuery.data]);
 
-  if (suppliersQuery.isLoading || allSustainabilityAttributes.isLoading || materialClassificationsQuery.isLoading) {
+  useEffect(() => {
+    if (otherMaterialsQuery.data) {
+      if (has(otherMaterialsQuery.data, 'errors')) {
+        setOtherMaterials([]);
+      } else {
+        const materials = get(otherMaterialsQuery.data, 'data.materials', []);
+        setOtherMaterials(materials);
+      }
+    }
+  }, [otherMaterialsQuery.data]);
+
+  if (suppliersQuery.isLoading || allSustainabilityAttributes.isLoading || materialClassificationsQuery.isLoading || otherMaterialsQuery.isLoading) {
 		return <Spinner />;
 	}
-
 
 	const supplierOptions: InputOption[] = suppliers.map((supplier, index) => {
 		return {
@@ -244,6 +265,7 @@ const _CreateMaterialPage = () => {
           disabled={saveButtonDisabled || saveButtonLoading}
           loading={saveButtonLoading}
           className={'h-[40px]'}
+          data-testid={'save_button'}
         />
       </div>
     )
@@ -253,6 +275,22 @@ const _CreateMaterialPage = () => {
     return attributes.filter(attribute => {
       return !some(attributesToAdd, { id: attribute.id, name: attribute.name });
     });
+  }
+
+  const validateName = (
+    name: string,
+    otherMaterials: {
+      id: string
+      name: string
+    }[]
+  ) => {
+    if(name.trim() === '') {
+      return 'Material name is required';
+    } else if(otherMaterials.some((material) => material.name === name)) {
+      return 'Material name already exists';
+    } else {
+      return undefined;
+    }
   }
 
 	return (
@@ -281,6 +319,12 @@ const _CreateMaterialPage = () => {
                   ...materialState,
                   name: e.target.value,
                 });
+                setErrors((prev) => {
+                  return {
+                    ...prev,
+                    name: validateName(e.target.value, otherMaterials),
+                  }
+                })
               },
               onValueChange: e => {
                 setMaterialState({
@@ -290,6 +334,8 @@ const _CreateMaterialPage = () => {
               },
               className: 'text-body p-4 rounded-[8px] border-[1.5px] border-gray-90 w-full focus:border-[1.5px] focus:border-gray-90 focus:ring-0',
               placeholder: '',
+              error: errors.name,
+              showError: true,
             }}
             container_classname={'w-full'}
             input_label_props={{
@@ -315,6 +361,7 @@ const _CreateMaterialPage = () => {
               },
               className: 'text-body p-4 rounded-[8px] border-[1.5px] border-gray-90 w-full focus:border-[1.5px] focus:border-gray-90 focus:ring-0',
               placeholder: '',
+              showError: true,
             }}
             container_classname={'w-full'}
             input_label_props={{
@@ -322,7 +369,7 @@ const _CreateMaterialPage = () => {
             }}
             input_label={'Description'}
           />
-          <div className={'flex flex-col gap-[8px] w-full'}>
+          <div className={'flex flex-col gap-[8px] w-full mb-5'}>
             <div className={'text-eyebrow'}>Supplier</div>
             <ComboBox
               options={[placeHolderOption, ...supplierOptions]}
@@ -331,7 +378,7 @@ const _CreateMaterialPage = () => {
               onChange={option => setSupplier(option)}
             />
           </div>
-          <div className={'flex flex-col gap-[8px] w-full'}>
+          <div className={'flex flex-col gap-[8px] w-full mb-5'}>
             <div className={'text-eyebrow'}>Material Classification</div>
             <ComboBox
               options={[placeHolderOption, ...materialClassifications.map((classification, index) => {
@@ -364,6 +411,7 @@ const _CreateMaterialPage = () => {
               },
               className: 'text-body p-4 rounded-[8px] border-[1.5px] border-gray-90 w-full focus:border-[1.5px] focus:border-gray-90 focus:ring-0',
               placeholder: '',
+              showError: true,
             }}
             container_classname={'w-full'}
             input_label_props={{
@@ -389,6 +437,7 @@ const _CreateMaterialPage = () => {
               },
               className: 'text-body p-4 rounded-[8px] border-[1.5px] border-gray-90 w-full focus:border-[1.5px] focus:border-gray-90 focus:ring-0',
               placeholder: '',
+              showError: true,
             }}
             container_classname={'w-full'}
             input_label_props={{
@@ -414,6 +463,7 @@ const _CreateMaterialPage = () => {
               },
               className: 'text-body p-4 rounded-[8px] border-[1.5px] border-gray-90 w-full focus:border-[1.5px] focus:border-gray-90 focus:ring-0',
               placeholder: '',
+              showError: true,
             }}
             container_classname={'w-full'}
             input_label_props={{
@@ -439,6 +489,7 @@ const _CreateMaterialPage = () => {
               },
               className: 'text-body p-4 rounded-[8px] border-[1.5px] border-gray-90 w-full focus:border-[1.5px] focus:border-gray-90 focus:ring-0',
               placeholder: '',
+              showError: true,
             }}
             container_classname={'w-full'}
             input_label_props={{
@@ -447,13 +498,21 @@ const _CreateMaterialPage = () => {
             input_label={'Supplier Material Id'}
           />
         </Card>
-        <Card className={'flex-col w-1/2 gap-[32px] self-start'} title={'Sustainability Attributes'} glow={true}>
-          <BaseButton
-            label={'Add'}
-            iconLeft={IconNames.PlusIcon}
-            variant={ButtonTypes.secondary}
-            onClick={() => setCreateModalType('attributes')}
-          />
+        <Card
+          className={'flex-col w-1/2 gap-[32px] self-start'}
+          title={'Sustainability Attributes'}
+          glow={true}
+          ctas={[
+            {
+              child: <BaseButton
+                label={'Add'}
+                iconLeft={IconNames.PlusIcon}
+                variant={ButtonTypes.secondary}
+                onClick={() => setCreateModalType('attributes')}
+              />,
+            }
+          ]}
+        >
           <CreateEntityTable
             type={'attributes'}
             remove={(id) => {
