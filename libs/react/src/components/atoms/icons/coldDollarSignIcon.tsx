2fdee821597e80efc1b6274b6fea3c91932cc70d
Qaalib Farah <qaalib1598@gmail.com>
2025-01-22 11:10:57 -0600

    chore: Update coldDollarSignIcon to support dynamic stroke color
    Replaced the hardcoded white stroke with a dynamic color from props, defaulting to white. This enhances flexibility and allows customization of the icon's appearance.

--- libs/react/src/components/atoms/icons/coldDollarSignIcon.tsx
+++ libs/react/src/components/atoms/icons/coldDollarSignIcon.tsx
@@ -6,7 +6,7 @@ export const ColdDollarSignIcon = (props: IconProps) => {
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
       <path
         d="M16 9.00021C16 7 15 6 12 6M12 6C9 6 8 7 8 9C8 11 9 12 12 12C15 12 16 13 16 15C16 17 14 18 12 18M12 6V4M12 18C10 18 8 17 8 15M12 18V20"
-        stroke="white" strokeWidth="1.5" strokeLinecap="round" />
+        stroke={props.color || "white"} strokeWidth="1.5" strokeLinecap="round" />
     </svg>
   )
 };
