1e1a130fc84027e890fbd0c32229793f941123f6
Jason Mooty <jason.mooty@gmail.com>
2025-01-22 14:48:59 -0600

    feat: ability to edit supplier details

--- libs/react/src/components/atoms/dropdownInputForEntityEdit/dropdownInputForEntityEdit.tsx
+++ libs/react/src/components/atoms/dropdownInputForEntityEdit/dropdownInputForEntityEdit.tsx
@@ -1,18 +1,35 @@
 import { ComboBox } from '@coldpbc/components';
 import React, { ReactNode, useMemo } from 'react';
 
-interface DropdownInputForEntityEditProps<T> {
+type EntityOption = { id: string; name: string };
+type ValueOption = { value: number };
+
+type BaseProps<T> = {
 	fieldName: keyof T;
+	fieldType?: 'object' | 'value';
 	label: string | ReactNode;
-	options: { id: string; name: string }[];
 	setEntityState: (state: T) => void;
 	entityState: T;
-  setError: (error?: string) => void;
-  error?: string;
-  originalEntity: T;
-  allowNone?: boolean;
+	setError: (error?: string) => void;
+	error?: string;
+	originalEntity: T;
+	allowNone?: boolean;
 	required?: boolean;
-}
+	disabled?: boolean;
+	disabledMessage?: string;
+};
+
+type ObjectProps<T> = BaseProps<T> & {
+	fieldType: 'object';
+	options: EntityOption[];
+};
+
+type ValueProps<T> = BaseProps<T> & {
+	fieldType: 'value';
+	options: ValueOption[];
+};
+
+type DropdownInputProps<T> = ObjectProps<T> | ValueProps<T>;
 
 export const DropdownInputForEntityEdit = <T,>({
 	fieldName,
@@ -20,34 +37,37 @@ export const DropdownInputForEntityEdit = <T,>({
 	options,
 	setEntityState,
 	entityState,
-  error,
-  setError,
-  originalEntity,
-  allowNone = false,
-	required = false
-}: DropdownInputForEntityEditProps<T>) => {
-  const dropdownOptions = useMemo(() => {
-    const noneOption = { id: -1, value: '', name: 'None' };
-    const defaultOption = { id: -1, value: '', name: 'Select an option' };
+	error,
+	setError,
+	originalEntity,
+	allowNone = false,
+	required = false,
+	fieldType = 'object',
+	disabled = false,
+	disabledMessage = '',
+}: DropdownInputProps<T>) => {
+	const dropdownOptions = useMemo(() => {
+		const noneOption = { id: -1, value: '', name: 'None' };
+		const defaultOption = { id: -1, value: '', name: 'Select an option' };
 
-    const formattedOptions = options
-      .sort((a, b) => a.name.localeCompare(b.name))
-      .map(((option, index) => ({
-        id: index,
-        value: option.id,
-        name: option.name,
-      })));
+		const formattedOptions = options
+			.map((option, index) => ({
+				id: index,
+				value: fieldType === 'object' ? (option as EntityOption).id || (option as EntityOption).name : String((option as ValueOption).value),
+				name: fieldType === 'object' ? (option as EntityOption).name : String((option as ValueOption).value),
+			}))
+			.sort((a, b) => a.name.localeCompare(b.name));
 
-    if (allowNone) {
-      return [noneOption, ...formattedOptions];
-    } else if (!originalEntity[fieldName]) {
-      return [defaultOption, ...formattedOptions];
-    } else {
-      return formattedOptions;
-    }
-  }, [allowNone, originalEntity, fieldName, options]);
+		if (allowNone) {
+			return [noneOption, ...formattedOptions];
+		} else if (!originalEntity[fieldName]) {
+			return [defaultOption, ...formattedOptions];
+		} else {
+			return formattedOptions;
+		}
+	}, [allowNone, originalEntity, fieldName, options, fieldType]);
 
-  return (
+	return (
 		<div className={'flex flex-col w-full h-full justify-between gap-4'}>
 			<div className="w-full h-fit flex flex-col gap-2 items-start">
 				<div className={'text-eyebrow text-tc-primary'}>
@@ -58,37 +78,46 @@ export const DropdownInputForEntityEdit = <T,>({
 						</span>
 					</div>
 				</div>
-        <div className={'w-full'}>
-          <ComboBox
-            options={dropdownOptions}
-            value={dropdownOptions.find(option => {
-              const fieldValue = entityState[fieldName] as { id: string };
-              return option.value === fieldValue?.id;
-            }) ?? dropdownOptions[0]}
-            name={`${String(fieldName)}-select`}
-            onChange={(selectedOption) => {
-              if (required && selectedOption.value === '') {
-                setError(`${label} is required`)
-              }
-              if (entityState) {
-                setEntityState({
-                  ...entityState,
-                  [fieldName]: selectedOption.value ? { id: selectedOption.value, name: selectedOption.name } : null,
-                });
-              }
-            }}
-            buttonClassName={error ? 'border-red-100' : ''}
-          />
-          {required && (
-            error ? (
-              <div className="text-red-100 text-eyebrow mt-[8px]">
-                {error}
-              </div>
-            ) : (
-              <div className={'h-5'}/>
-            )
-          )}
-        </div>
+				<div className={'w-full'}>
+					<ComboBox
+						options={dropdownOptions}
+						value={
+							dropdownOptions.find(option => {
+								if (fieldType === 'object') {
+									const fieldValue = entityState[fieldName] as { id: string } | null;
+									return option.value === fieldValue?.id;
+								} else {
+									return option.value === String(entityState[fieldName]);
+								}
+							}) ?? dropdownOptions[0]
+						}
+						name={`${String(fieldName)}-select`}
+						onChange={selectedOption => {
+							if (required && selectedOption.value === '') {
+								setError(`${label} is required`);
+							}
+							if (entityState) {
+								if (fieldType === 'object') {
+									// e.g. MaterialClassification, Supplier
+									setEntityState({
+										...entityState,
+										[fieldName]: selectedOption.value ? { id: selectedOption.value, name: selectedOption.name } : null,
+									});
+								} else {
+									setEntityState({
+										// e.g. Supplier Tier
+										...entityState,
+										[fieldName]: parseInt(selectedOption.value),
+									});
+								}
+							}
+						}}
+						buttonClassName={error ? 'border-red-100' : ''}
+						disabled={disabled}
+					/>
+					{required && error && <div className="text-red-100 text-eyebrow mt-[8px]">{error}</div>}
+					{disabled && disabledMessage && <div className="text-tc-disabled text-eyebrow mt-[8px]">{disabledMessage}</div>}
+				</div>
 			</div>
 		</div>
 	);
---
+++ libs/react/src/components/molecules/editSupplierDetails/editSupplierDetails.tsx
@@ -0,0 +1,166 @@
+import { Card, DropdownInputForEntityEdit, ErrorFallback, TextInputForEntityEdit } from '@coldpbc/components';
+import { ButtonTypes, EntityLevel } from '@coldpbc/enums';
+import React, { useCallback, useEffect, useState } from 'react';
+import { withErrorBoundary } from 'react-error-boundary';
+import { SupplierGraphQL, ToastMessage } from '@coldpbc/interfaces';
+import { useAddToastMessage, useAuth0Wrapper, useColdContext, useEntityData, useGraphQLMutation } from '@coldpbc/hooks';
+import { ApolloQueryResult } from '@apollo/client';
+import { KeyedMutator } from 'swr';
+
+interface EditSupplierDetailsProps {
+	supplier: SupplierGraphQL;
+	onClose: () => void;
+	refreshSupplier: KeyedMutator<ApolloQueryResult<{ organizationFacility: SupplierGraphQL | null }>>;
+}
+
+const isSupplierEdited = (originalSupplier: SupplierGraphQL, editedSupplier: SupplierGraphQL): boolean => {
+	const keysToCompare = ['name', 'addressLine1', 'addressLine2', 'city', 'stateProvince', 'postalCode', 'country', 'supplierTier', 'brandFacilityId', 'category', 'subcategory'];
+
+	return keysToCompare.some(key => {
+		return originalSupplier[key] !== editedSupplier[key];
+	});
+};
+
+const _EditSupplierDetails: React.FC<EditSupplierDetailsProps> = ({ supplier, onClose, refreshSupplier }) => {
+	const { orgId } = useAuth0Wrapper();
+	const { logBrowser } = useColdContext();
+	const { addToastMessage } = useAddToastMessage();
+	const [isLoading, setIsLoading] = useState(false);
+	const [isDisabled, setIsDisabled] = useState(false);
+	const [tierSelectorIsDisabled, setTierSelectorIsDisabled] = useState(false);
+	const [editedSupplier, setEditedSupplier] = useState<SupplierGraphQL>(supplier);
+	const [errors, setErrors] = useState<Partial<Record<keyof SupplierGraphQL, string>>>({});
+	const existingSuppliers = useEntityData(EntityLevel.SUPPLIER, orgId);
+	const preexistingSupplierNames = existingSuppliers.filter(s => s.name !== supplier.name).map(s => s.name);
+	const { mutateGraphQL: updateSupplier } = useGraphQLMutation('UPDATE_SUPPLIER');
+
+	useEffect(() => {
+		const hasErrors = Object.values(errors).some(error => error !== null && error !== undefined);
+		const isEdited = isSupplierEdited(supplier, editedSupplier);
+
+		setIsDisabled(hasErrors || !isEdited);
+	}, [errors, supplier, editedSupplier]);
+
+	useEffect(() => {
+    if (supplier.supplierTier === 1 && supplier.products.length > 0) {
+      setTierSelectorIsDisabled(true);
+    } else {
+      setTierSelectorIsDisabled(false);
+    }
+  }, [supplier.supplierTier, supplier.products]);
+
+	const saveSupplierChange = useCallback(
+		async (editedSupplier: SupplierGraphQL) => {
+			setIsLoading(true);
+
+			if (!editedSupplier.supplierTier) {
+				throw new Error('Cannot save supplier without a supplier tier');
+			}
+
+			try {
+				await updateSupplier({
+					input: {
+						id: supplier.id,
+						name: editedSupplier.name,
+						supplierTier: editedSupplier.supplierTier,
+						addressLine1: editedSupplier.addressLine1,
+						addressLine2: editedSupplier.addressLine2,
+						city: editedSupplier.city,
+						stateProvince: editedSupplier.stateProvince,
+						postalCode: editedSupplier.postalCode,
+						country: editedSupplier.country,
+						category: editedSupplier.category,
+						subcategory: editedSupplier.subcategory,
+						brandFacilityId: editedSupplier.brandFacilityId,
+					},
+				});
+
+				logBrowser(`Updated supplier ${supplier.id} successfully`, 'info', {
+					orgId,
+					supplierId: supplier.id,
+				});
+
+				// Revalidate material query
+				await refreshSupplier();
+
+				onClose();
+
+				await addToastMessage({
+					message: 'Supplier updated successfully',
+					type: ToastMessage.SUCCESS,
+				});
+			} catch (e) {
+				logBrowser(`Error updating supplier ${supplier.id}`, 'error', {
+					orgId,
+					supplierId: supplier.id,
+					error: e,
+				});
+				await addToastMessage({
+					message: 'Error updating supplier',
+					type: ToastMessage.FAILURE,
+				});
+			} finally {
+				setIsLoading(false);
+			}
+		},
+		[orgId, supplier, onClose, logBrowser, addToastMessage, refreshSupplier, updateSupplier],
+	);
+
+	const ctas = [
+		{
+			text: 'Cancel',
+			action: onClose,
+			variant: ButtonTypes.secondary,
+		},
+		{
+			text: 'Save',
+			action: async () => {
+				if (editedSupplier) {
+					await saveSupplierChange(editedSupplier);
+				}
+			},
+			disabled: isDisabled,
+			loading: isLoading,
+			variant: ButtonTypes.primary,
+		},
+	];
+
+	const inputProps = (fieldName: keyof SupplierGraphQL) => ({
+		fieldName: fieldName,
+		setEntityState: setEditedSupplier,
+		entityState: editedSupplier,
+		originalEntity: supplier,
+		error: errors[fieldName],
+		setError: (error?: string) => {
+			setErrors({ ...errors, [fieldName]: error });
+		},
+	});
+
+	return (
+		<Card title={'Details'} ctas={ctas} className="w-[406px] min-w-[406px] h-fit" overflowHidden={false}>
+			<TextInputForEntityEdit<SupplierGraphQL> {...inputProps('name')} label={'Name'} required={true} preexistingValues={preexistingSupplierNames} />
+			<DropdownInputForEntityEdit<SupplierGraphQL>
+				{...inputProps('supplierTier')}
+				label={'Supplier Tier'}
+				required={true}
+				options={[{ value: 1 }, { value: 2 }]}
+				fieldType={'value'}
+				disabled={tierSelectorIsDisabled}
+				disabledMessage={'Cannot change tier of supplier with products'}
+			/>
+			<TextInputForEntityEdit<SupplierGraphQL> {...inputProps('addressLine1')} label={'Address Line 1'} />
+			<TextInputForEntityEdit<SupplierGraphQL> {...inputProps('addressLine2')} label={'Address Line 2'} />
+			<TextInputForEntityEdit<SupplierGraphQL> {...inputProps('city')} label={'City'} />
+			<TextInputForEntityEdit<SupplierGraphQL> {...inputProps('stateProvince')} label={'State'} />
+			<TextInputForEntityEdit<SupplierGraphQL> {...inputProps('postalCode')} label={'Postal Code'} />
+			<TextInputForEntityEdit<SupplierGraphQL> {...inputProps('country')} label={'Country'} />
+			<TextInputForEntityEdit<SupplierGraphQL> {...inputProps('brandFacilityId')} label={'Brand Facility ID'} />
+			<TextInputForEntityEdit<SupplierGraphQL> {...inputProps('category')} label={'Category'} />
+			<TextInputForEntityEdit<SupplierGraphQL> {...inputProps('subcategory')} label={'Sub-Category'} />
+		</Card>
+	);
+};
+
+export const EditSupplierDetails = withErrorBoundary(_EditSupplierDetails, {
+	FallbackComponent: props => <ErrorFallback {...props} />,
+});
