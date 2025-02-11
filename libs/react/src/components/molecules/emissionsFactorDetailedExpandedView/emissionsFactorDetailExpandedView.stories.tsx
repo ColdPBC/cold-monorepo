86778db32d4645bdf2ea43326ccc610f409033b1
Qaalib Farah <qaalib1598@gmail.com>
2025-02-11 13:46:42 -0600

    chore: Update story args for EmissionsFactorDetailedExpandedView
    Revised the story configuration to provide a detailed `emissionsFactor` object while setting `weight` to null. Removed unused imports to improve code clarity and maintainable structure.

--- libs/react/src/components/molecules/emissionsFactorDetailedExpandedView/emissionsFactorDetailExpandedView.stories.tsx
+++ libs/react/src/components/molecules/emissionsFactorDetailedExpandedView/emissionsFactorDetailExpandedView.stories.tsx
@@ -1,9 +1,8 @@
-import { ColdEmissionsContext } from '@coldpbc/context';
 import { withKnobs } from '@storybook/addon-knobs';
 import { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
-import {EmissionsFactorDetailedExpandedView, EmissionsScopeChartCard} from '@coldpbc/components';
-import { getDefaultEmissionMock, StoryMockProvider } from '@coldpbc/mocks';
+import { EmissionsFactorDetailedExpandedView } from '@coldpbc/components';
+import { StoryMockProvider } from '@coldpbc/mocks';
 
 const meta: Meta<typeof EmissionsFactorDetailedExpandedView> = {
   title: 'Molecules/EmissionsFactorDetailedExpandedView',
@@ -53,8 +52,12 @@ export const WithOutEmissions: Story = {
 
 export const WithOutWeight: Story = {
   args: {
-    emissionsFactor: null,
-    weight: 0.176,
+    emissionsFactor: {
+      name: 'Sheep Wool Insulation',
+      description: 'The emissions factor for ethical wool insulation measures the greenhouse gas emissions produced throughout its lifecycle, including sheep farming, shearing, processing, and transportation. Ethical wool typically emphasizes sustainable grazing practices, animal welfare, and eco-friendly processing, which can help reduce its carbon footprint compared to conventional wool. The emissions factor serves as a critical metric for assessing the environmental impact of the fabric and guiding sustainable  choices.',
+      emissions_factor: 3.2,
+    },
+    weight: null,
   },
   render: (args) => {
     return (
