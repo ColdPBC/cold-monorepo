72b3944e0993fb132220e9329b210a66895568c5
Jason Mooty <jason.mooty@gmail.com>
2025-02-12 11:57:04 -0600

    chore: use new weight component on Product BOM tab

--- libs/react/src/components/atoms/calculatedWeight/calculatedWeight.tsx
+++ libs/react/src/components/atoms/calculatedWeight/calculatedWeight.tsx
@@ -1,18 +1,15 @@
-import { ProductMaterial } from '@coldpbc/interfaces';
-import { getCalculatedWeight } from '@coldpbc/lib';
+import { CalculatedWeightResult } from '@coldpbc/lib';
 import { Popover } from '../popover';
 import { ColdIcon } from '../icons';
 import { IconNames } from '@coldpbc/enums';
 import React from 'react';
 
 interface CalculatedWeightProps {
-	productMaterial: ProductMaterial;
+	weightResult: CalculatedWeightResult;
 }
 
-export const CalculatedWeight: React.FC<CalculatedWeightProps> = ({ productMaterial }) => {
-	const result = getCalculatedWeight(productMaterial);
-
-	if ('error' in result) {
+export const CalculatedWeight: React.FC<CalculatedWeightProps> = ({ weightResult }) => {
+	if ('error' in weightResult) {
 		return (
 			<div
 				className={'h-full flex gap-1 items-center justify-start text-tc-disabled'}
@@ -22,23 +19,23 @@ export const CalculatedWeight: React.FC<CalculatedWeightProps> = ({ productMater
 				<span className="text-body">Unknown</span>
 				<Popover
 					contentClassName="max-w-[260px]"
-					content={result.error}
+					content={weightResult.error}
 					aria-label="Weight calculation error details"
 				>
 					<ColdIcon name={IconNames.ColdUnknownIcon} />
 				</Popover>
 			</div>
 		);
-	} else if ('explanation' in result) {
+	} else if ('explanation' in weightResult) {
 		return (
 			<div className={'h-full flex gap-1 items-center justify-start'}>
-				<span className="text-body text-tc-primary">{result.displayWeight}</span>
-				<Popover contentClassName="max-w-[260px]" content={result.explanation}>
+				<span className="text-body text-tc-primary">{weightResult.displayWeight}</span>
+				<Popover contentClassName="max-w-[260px]" content={weightResult.explanation}>
 					<ColdIcon className={'text-tc-disabled'} color={'currentColor'} name={IconNames.ColdCalculatorIcon} />
 				</Popover>
 			</div>
 		);
 	} else {
-    return <span className="text-tc-primary text-body">{result.displayWeight}</span>;
+    return <span className="text-tc-primary text-body">{weightResult.displayWeight}</span>;
   }
 };
