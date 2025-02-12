351f68174f3302136834810318551a7900b01cc6
Jason Mooty <jason.mooty@gmail.com>
2025-02-12 09:09:27 -0600

    chore: add popover on emoji

--- libs/react/src/components/molecules/materialDetailsCard/materialDetailsCard.tsx
+++ libs/react/src/components/molecules/materialDetailsCard/materialDetailsCard.tsx
@@ -11,20 +11,31 @@ interface MaterialDetailsCardProps {
   editMaterial: () => void;
 }
 
-const getWeightFactorValue = (material) => {
-  if (material.weightFactor && material.weightFactorUnitOfMeasure) {
-    return `${material.weightFactor.toFixed(2)} ${material.weightFactorUnitOfMeasure}`;
-  } else if (material.materialClassification?.weightFactor) {
-    return `✨ ${material.materialClassification.weightFactor.toFixed(2)} ${WeightFactorUnits.KG_PER_M2}`;
-  } else {
-    return undefined;
-  }
-}
+const getWeightFactorValue = material => {
+	if (material.weightFactor && material.weightFactorUnitOfMeasure) {
+		return `${material.weightFactor.toFixed(2)} ${material.weightFactorUnitOfMeasure}`;
+	} else if (material.materialClassification?.weightFactor) {
+		return (
+			<div className={'flex items-center justify-start gap-1'}>
+				<Popover content={`Based on a material classification of ${material.materialClassification.name}`} contentClassName="w-[275px]">
+					<span role="img" aria-label={'Sparkles emoji'}>
+						✨
+					</span>
+				</Popover>
+				<span>
+					{material.materialClassification.weightFactor.toFixed(2)} {WeightFactorUnits.KG_PER_M2}
+				</span>
+			</div>
+		);
+	} else {
+		return undefined;
+	}
+};
 
 const _MaterialDetailsCard: React.FC<MaterialDetailsCardProps> = ({ material, editMaterial }) => {
-  const tier2Supplier = material.organizationFacility;
+	const tier2Supplier = material.organizationFacility;
 
-  const classificationCategory = (
+	const classificationCategory = (
 		<div className={'flex items-center justify-start gap-1'}>
 			<span>Classification</span>
 			<Popover content={'This classification is used for carbon accounting and sustainability attribute reporting.'} contentClassName="w-[275px]">
@@ -34,7 +45,7 @@ const _MaterialDetailsCard: React.FC<MaterialDetailsCardProps> = ({ material, ed
 	);
 
 	return (
-		<Card title={'Details'} ctas={[{text: 'Edit', action: editMaterial}]} className={'w-[406px] min-w-[406px] h-fit'} data-testid={'material-details-card'}>
+		<Card title={'Details'} ctas={[{ text: 'Edit', action: editMaterial }]} className={'w-[406px] min-w-[406px] h-fit'} data-testid={'material-details-card'}>
 			<DetailsItem category={'Name'} value={material.name} />
 			<DetailsItem category={'Description'} value={material.description} />
 			<DetailsItem category={'Tier 2 Supplier'} value={tier2Supplier?.name} href={tier2Supplier ? `/suppliers/${tier2Supplier.id}` : undefined} />
@@ -44,8 +55,8 @@ const _MaterialDetailsCard: React.FC<MaterialDetailsCardProps> = ({ material, ed
 			<DetailsItem category={'Sub-Category'} value={material.materialSubcategory} />
 			<DetailsItem category={'Brand Material ID'} value={material.brandMaterialId} />
 			<DetailsItem category={'Supplier Material ID'} value={material.supplierMaterialId} />
-      <DetailsItem category={'Weight Factor'} value={getWeightFactorValue(material)} />
-      <DetailsItem category={'Width'} value={(material.width && material.widthUnitOfMeasure) ? `${material.width.toFixed(2)} ${material.widthUnitOfMeasure}` : undefined} />
+			<DetailsItem category={'Weight Factor'} value={getWeightFactorValue(material)} />
+			<DetailsItem category={'Width'} value={material.width && material.widthUnitOfMeasure ? `${material.width.toFixed(2)} ${material.widthUnitOfMeasure}` : undefined} />
 		</Card>
 	);
 };
