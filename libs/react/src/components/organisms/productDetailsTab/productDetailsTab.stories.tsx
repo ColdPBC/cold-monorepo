59ab742c658f73f49bd3e01bc74370c260ed429d
Jason Mooty <jason.mooty@gmail.com>
2025-02-19 14:51:21 -0600

    chore: wrap story components in StoryMockProvider

--- libs/react/src/components/organisms/productDetailsTab/productDetailsTab.stories.tsx
+++ libs/react/src/components/organisms/productDetailsTab/productDetailsTab.stories.tsx
@@ -1,7 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
 
 import { ProductDetailsTab } from '@coldpbc/components';
-import { getProductsMockById } from '@coldpbc/mocks';
+import { getProductsMockById, StoryMockProvider } from '@coldpbc/mocks';
 
 const meta: Meta<typeof ProductDetailsTab> = {
   title: "Molecules/ProductDetailsTab",
@@ -16,5 +16,10 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     product: getProductsMockById('op_wvjr8v3tdzk6j3wl2x8a26bw')!,
-  }
+  },
+  render: (args) => (
+    <StoryMockProvider>
+      <ProductDetailsTab {...args} />
+    </StoryMockProvider>
+  )
 };
