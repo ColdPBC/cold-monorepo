b0eada65e0b9e6dad5215eb69f6c1852c0339ffd
Qaalib Farah <qaalib1598@gmail.com>
2025-02-20 16:48:54 -0600

    feat: Add SustainabiliBuddyContainer component and integrate it
    Introduce the `SustainabiliBuddyContainer` molecule with gradient styling and embed it into the `SustainabiliBuddy` organism. Also, create a Storybook story for `SustainabiliBuddy` to enable testing and documentation.

---
+++ libs/react/src/components/molecules/sustainabiliBuddyContainer/sustainabiliBuddyContainer.tsx
@@ -0,0 +1,13 @@
+
+
+export const SustainabiliBuddyContainer = () => {
+
+
+  return (
+    <div className="h-[calc(100vh-170px)] min-h-[400px] w-[506px] p-[2px] rounded-[16px] bg-gradient-to-br from-[#F7FF58] via-[#32B5FF] to-[#485CEA]">
+      <div className={'p-6 h-full w-full flex flex-col rounded-[16px] bg-black'}>
+
+      </div>
+    </div>
+  )
+}
--- libs/react/src/components/organisms/sustainabiliBuddy/sustainabiliBuddy.tsx
+++ libs/react/src/components/organisms/sustainabiliBuddy/sustainabiliBuddy.tsx
@@ -1,12 +1,18 @@
 import {useState} from "react";
 import { SustainabiliBuddyButton } from "@coldpbc/components";
+import {SustainabiliBuddyContainer} from "../../molecules/sustainabiliBuddyContainer/sustainabiliBuddyContainer";
 
 
 export const SustainabiliBuddy = () => {
   const [showPromptContainer, setShowPromptContainer] = useState(false);
 
   return (
-    <div className={'fixed right-5 bottom-5'}>
+    <div className={'fixed right-5 bottom-5 flex flex-col items-end gap-[14px]'}>
+      {
+        showPromptContainer && (
+          <SustainabiliBuddyContainer />
+        )
+      }
       <SustainabiliBuddyButton
         showPromptContainer={showPromptContainer}
         setShowPromptContainer={setShowPromptContainer}
