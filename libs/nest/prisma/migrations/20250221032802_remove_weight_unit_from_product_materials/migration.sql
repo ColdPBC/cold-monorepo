d9d7bb96a821fc794332e4460f78013eaaa309d7
Troy Morvant <troy.morvant@gmail.com>
2025-02-20 21:35:46 -0600

    revert: mistakenly added WeightUnit to product_materials
    - Added a migration to add the weight_unit column to the product_materials table.
    - Added a subsequent migration to remove the weight_unit column from the product_materials table.
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

---
+++ libs/nest/prisma/migrations/20250221032802_remove_weight_unit_from_product_materials/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `weight_unit` on the `product_materials` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "product_materials" DROP COLUMN "weight_unit";
