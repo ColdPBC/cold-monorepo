4c5e19e3db2b1a66d171760a7578268b6e0e57bc
Qaalib Farah <qaalib1598@gmail.com>
2025-02-21 17:38:37 -0600

    feat: Update gradient colors in SustainabiliBuddy components
    Changed the gradient "from" color from `#F7FF58` to `yellow-250` in both the `sustainabiliBuddyContainer` and `sustainabiliBuddyButton` components for consistency. This ensures a more cohesive design across the application.

--- libs/react/src/components/molecules/sustainabiliBuddyContainer/sustainabiliBuddyContainer.tsx
+++ libs/react/src/components/molecules/sustainabiliBuddyContainer/sustainabiliBuddyContainer.tsx
@@ -23,7 +23,7 @@ export const SustainabiliBuddyContainer = () => {
   }
 
   return (
-    <div className="h-[calc(100vh-170px)] md:h-[75vh] lg:h-[66.67vh] min-h-[400px] w-screen md:w-[66.67vw] lg:w-[50vw] p-0.5 rounded-[16px] bg-gradient-to-br from-[#F7FF58] via-lightblue-400 to-primary-300">
+    <div className="h-[calc(100vh-170px)] md:h-[75vh] lg:h-[66.67vh] min-h-[400px] w-screen md:w-[66.67vw] lg:w-[50vw] p-0.5 rounded-[16px] bg-gradient-to-br from-yellow-250 via-lightblue-400 to-primary-300">
       <div className={'p-6 h-full w-full flex flex-col rounded-[16px] bg-black justify-end'}>
         <SustainabiliBuddyInput
           onEnter={onEnter}
