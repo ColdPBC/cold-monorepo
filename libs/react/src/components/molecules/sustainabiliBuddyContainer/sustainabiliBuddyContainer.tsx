83c61208860d6da549b61fc3f6fea642df10b1cb
Qaalib Farah <qaalib1598@gmail.com>
2025-02-21 10:09:47 -0600

    feat: Add SustainabiliBuddyInput component and input enhancements
    Introduced a new SustainabiliBuddyInput component to handle input interactions, including multi-line text support and prompt submission. Updated SustainabiliBuddyContainer to integrate this component. Additionally, refined data-test ID naming conventions in the Input component for consistency.

--- libs/react/src/components/molecules/sustainabiliBuddyContainer/sustainabiliBuddyContainer.tsx
+++ libs/react/src/components/molecules/sustainabiliBuddyContainer/sustainabiliBuddyContainer.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
+import {ColdIcon, Input, SustainabiliBuddyInput} from "@coldpbc/components";
+import { IconNames } from "@coldpbc/enums";
 
 
 export const SustainabiliBuddyContainer = () => {
+  const [queue, setQueue] = useState<string[]>([]);
+  const [currentPrompt, setCurrentPrompt] = useState<string>('');
 
+  const onEnter = () => {
+    if (currentPrompt) {
+      setQueue([...queue, currentPrompt]);
+      setCurrentPrompt('');
+    }
+  }
+
+  console.log(queue);
 
   return (
     <div className="h-[calc(100vh-170px)] min-h-[400px] w-[506px] p-[2px] rounded-[16px] bg-gradient-to-br from-[#F7FF58] via-[#32B5FF] to-[#485CEA]">
-      <div className={'p-6 h-full w-full flex flex-col rounded-[16px] bg-black'}>
-
+      <div className={'p-6 h-full w-full flex flex-col rounded-[16px] bg-black justify-end'}>
+        <SustainabiliBuddyInput
+          currentPrompt={currentPrompt}
+          setCurrentPrompt={setCurrentPrompt}
+          onEnter={onEnter}
+          />
       </div>
     </div>
   )
--- libs/react/src/interfaces/input.ts
+++ libs/react/src/interfaces/input.ts
@@ -21,6 +21,7 @@ export interface NumericProps extends NumericInputProps {
 export interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   error?: string;
   showError?: boolean;
+  ref?: MutableRefObject<HTMLTextAreaElement | null>;
 }
 
 export interface BaseInputProps extends InputHTMLAttributes<HTMLInputElement> {
