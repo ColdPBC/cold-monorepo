44ee0173f8df779ec0e37c90f783e426df287f7d
Qaalib Farah <qaalib1598@gmail.com>
2025-03-05 10:52:17 -0600

    feat: Handle errors at all levels in getGraphqlError utility
    Improve error handling in the getGraphqlError function by addressing SWR, Apollo, and GraphQL server-level errors. This ensures more comprehensive and precise error reporting across different layers.

--- libs/react/src/lib/graphql/graphQLErrorUtils.ts
+++ libs/react/src/lib/graphql/graphQLErrorUtils.ts
@@ -6,14 +6,17 @@ import {GraphQLFormattedError} from "graphql/error";
 export const getGraphqlError = <TData>(
   query: SWRResponse<ApolloQueryResult<TData>, any>
 ): ApolloError | GraphQLFormattedError[] | null => {
+  // handle SWR level error
   if(query.error){
     return query.error;
   }
-
+  // handle Apollo level error
   const error = get(query, 'data.error', null);
   if (error) {
     return error;
   }
+
+  // handle GraphQL Server level error
   const errors = get(query, 'data.errors', []);
   return isArray(errors) && errors.length > 0 ? errors : null;
 };
