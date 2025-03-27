4b0ce1bee0712a8d0484b6e6df640a55c1110dbc
Troy Morvant <troy.morvant@gmail.com>
2025-03-27 11:39:51 -0500

    feat(prisma): update schema and migrations for material and emission factors
    - Add `organization_id` and `reasoning` fields to `material_emission_factors` table
    - Remove `reasoning` field from `ecoinvent_activity_classifications` table
    - Fix indentation in `product_tag_assignments` field definition in schema
    
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

---
+++ libs/nest/prisma/migrations/20250326193729_add_reasoning_to_material_emission_factos/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "material_emission_factors" ADD COLUMN     "organization_id" TEXT,
+ADD COLUMN     "reasoning" TEXT;
---
+++ libs/nest/prisma/migrations/20250326201732_remove_unnecessary_reasoning_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `reasoning` on the `ecoinvent_activity_classifications` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ecoinvent_activity_classifications" DROP COLUMN "reasoning";
