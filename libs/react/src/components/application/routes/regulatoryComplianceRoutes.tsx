8c97327d97d81cdf851d187b0a9e532f0e4be918
Jason Mooty <jason.mooty@gmail.com>
2025-04-04 16:50:42 -0500

    feat: simply route logic

--- libs/react/src/components/application/routes/regulatoryComplianceRoutes.tsx
+++ libs/react/src/components/application/routes/regulatoryComplianceRoutes.tsx
@@ -1,22 +1,14 @@
 import {Navigate, Route} from "react-router-dom";
-import {RegulatoryComplianceDetail, RegulatoryCompliance, DEFAULT_PAGE} from "@coldpbc/components";
+import { RegulatoryComplianceDetail, RegulatoryCompliance, DEFAULT_PAGE } from '@coldpbc/components';
 import {useFlags} from "launchdarkly-react-client-sdk";
 
 
 export const RegulatoryComplianceRoutes = () => {
   const ldFlags = useFlags()
   return (
-    <Route path={'/regulatory_compliance'}>
-      {
-        ldFlags.showRegulationsPage ? (
-          <>
-            <Route index element={<RegulatoryCompliance />} />
-            <Route path={':id'} element={<RegulatoryComplianceDetail />} />
-          </>
-        ) : (
-          <Route index element={<Navigate to={DEFAULT_PAGE} replace={true} />} />
-        )
-      }
-    </Route>
+    <>
+      <Route path={'/regulatory_compliance'} element={ldFlags.showRegulationsPage ? <RegulatoryCompliance /> : <Navigate to={DEFAULT_PAGE} replace={true} />} />
+      <Route path={'/regulatory_compliance/:slug'} element={ldFlags.showRegulationsPage ? <RegulatoryComplianceDetail /> : <Navigate to={DEFAULT_PAGE} replace={true} />} />
+    </>
   );
 }
