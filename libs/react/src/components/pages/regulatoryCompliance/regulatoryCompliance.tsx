1986a82430ff000685d34e1eaff78eee4a59b989
Qaalib Farah <qaalib1598@gmail.com>
2025-04-04 17:11:46 -0500

    feat: Add YesNo enum for penalties column in regulatory table
    Updated the "Penalties (Beyond Fees)" column to use the YesNo enum and specify it as a singleSelect type. This enhances consistency and ensures valid value options for the column.

--- libs/react/src/components/pages/regulatoryCompliance/regulatoryCompliance.tsx
+++ libs/react/src/components/pages/regulatoryCompliance/regulatoryCompliance.tsx
@@ -2,7 +2,7 @@ import {DEFAULT_GRID_COL_DEF, MainContent, MuiDataGrid} from "@coldpbc/component
 import {useRegulations} from "@coldpbc/hooks";
 import {GridColDef} from "@mui/x-data-grid-pro";
 import {useNavigate} from "react-router-dom";
-import {RegulationCategory, RegulationStatus, RegulationSubcategory} from "@coldpbc/enums";
+import {RegulationCategory, RegulationStatus, RegulationSubcategory, YesNo} from "@coldpbc/enums";
 import React from "react";
 
 /**
@@ -43,7 +43,7 @@ export const RegulatoryCompliance = () => {
     { ...DEFAULT_GRID_COL_DEF, field: 'Subcategory', headerName: 'Subcategory', width: 150, type: 'singleSelect', valueOptions: Object.values(RegulationSubcategory) },
     { ...DEFAULT_GRID_COL_DEF, field: 'Product Type Eligibility', headerName: 'Product Type Eligibility', minWidth: 300, flex: 1, renderCell: (params) => (<ProductTypeEligibility eligibility={params.row["Product Type Eligibility"]} />) },
     { ...DEFAULT_GRID_COL_DEF, field: 'Fees', headerName: 'Fees', width: 100 },
-    { ...DEFAULT_GRID_COL_DEF, field: 'Penalties (Beyond Fees)', headerName: 'Penalties (Beyond Fees)', width: 200 },
+    { ...DEFAULT_GRID_COL_DEF, field: 'Penalties (Beyond Fees)', headerName: 'Penalties (Beyond Fees)', width: 200, type: 'singleSelect', valueOptions: Object.values(YesNo) },
   ]
 
   const rows = filteredRegulations.map(({slug, regulation}) => {
