55e73387cb64262d2530b42e90d011427ca2247b
Troy Morvant <troy.morvant@gmail.com>
2025-04-17 17:13:39 -0500

    fix(controller): refactor ComponentDefinitionsController for improved readability and maintainability
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-api/src/platform/modules/resources/component-definitions/component-definitions.controller.ts
+++ apps/cold-api/src/platform/modules/resources/component-definitions/component-definitions.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post, Req, UseFilters, UseGuards } from '@nestjs/common';
 import { ApiBody, ApiOAuth2, ApiOperation, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { Span } from 'nestjs-ddtrace';
-import { component_definition_types } from 'prisma/prisma-client';
+import { component_definition_types } from '@prisma/client';
 import { ResourceValidationPipe } from '../../../pipes/resource.pipe';
 import { BaseWorker, HttpExceptionFilter, IRequest, JwtAuthGuard, Roles, RolesGuard } from '@coldpbc/nest';
 import { allRoles, bpcDecoratorOptions, coldAdminOnly } from '../_global/global.params';
@@ -18,135 +18,135 @@ import ComponentDefinitionSchema from '../../../../../../../libs/nest/src/valida
 @UseFilters(new HttpExceptionFilter(ComponentDefinitionsController.name))
 @Controller('components')
 export class ComponentDefinitionsController extends BaseWorker {
-  constructor(private readonly componentDefinitions: ComponentDefinitionsService) {
-    super(ComponentDefinitionsController.name);
-  }
+	constructor(private readonly componentDefinitions: ComponentDefinitionsService) {
+		super(ComponentDefinitionsController.name);
+	}
 
-  @ApiOperation({
-    summary: 'Create Component Definition',
-    operationId: 'CreateComponentDefinition',
-  })
-  @Post()
-  @ApiBody({
-    schema: {
-      example: postComponentExample,
-    },
-    type: ComponentDefinitionDto,
-  })
-  @Roles(...coldAdminOnly)
-  @HttpCode(201)
-  create(
-    @Req()
-    req: IRequest,
-    @Body(new ResourceValidationPipe(ComponentDefinitionSchema, 'POST')) createComponentDefinitionDto: ComponentDefinitionDto,
-  ) {
-    return this.componentDefinitions.create(createComponentDefinitionDto, req);
-  }
+	@ApiOperation({
+		summary: 'Create Component Definition',
+		operationId: 'CreateComponentDefinition',
+	})
+	@Post()
+	@ApiBody({
+		schema: {
+			example: postComponentExample,
+		},
+		type: ComponentDefinitionDto,
+	})
+	@Roles(...coldAdminOnly)
+	@HttpCode(201)
+	create(
+		@Req()
+		req: IRequest,
+		@Body(new ResourceValidationPipe(ComponentDefinitionSchema, 'POST')) createComponentDefinitionDto: ComponentDefinitionDto,
+	) {
+		return this.componentDefinitions.create(createComponentDefinitionDto, req);
+	}
 
-  @ApiOperation({
-    summary: 'Get Component Definitions',
-    operationId: 'GetComponentDefinitions',
-  })
-  @Get()
-  @ApiQuery(bpcDecoratorOptions)
-  @Roles(...allRoles)
-  findAll(
-    @Req()
-    req: IRequest,
-  ) {
-    return this.componentDefinitions.findAll(req);
-  }
+	@ApiOperation({
+		summary: 'Get Component Definitions',
+		operationId: 'GetComponentDefinitions',
+	})
+	@Get()
+	@ApiQuery(bpcDecoratorOptions)
+	@Roles(...allRoles)
+	findAll(
+		@Req()
+		req: IRequest,
+	) {
+		return this.componentDefinitions.findAll(req);
+	}
 
-  @ApiOperation({
-    summary: 'Get Component Definitions By Type',
-    operationId: 'GetComponentDefinitionsByType',
-  })
-  @Get('types/:type')
-  @Roles(...allRoles)
-  @ApiParam({
-    name: 'type',
-    enum: component_definition_types,
-  })
-  async getAllByType(
-    @Param('type') type: component_definition_types,
-    @Req()
-    req: IRequest,
-  ) {
-    const response = await this.componentDefinitions.findByType(req, type);
-    return response;
-  }
+	@ApiOperation({
+		summary: 'Get Component Definitions By Type',
+		operationId: 'GetComponentDefinitionsByType',
+	})
+	@Get('types/:type')
+	@Roles(...allRoles)
+	@ApiParam({
+		name: 'type',
+		enum: component_definition_types,
+	})
+	async getAllByType(
+		@Param('type') type: component_definition_types,
+		@Req()
+		req: IRequest,
+	) {
+		const response = await this.componentDefinitions.findByType(req, type);
+		return response;
+	}
 
-  @ApiOperation({
-    summary: 'Get Component Definitions By Name',
-    operationId: 'GetComponentDefinitionsByName',
-  })
-  @Get(':name')
-  @Roles(...allRoles)
-  @ApiParam({
-    name: 'name',
-    description: 'Component Name',
-    example: '{{test_component_name}}',
-  })
-  async findOne(
-    @Param('name') name: string,
-    @Req()
-    req: IRequest,
-  ) {
-    const response = await this.componentDefinitions.findOne(name, req);
-    return response;
-  }
+	@ApiOperation({
+		summary: 'Get Component Definitions By Name',
+		operationId: 'GetComponentDefinitionsByName',
+	})
+	@Get(':name')
+	@Roles(...allRoles)
+	@ApiParam({
+		name: 'name',
+		description: 'Component Name',
+		example: '{{test_component_name}}',
+	})
+	async findOne(
+		@Param('name') name: string,
+		@Req()
+		req: IRequest,
+	) {
+		const response = await this.componentDefinitions.findOne(name, req);
+		return response;
+	}
 
-  @ApiOperation({
-    summary: 'Update Component Definitions By Name',
-    operationId: 'UpdateComponentDefinitionsByName',
-  })
-  @Patch(':name')
-  @HttpCode(201)
-  @Roles(...coldAdminOnly)
-  @ApiParam({
-    name: 'name',
-    description: 'Component Name',
-    example: '{{test_component_name}}',
-  })
-  update(
-    @Param('name') name: string,
-    @Body(new ResourceValidationPipe(ComponentDefinitionSchema, 'PATCH')) updateComponentDefinitionDto: any,
-    @Req()
-    req: IRequest,
-  ) {
-    return this.componentDefinitions.update(name, updateComponentDefinitionDto, req);
-  }
+	@ApiOperation({
+		summary: 'Update Component Definitions By Name',
+		operationId: 'UpdateComponentDefinitionsByName',
+	})
+	@Patch(':name')
+	@HttpCode(201)
+	@Roles(...coldAdminOnly)
+	@ApiParam({
+		name: 'name',
+		description: 'Component Name',
+		example: '{{test_component_name}}',
+	})
+	update(
+		@Param('name') name: string,
+		@Body(new ResourceValidationPipe(ComponentDefinitionSchema, 'PATCH')) updateComponentDefinitionDto: any,
+		@Req()
+		req: IRequest,
+	) {
+		return this.componentDefinitions.update(name, updateComponentDefinitionDto, req);
+	}
 
-  @ApiOperation({
-    summary: 'Delete Component Definitions By Name',
-    operationId: 'DeleteComponentDefinitionsByName',
-  })
-  @Delete(':name')
-  @HttpCode(204)
-  @Roles(...coldAdminOnly)
-  @ApiParam({
-    name: 'name',
-    description: 'Component Name',
-    example: '{{test_component_name}}',
-  })
-  remove(
-    @Param('name') name: string,
-    @Req()
-    req: IRequest,
-  ) {
-    return this.componentDefinitions.remove(name, req);
-  }
+	@ApiOperation({
+		summary: 'Delete Component Definitions By Name',
+		operationId: 'DeleteComponentDefinitionsByName',
+	})
+	@Delete(':name')
+	@HttpCode(204)
+	@Roles(...coldAdminOnly)
+	@ApiParam({
+		name: 'name',
+		description: 'Component Name',
+		example: '{{test_component_name}}',
+	})
+	remove(
+		@Param('name') name: string,
+		@Req()
+		req: IRequest,
+	) {
+		return this.componentDefinitions.remove(name, req);
+	}
 
-  @ApiOperation({
-    deprecated: true,
-    summary: 'Get Test Orgs',
-    description: 'Internal Use Only',
-    operationId: 'GetDarklyTestOrgs',
-  })
-  @Get('darkly/testOrgs')
-  @HttpCode(200)
-  @Roles(...coldAdminOnly)
-  testOrgs() {
-    return this.componentDefinitions.getTestOrgs();
-  }
+	@ApiOperation({
+		deprecated: true,
+		summary: 'Get Test Orgs',
+		description: 'Internal Use Only',
+		operationId: 'GetDarklyTestOrgs',
+	})
+	@Get('darkly/testOrgs')
+	@HttpCode(200)
+	@Roles(...coldAdminOnly)
+	testOrgs() {
+		return this.componentDefinitions.getTestOrgs();
+	}
 }
