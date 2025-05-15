55a4c8033be9602ef0f254a1736a250d0794589e
Troy Morvant <troy.morvant@gmail.com>
2025-05-15 09:46:31 -0500

    chore: enhance database connection handling in graphweaver-config
    Signed-off-by: Troy Morvant <troy.morvant@gmail.com>

--- apps/cold-graphql/graphweaver-config.js
+++ apps/cold-graphql/graphweaver-config.js
@@ -1,10 +1,31 @@
+const connectionValues = () => {
+	const url = process.env.DATABASE_URL;
+	if (!url) {
+		throw new Error('DATABASE_URL is not set');
+	}
+	const pattern = /postgres(?:ql)?:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/;
+	const match = url.match(pattern);
+
+	if (match) {
+		const [, user, password, host, port, dbName] = match;
+		return { user, password, host, port: +port, dbName };
+	} else {
+		throw new Error(`DATABASE_URL is not valid: ${url}`);
+	}
+};
+
 module.exports = {
-    backend: {
-        introspection: true
-    },
-    adminUI: {
-        auth: {
-            primaryMethods: ["AUTH_ZERO"],
-        },
-    },
+	backend: {
+		introspection: true,
+	},
+	adminUI: {
+		auth: {
+			primaryMethods: ['AUTH_ZERO'],
+		},
+	},
+	import: {
+		source: 'postgresql',
+		...connectionValues(),
+		overwrite: false,
+	},
 };
